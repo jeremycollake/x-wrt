d478c95156a2889b02e75d98e780dda1b587722d
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-20 12:29:03 +0000

    merged LAN page into WAN page - still needs some asthetic work

--- trunk/package/webif/files/www/cgi-bin/webif/wan.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/wan.sh
@@ -176,7 +176,7 @@ $PPTP_OPTION
 helplink|http://wiki.openwrt.org/OpenWrtDocs/Configuration#head-b62c144b9886b221e0c4b870edb0dd23a7b6acab
 end_form
 start_form|@TR<<IP Settings>>|ip_settings|hidden
-field|@TR<<IP Address>>|ipaddr|hidden
+field|@TR<<WAN IP Address>>|ipaddr|hidden
 text|wan_ipaddr|$FORM_wan_ipaddr
 field|@TR<<Netmask>>|netmask|hidden
 text|wan_netmask|$FORM_wan_netmask
@@ -213,6 +213,55 @@ field|@TR<<MTU>>|mtu|hidden
 text|ppp_mtu|$FORM_ppp_mtu
 end_form
 EOF
+
+
+FORM_dns="${lan_dns:-$(nvram get lan_dns)}"
+LISTVAL="$FORM_dns"
+handle_list "$FORM_dnsremove" "$FORM_dnsadd" "$FORM_dnssubmit" 'ip|FORM_dnsadd|@TR<<DNS Address>>|required' && {
+	FORM_dns="$LISTVAL"
+	save_setting network lan_dns "$FORM_dns"
+}
+FORM_dnsadd=${FORM_dnsadd:-192.168.1.1}
+
+if empty "$FORM_submit"; then 
+	FORM_lan_ipaddr=${lan_ipaddr:-$(nvram get lan_ipaddr)}
+	FORM_lan_netmask=${lan_netmask:-$(nvram get lan_netmask)}
+	FORM_lan_gateway=${lan_gateway:-$(nvram get lan_gateway)}
+else 
+	SAVED=1
+	validate <<EOF
+ip|FORM_lan_ipaddr|@TR<<IP Address>>|required|$FORM_lan_ipaddr
+netmask|FORM_lan_netmask|@TR<<Netmask>>|required|$FORM_lan_netmask
+ip|FORM_lan_gateway|@TR<<Gateway>>||$FORM_lan_gateway
+EOF
+	equal "$?" 0 && {
+		save_setting network lan_ipaddr $FORM_lan_ipaddr
+		save_setting network lan_netmask $FORM_lan_netmask
+		save_setting network lan_gateway $FORM_lan_gateway
+	}
+fi
+
+display_form <<EOF
+start_form|@TR<<LAN Configuration>>
+field|@TR<<LAN IP Address>>
+text|lan_ipaddr|$FORM_lan_ipaddr
+helpitem|IP Address
+helptext|Helptext LAN IP Address#This is the address you want this device to have on your LAN.
+field|@TR<<Netmask>>|||For those who do not know what a bitmask is, just think of 255 as match this part and 0 as any number here.
+text|lan_netmask|$FORM_lan_netmask
+helpitem|Netmask
+helptext|Helptext Netmask#This bitmask indicates what addresses are included in your LAN.
+field|@TR<<Default Gateway>>
+text|lan_gateway|$FORM_lan_gateway
+end_form
+start_form|@TR<<DNS Servers>>
+listedit|dns|$SCRIPT_NAME?|$FORM_dns|$FORM_dnsadd
+helpitem|Note
+helptext|Helptext DNS save#You need save your settings on this page before adding/removing DNS servers
+end_form
+EOF
+
+
 
 footer ?>
 <!--
