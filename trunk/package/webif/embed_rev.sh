ba38a1c3ab172dd9a0b3c3354361455d641ec7da
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-22 20:48:16 +0000

    stupid script to embed svn revision number into package

---
+++ trunk/package/webif/embed_rev.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# this script embeds the global revision number in document(s) given on command line
+# document should have __SVN_REVISION__ where it wants the revision number
+# to be placed. Wildcards okay.
+#
+if [ $# -lt 1 ]; then
+	echo " Invalid usage. Must supply one or more files."
+	exit 1
+fi
+revision_number=$(svn info | grep Revision | cut -c11-)
+tempfile=$(mktemp)
+echo " Revision number is $revision_number"
+echo " Temporary file is $tempfile"
+# todo: update to unlimited args
+for curfile in $1 $2 $3 $4 $5 $6 $7 $8 $9; do
+	echo " Processing $curfile ..."
+	cp $curfile $tempfile	
+	cat $tempfile | sed s/__SVN_REVISION__/$revision_number/ > $curfile
+	rm $tempfile	
+done
+
