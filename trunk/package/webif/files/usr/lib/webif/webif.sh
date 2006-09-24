0847a5bf0aa963d6b6a37b84742157e1475d8a6e
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-24 09:49:16 +0000

    i forget

--- trunk/package/webif/files/usr/lib/webif/webif.sh
+++ trunk/package/webif/files/usr/lib/webif/webif.sh
@@ -84,7 +84,6 @@ header() {
 Content-Type: text/html
 Pragma: no-cache
 
-
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <?xml version="1.0" encoding="@TR<<Encoding|ISO-8859-1>>"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
