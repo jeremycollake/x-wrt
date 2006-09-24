c094d055b1a29f4a2a23399c8d36bd348d56c4d2
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-24 19:05:48 +0000

    fixed for squashfs images that use symlink hack (had 'error: is dir') display improvements a little help text embedded in form needs a bit more work still

--- trunk/package/webif/files/www/cgi-bin/webif/crontabs.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/crontabs.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/webif-page
 <? 
 crondir_base="/var/spool/cron"
-crondir="$crondir_base/crontabs"
+crondir="$crondir_base/crontabs/crontabs/"
 
 . /usr/lib/webif/webif.sh
 
 load_settings "cron"
 
-header "System" "Crontabs" '' "$SCRIPT_NAME"
+header "System" "Crontabs" "@TR<<Cron Tables>>" "$SCRIPT_NAME"
 
-echo "Cron Tables Directory:<pre>$crondir</pre><br/>"
+cron_dir_text="Cron Tables Directory:<pre>$crondir</pre><br/>"
 
-cd $crondir
-
-for crontab in $(ls * 2>&-); do
+for crontab in $(ls $crondir/* 2>&-); do
     echo -n "<h3>$crontab</h3><br /><pre>"
-    cat "$crondir/$crontab"
+    cron_text='$cron_text $(cat "$crondir/$crontab")'
     echo '</pre><br />'
 done
+
+display_form <<EOF
+start_form|@TR<<Cron Jobs>>
+string|$cron_text
+helpitem|crontabs
+helptext|HelpText crontabs#The Cron Tables is a list of jobs the cron daemon (crond) should execute at specified intervals or times.
+string|$cron_dir_text
+end_form
+EOF
 
 footer ?>
 <!--
