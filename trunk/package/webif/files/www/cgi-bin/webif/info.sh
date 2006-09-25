406629d871cc5caa561c48c0c10209ce564e46fb
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-25 14:40:55 +0000

    re-enabled wrt54g board check - problem with other boards was inifinite loop

--- trunk/package/webif/files/www/cgi-bin/webif/info.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/info.sh
@@ -33,14 +33,15 @@ EOF
 #
 # board id checks go here.. todo: much work remains here
 #
-while empty $board_type; do
-	# disabled until sped up..
-	#strings /dev/mtdblock/0 | grep 'W54G' 2>&1 >> /dev/null
-	#if [ $? = "0" ]; then
- 	#	board_type="WRT54G"
- 		#board_version="v??"
- 	#	break
-	#fi	
+# loop is just used to avoid a jump
+while empty $board_type; do	
+	strings /dev/mtdblock/0 | grep 'W54G' 2>&1 >> /dev/null
+	if [ $? = "0" ]; then
+ 		board_type="WRT54G"
+ 		board_version="v??"
+ 		break
+	fi	
+	break
 done
 empty $board_type && board_type="-id code not done for this board-";
 
