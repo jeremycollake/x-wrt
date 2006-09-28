946d53d3baca806a86fa606b815dc03b580b1c54
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-28 22:38:24 +0000

    considerable cleanup changed old board_type and board_version to device_type/device_version add board id from /proc/cpuinfo removed /proc/cpuinfo raw dump removed /proc/meminfo raw dump formatting improvements

--- trunk/package/webif/files/www/cgi-bin/webif/info.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/info.sh
@@ -12,20 +12,21 @@ cat <<EOF
 </pre>
 <table>
 <tbody>
+<tr><td><br /><br /></td></tr>
 	<tr>
-		<td><strong>@TR<<Firmware>></strong></td>
+		<td><strong>@TR<<Firmware>></strong></td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 		<td>$_firmware_name - $_firmware_subtitle $_version</td>		
 	</tr>
 	<tr>
-		<td><strong>@TR<<Mangement Console>></strong></td>
+		<td><strong>@TR<<Webif>></strong></td><td>&nbsp;</td>
 		<td>webif^2 r__SVN_REVISION__</td>
 	</tr>
 	<tr>
-		<td><strong>@TR<<Kernel>></strong></td>
+		<td><strong>@TR<<Kernel>></strong></td><td>&nbsp;</td>
 		<td>$_kversion</td>
 	</tr>
 	<tr>
-		<td><strong>@TR<<MAC>></strong></td>
+		<td><strong>@TR<<MAC>></strong></td><td>&nbsp;</td>
 		<td>$_mac</td>
 	</tr>
 EOF
@@ -34,39 +35,27 @@ EOF
 # board id checks go here.. todo: much work remains here
 #
 # loop is just used to avoid a jump
-while empty $board_type; do	
+while empty $device_type; do	
 	strings /dev/mtdblock/0 | grep 'W54G' 2>&1 >> /dev/null
 	if [ $? = "0" ]; then
- 		board_type="WRT54G"
- 		board_version="v??"
+ 		device_type="WRT54G"
+ 		device_version="v??"
  		break
 	fi	
 	break
 done
-empty $board_type && board_type="-id code not done for this board-";
+empty $device_type && device_type="-id code not done for this board-";
+board_type=$(cat /proc/cpuinfo | sed 2,20d | cut -c16-)
 
-?>
+?>	
 	<tr>
-		<td><strong>@TR<<Board>></strong></td><td> <? echo $board_type && ! empty $board_version && echo $board_version ?></td>
+		<td><strong>@TR<<Device>></strong></td><td>&nbsp;</td><td> <? echo $device_type && ! empty $device_version && echo $device_version ?></td>
+	</tr>		
+	<tr>
+		<td><strong>@TR<<Board>></strong></td><td>&nbsp;</td><td> <? echo $board_type ?></td>
 	</tr>
 	
 	<tr><td><br /><br /></td></tr>
-	<tr>
-		<th><b>@TR<<Statistics|CPU Info>></b></th>
-	</tr>
-	
-	<tr>
-		<td><pre><? cat /proc/cpuinfo ?></pre></td>
-	</tr>	
-	<tr><td><br /><br /></td></tr>
-	
-		<tr>
-		<th><b>@TR<<Statistics|Memory Usage>></b></th>
-	</tr>
-	<tr>
-		<td><pre><? cat /proc/meminfo ?></pre></td>
-	</tr>
-	<tr><td><br /></td></tr>
 </tbody>
 </table>
 
