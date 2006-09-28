53bb2b8bc351bd28086600dd4258b168b17d6cb8
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-28 16:20:04 +0000

    fix for the web address having a double /

--- trunk/package/webif/files/www/cgi-bin/webif/subcategories.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/subcategories.sh
@@ -3,7 +3,7 @@ subcategories() {
 	(
 	echo "#""#WEBIF:name:Graphs:1:CPU:cpu.sh"
 	cat /proc/net/dev | \
-	sed -n '/: *0/d;/:/{s/:.*//;s/^ *\(.*\)/##WEBIF:name:Graphs:2:Traffic \1:\/if.sh?if=\1/;p}'
+	sed -n '/: *0/d;/:/{s/:.*//;s/^ *\(.*\)/##WEBIF:name:Graphs:2:Traffic \1:\if.sh?if=\1/;p}'
 	) | \
 
 	awk -v "selected=$2" \
