f5661b7d40a50f3b0d349e057847ae7d956e9791
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-29 21:35:11 +0000

    remove useless WLAN help item since 2 help items don't fit well into 1 field

--- trunk/package/webif/files/www/cgi-bin/webif/network-wifi-lan.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/network-wifi-lan.sh
@@ -102,8 +102,6 @@ option|none|@TR<<Disable>>
 option|static|@TR<<Enable>>
 helpitem|Enable/Disable
 helptext|Helptext Enable/Disable#When this is enabled it will remove your wireless interface from the bridge with your switch and put it in its own bridge on a seperate subnet.
-helpitem|WLAN
-helptext|termtext WLAN#WLAN stands for "Wireless LAN".
 end_form
 start_form|@TR<<Wireless LAN Configuration>>|wifi_config|hidden
 field|@TR<<IP Address>>
