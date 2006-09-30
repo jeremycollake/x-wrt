d614c92fbc2a44a5753a0a5cf19fa1362647a713
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-30 03:54:57 +0000

    fix more leftover errors from webif pages rename

--- trunk/package/webif/files/www/cgi-bin/webif/subcategories.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/subcategories.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 subcategories() {
 	(
-	echo "#""#WEBIF:name:Graphs:1:CPU:cpu.sh"
+	echo "#""#WEBIF:name:Graphs:1:CPU:graphs-cpu.sh"
 	cat /proc/net/dev | \
-	sed -n '/: *0/d;/:/{s/:.*//;s/^ *\(.*\)/##WEBIF:name:Graphs:2:Traffic \1:\if.sh?if=\1/;p}'
+	sed -n '/: *0/d;/:/{s/:.*//;s/^ *\(.*\)/##WEBIF:name:Graphs:2:Traffic \1:\graphs-if.sh?if=\1/;p}'
 	) | \
 
 	awk -v "selected=$2" \
