a80615224fb61b79a2c7c738b83bfaf3da2e6746
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-09-30 04:27:20 +0000

    switch to <embed>

--- trunk/package/webif/files/www/cgi-bin/webif/graphs-if.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/graphs-if.sh
@@ -9,12 +9,12 @@
 header "Graphs" "Traffic $FORM_if" "Interface $FORM_if Traffic" "" "$SCRIPT_NAME"
 
 echo "<center>"
-#cat /proc/net/dev|sed -n '/:/{s/:.*//;s/^ *//;s/\(.*\)/<a href="\/cgi-bin\/webif\/if.sh?if=\1">\1<\/a>/p}'
+#cat /proc/net/dev|sed -n '/:/{s/:.*//;s/^ *//;s/\(.*\)/<a href="\/cgi-bin\/webif\/graphs-if.sh?if=\1">\1<\/a>/p}'
 ?>
 <br /><br />
 <?if [ "$FORM_if" ] ?>
-	<object data="/svggraph/graph_if.svg?/cgi-bin/webif/data.sh%3Fif%3D<? echo -n ${FORM_if} ?>" 
-		width="500" height="250"
+	<embed src="/svggraph/graph_if.svg?/cgi-bin/webif/data.sh%3Fif%3D<? echo -n ${FORM_if} ?>" 
+		width="500" height="250" type="image/svg+xml"
 	/>
 <?fi?>
 </center>
