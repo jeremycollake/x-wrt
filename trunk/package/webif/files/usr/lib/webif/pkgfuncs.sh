970e42ae5dfa2c5e3432061ee5cc5109a84d4c31
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-03 12:40:43 +0000

    isolate package funcs in case we ever need/want to only include them for pages they are actually used by.

---
+++ trunk/package/webif/files/usr/lib/webif/pkgfuncs.sh
@@ -0,0 +1,53 @@
+#
+# This file includes all functions applicable to
+# package management.
+#
+# 
+#
+
+is_package_installed() {
+	# $1 = package name
+	# returns 0 if package is installed.
+	
+	ipkg list_installed | grep "$1" >> /dev/null 2>&1	
+}
+
+install_package() {
+	# $1 = package name or URL
+	# returns 0 if success.
+	# if package is not found, and it isn't a URL, then it'll
+	# try an 'ipkg update' to see if it can locate it. Does
+	# emit output to std devices.	
+	ipkg install "$1"
+	! equal "$?" "0" &&
+	{		
+		echo "$1" | grep "://" >> /dev/null
+		! equal "$?" "0" && {
+			# wasn't a URL, so update
+			ipkg update
+			ipkg install "$1"
+		}			
+	}
+}
+
+remove_package() {
+	# $1 = package name
+	# returns 0 if success.
+	ipkg remove "$1"
+}
+
+update_package_list() {
+	ipkg update >> /dev/null
+}
+
+add_package_source() {
+	# $1 = new source
+	# this will not check for duplicates.
+	# for squashfs with symlink hack, rm first.
+	
+	local ipkgtmp=$(mktemp /tmp/.webif-XXXXXX)
+	cp "/etc/ipkg.conf" "$ipkgtmp"	
+	cat "$1" >> "$ipkgtmp"	
+	rm "/etc/ipkg.conf"
+	mv "$ipkgtmp" "/etc/ipkg.conf"	
+}
--- trunk/package/webif/files/usr/lib/webif/webif.sh
+++ trunk/package/webif/files/usr/lib/webif/webif.sh
@@ -4,6 +4,7 @@ cgidir=/www/cgi-bin/webif
 rootdir=/cgi-bin/webif
 indexpage=index.sh
 . /usr/lib/webif/configfuncs.sh
+. /usr/lib/webif/pkgfuncs.sh
 
 # workarounds for stupid busybox slowness on [ ]
 empty() {
@@ -270,52 +271,3 @@ is_bcm947xx() {
 }
 
 
-#####
-# functions dealing with packages
-#
-is_package_installed() {
-	# $1 = package name
-	# returns 0 if package is installed.
-	
-	ipkg list_installed | grep "$1" >> /dev/null 2>&1	
-}
-
-install_package() {
-	# $1 = package name or URL
-	# returns 0 if success.
-	# if package is not found, and it isn't a URL, then it'll
-	# try an 'ipkg update' to see if it can locate it. Does
-	# emit output to std devices.	
-	ipkg install "$1"
-	! equal "$?" "0" &&
-	{		
-		echo "$1" | grep "://" >> /dev/null
-		! equal "$?" "0" && {
-			# wasn't a URL, so update
-			ipkg update
-			ipkg install "$1"
-		}			
-	}
-}
-
-remove_package() {
-	# $1 = package name
-	# returns 0 if success.
-	ipkg remove "$1"
-}
-
-update_package_list() {
-	ipkg update >> /dev/null
-}
-
-add_package_source() {
-	# $1 = new source
-	# this will not check for duplicates.
-	# for squashfs with symlink hack, rm first.
-	
-	local ipkgtmp=$(mktemp /tmp/.webif-XXXXXX)
-	cp "/etc/ipkg.conf" "$ipkgtmp"	
-	cat "$1" >> "$ipkgtmp"	
-	rm "/etc/ipkg.conf"
-	mv "$ipkgtmp" "/etc/ipkg.conf"	
-}
