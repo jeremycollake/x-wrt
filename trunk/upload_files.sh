0d9d34d1aff86b76f566c300161195eeb81c2437
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-10 18:41:41 +0000

    change username to fictional name

--- trunk/upload_files.sh
+++ trunk/upload_files.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+echo " Uploading new X-Wrt Repository and Other Files ... "
 scp \
 	bin/packages/* \
-	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
+	someuser@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
 
 scp \
 	build_mipsel/webif-0.3/ipkg/webif/www/.version \
 	../../whale/xwrt/xwrt.htm \
 	../../whale/xwrt/xwrt.asp \
-	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
+	someuser@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
 
 scp patches/* \
-	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/patches/
+	someuser@shell.berlios.de:/home/groups/ftp/pub/xwrt/patches/
 
