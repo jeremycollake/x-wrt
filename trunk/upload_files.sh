e2c2b051016f6c7aa2364372e767485fc47422e2
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-10 08:10:15 +0000

    my personal scp upload script to our repository, to demonstrate how it should be updated

---
+++ trunk/upload_files.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+scp bin/packages/webif_0.3*.ipk \
+	bin/packages/* \
+	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
+
+scp \
+	build_mipsel/webif-0.3/ipkg/webif/www/.version \
+	../../whale/xwrt/xwrt.htm \
+	../../whale/xwrt/xwrt.asp \
+	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/packages/
+
+scp patches/* \
+	jcollake@shell.berlios.de:/home/groups/ftp/pub/xwrt/patches/
+
