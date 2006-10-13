1bc9065ec18ee1344808e62450a19459f156ada0
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-13 20:06:50 +0000

    added buttons to install either Rudy's QoS scripts or Nbd's QoS scripts - shown when neither is found.

--- trunk/package/webif/files/www/cgi-bin/webif/network-qos.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/network-qos.sh
@@ -5,20 +5,46 @@
 
 header "Network" "QoS" "@TR<<QOS Configuration>>" ' onLoad="modechange()" ' "$SCRIPT_NAME"
 
-if is_package_installed "qos-fw"; then
-#
-# nbd's QoS scripts
-#	
-	echo "nbd QoS scripts found installed. We haven't written code yet for this."
+if ! empty "$FORM_install_rudy"; then	
+	echo "Installing Rudy's QoS scripts (HSFC) ...<pre>"	
+	! install_package "qos-re" && {
+		install_package "http://ftp.berlios.de/pub/xwrt/packages/qos-re-hfsc_1.02_all.ipk"	
+	}
+	echo "</pre>"
+fi
+
+if ! empty "$FORM_install_nbd"; then	
+	echo "Installing Nbd's QoS scripts ...<pre>"	
+	# assume X-Wrt repository available
+	! install_package "qos-scripts" && {
+		install_package "http://ftp.berlios.de/pub/xwrt/packages/qos-scripts_0.9.1-1_mipsel.ipk"
+	}
+	echo "</pre>"
+fi
+
+if is_package_installed "qos-scripts"; then
+	#
+	# nbd's QoS scripts
+	#	
+	echo "nbd's QoS scripts found installed. We haven't written code yet for this."
 	
 elif is_package_installed "qos-re"; then
-#
-# Rudy's QoS scripts
-#
+	#
+	# Rudy's QoS scripts
+	#
 . ./qos-rudy.inc
-
 else
-	echo "A compatible QOS package was not found to be installed."
+	echo "<div class=\"warning\">A compatible QOS package was not found to be installed. Currently this page supports Rudy's QoS scripts.</div>"	
+	
+display_form <<EOF
+onchange|modechange
+start_form|@TR<<QoS Packages>>
+field|Rudy's QoS Scripts|rudy_qos
+submit|install_rudy|Install
+field|Nbd's QoS Scripts (unsupported by webif)|nbd_qos
+submit|install_nbd|Install
+end_form
+EOF
 fi
 
 footer ?>
