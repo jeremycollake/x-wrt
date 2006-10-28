df208c9ab35eead72191d6c31243f9526321d8d9
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-28 15:35:06 +0000

    add wan mac address to wan status form

--- trunk/package/webif/files/www/cgi-bin/webif/status-interfaces.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/status-interfaces.sh
@@ -4,17 +4,20 @@
 header "Status" "Interfaces" "@TR<<Interfaces Status>>"
 
 # get WAN IP
-wan_ip_addr=$(ifconfig 2>&1 | grep -A 1 "`nvram get wan_ifname`" | grep "inet addr" | cut -d: -f 2 | sed s/Bcast//g)
+wan_config=$(ifconfig 2>&1 | grep -A 1 "`nvram get wan_ifname`")
+wan_ip_addr=$(echo "$wan_config" | grep "inet addr" | cut -d: -f 2 | sed s/Bcast//g)
+wan_mac_addr=$(echo "$wan_config" | grep -A 1 "`nvram get wan_ifname`" | grep "HWaddr" | cut -d' ' -f 10)
 
 # enumerate WAN nameservers
 form_dns_servers=$(awk '
 	BEGIN { counter=1 }
 	/nameserver/ {print "field|@TR<<DNS Server>> " counter "|dns_server_" counter "\n string|" $2 "\n" ;counter+=1}
 	' /etc/resolv.conf)
-
  
 display_form <<EOF
 start_form|@TR<<WAN Status>>
+field|@TR<<MAC Address>>|wan_mac_addr
+string|$wan_mac_addr
 field|@TR<<IP Address>>|wan_ip_addr
 string|$wan_ip_addr
 $form_dns_servers
