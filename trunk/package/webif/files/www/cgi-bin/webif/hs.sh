85ac86b4670edcd3d34b641cdfde79e6f1f867f0
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-10-29 21:46:15 +0000

    add stub page for hotspot category prompting install of hswebif

---
+++ trunk/package/webif/files/www/cgi-bin/webif/hs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/webif-page
+<? 
+. /usr/lib/webif/webif.sh
+header "HotSpot" "Install" "HotSpot Config $HS_USING" ' onLoad="modechange()" ' "$SCRIPT_NAME"
+has_pkgs hswebif
+
+footer ?>
+<!--
+##WEBIF:name:HotSpot:1:Install
+-->
