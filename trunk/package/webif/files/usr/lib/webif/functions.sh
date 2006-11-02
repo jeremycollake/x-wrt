32847cf718ec02dff4a858b1faf4dadc40356b8a
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-02 05:14:52 +0000

    remove compatibility uci config funcs and add depreciated note with pointer to new funcs in /lib/config/uci.sh

--- trunk/package/webif/files/usr/lib/webif/functions.sh
+++ trunk/package/webif/files/usr/lib/webif/functions.sh
@@ -42,98 +42,20 @@ remove_lines_from_file() {
 }
 
 #############################################################################
-# Configuration update functions
-#
-# Copyright (C) 2006 by Felix Fietkau <nbd@openwrt.org>
-# Lame comments and hacks by Jeremy Collake.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
-# to be used in webif.sh
-#
-
-#############################################################################
 # UCI config functions
 # 
-#  These work with UCI compatibile configuration files. These files are 
-#  stored in /etc/config.
+# !!! These are deprecated. See /lib/config/uci.sh. !!!
 #
 #############################################################################
 
-#
-# load_settings_ex loads all settings from a UCI config file to shell
-# variables. Use 'env' after loading to get a feel for how they are named.
-#
 load_settings_ex() {
-	# $1 = file
-	config_load "$1"	
-	exists /tmp/.webif-uci/config-$1 && . /tmp/.webif-uci/config-$1
 }
 
-#
-# save_settings_ex saves a setting to an intermediate (temporary)
-# UCI config file. To persist changes to the final UCI config file, 
-# call commit_settings_ex.
-#
 save_setting_ex() {
-	# $1 = file
-	# $2 = group
-	# $3 = name
-	# $4 = value
-	mkdir -p /tmp/.webif-uci
-	cat >> /tmp/.webif-uci/config-$1 <<EOF
-config update "$2"
-	option "$3" "$4"
-EOF
 }
 
-#
-# commit_settings_ex applies all pending config changes from save_setting_ex.
-#
-commit_settings_ex() {(	
-	reset_cb
-	option_cb() {
-		# FIXME: ';' cannot be used in any config values
-		updatestr="${updatestr:+$updatestr;}$CONFIG_SECTION.$1=$2"
-	}
-
-	for cfg in /tmp/.webif-uci/config-*; do
-		exists $cfg || continue
-		export cfgfile="${cfg##*config-}"
-		export updatestr=
-		. $cfg
-		
-		lock /tmp/.webif-uci/update-$cfgfile
-		awk \
-			-v cfgfile="/etc/config/$cfgfile" \
-			-v updatestr="$updatestr" \
-			-f /lib/config/uci-update.awk \
-			-f - > /etc/config/$cfgfile.new <<EOF
-BEGIN {
-	cfg = read_file(cfgfile)
-	print update_config(cfg, updatestr)
+commit_settings_ex() {
 }
-EOF
-		equal "$?" 0 && {
-			mv /etc/config/$cfgfile.new /etc/config/$cfgfile
-			rm -f /tmp/.webif-uci/config-$cfgfile
-		}
-		lock -u /tmp/.webif-uci/update-$cfgfile
-	done
-)}
-
 
 #############################################################################
 # Original config functions
