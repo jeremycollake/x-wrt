750dffd1e9f56c15fe510c7f8351aff0508b8f63
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-02 02:46:54 +0000

    add some parameter and synopsis docs to UCI config functions.

--- trunk/package/webif/files/usr/lib/webif/functions.sh
+++ trunk/package/webif/files/usr/lib/webif/functions.sh
@@ -1,6 +1,8 @@
+#############################################################################
 # Configuration update functions
 #
 # Copyright (C) 2006 by Felix Fietkau <nbd@openwrt.org>
+# Lame comments and hacks by Jeremy Collake.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,16 +20,36 @@
 #
 # to be used in webif.sh
 #
+. /etc/functions_ex.sh
 
-######
-# New settings functions
+#############################################################################
+# New UCI config functions
+# 
+#  These work with UCI compatibile configuration files. These files are 
+#  stored in /etc/config.
+#
 
+
+#
+# load_settings_ex loads all settings from a UCI config file to shell
+# variables. Use 'env' after loading to get a feel for how they are named.
+#
 load_settings_ex() {
+	# $1 = file
 	config_load "$1"
 	exists /tmp/.webif/config-$1 && . /tmp/.webif/config-$1
 }
 
+#
+# save_settings_ex save a setting to an intermediate (temporary)
+# UCI config file. To persist changes to the final UCI config file, 
+# call commit_settings_ex.
+#
 save_setting_ex() {
+	# $1 = file
+	# $2 = group
+	# $3 = name
+	# $4 = value
 	mkdir -p /tmp/.webif
 	cat >> /tmp/.webif/config-$1 <<EOF
 config update "$2"
@@ -35,6 +57,9 @@ config update "$2"
 EOF
 }
 
+#
+# commit_settings_ex applies all pending config changes from save_setting_ex.
+#
 commit_settings_ex() {(
 	reset_cb
 	option_cb() {
@@ -68,8 +93,12 @@ EOF
 )}
 
 
-######
-# Original settings functions
+#############################################################################
+# Original config functions
+#
+#  These work with as a tuple based configuration system. apply.sh applies
+#  the changes.
+#
 
 load_settings() {
 	equal "$1" "nvram" || {
@@ -88,6 +117,9 @@ validate() {
 
 
 save_setting() {
+	# $1 = group
+	# $2 = name
+	# $3 = value
 	exists /tmp/.webif/* || mkdir -p /tmp/.webif
 	oldval=$(eval "echo \${$2}")
 	oldval=${oldval:-$(nvram get "$2")}
@@ -99,7 +131,11 @@ save_setting() {
 	equal "$oldval" "$3" || echo "$2=\"$3\"" >> /tmp/.webif/config-$1
 }
 
-# misc. functions
+
+#############################################################################
+# Misc. functions - todo: move elsewhere as they are not used by above.
+#
+#
 remove_lines_from_file() {
 	# $1=filename
 	# $2=substring in lines to remove
