077db8dc49ec045b104c92648351659453eaeb59
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-07 02:21:45 +0000

    add support for reading integer input(s) from stdin if not supplied on command line.

--- trunk/package/int2human/main.c
+++ trunk/package/int2human/main.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "human_readable.h"
 
 void usage() 
@@ -10,21 +11,57 @@ void usage()
 	printf("Usage: int2human integer1 integer2 integer3 ...\n\n"
 		" Where integerX is an integer to convert to human readable form. If multiple\n"
 		" integers are supplied, the output is delimited by a space. Currently this\n"
-		" program is limited to integers the width of the system's unsigned long.\n\n");
+		" program is limited to integers the width of the system's unsigned long. If no\n"
+		" command line arguments are supplied, stdin is used.\n\n");
 }
+
+#define LAME_STATIC_SIZE 4096 	/* be very lazy and limit stdin input to 4KB */
 
 int main(int argc, char **argv)
 {
+	int nUseStdin=0;
+	char *pszInputText;
+	int nI;
 	if(argc<2)
 	{
-	 	usage();
-		exit(1);
+		pszInputText=(char *)malloc(LAME_STATIC_SIZE+1);
+		if(!fgets(pszInputText,LAME_STATIC_SIZE,stdin))
+		{
+			usage();
+			exit(1);
+		}		
 	}
-	int nI;
-	for(nI=1;nI<argc;nI++)
+	else
 	{
-		printf("%sB ", make_human_readable_str(strtoul(argv[nI], NULL, 10), 1,  0));
+		/* calculate required buffer size and allocate */
+		int nReqLen=1;  /* require at least null terminator */
+		for(nI=1; nI<argc;nI++)
+		{
+			if(!strcmp(argv[nI],"--?")) 
+			{
+				usage();
+				exit(1);
+			}
+			nReqLen+=strlen(argv[nI])+1; /* extra for delimeter */
+		}
+		pszInputText=(char *)malloc(nReqLen);
+		pszInputText[0]=0;
+		for(nI=1; nI<argc;nI++)
+		{			
+			strcat(pszInputText,argv[nI]);
+			strcat(pszInputText," ");
+		}
 	}
-	printf("\n");			
+	char *p;
+	for(p=pszInputText; *p; )
+	{
+		char *pS=p;
+		/* throw a null at end of field */
+		for(;*p!=0x20 && *p; p++);
+		*p++=0;
+		printf("%sB ", make_human_readable_str(strtoul(pS, NULL, 10), 1,  0));
+	}
+	free(pszInputText);
+	printf("\n");				
 	exit(0);
 }
