45d2072f95ac771b4665dffb53684cf9ed3934ee
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-24 17:09:33 +0000

    add a 'build-date.txt' file to indicate build date of firmware images and change path from 'rc6' to 'whiterussian-latest'

--- trunk/upload_files.sh
+++ trunk/upload_files.sh
@@ -4,6 +4,7 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 export SCP_USER="$1"
+rm bin/packages/w?bf??e*
 chmod -R 775 bin/*
 chmod 775 build_mipsel/webif-0.3/ipkg/webif/www/.version
 echo "Uploading webif ..."
@@ -34,45 +35,49 @@ scp \
 	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/patches/
 
 echo "Uploading firmware images ..."
+date > /tmp/build-date.txt
+scp \
+	/tmp/build-date.txt \
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/default/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.7z \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/default/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/default/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.bin \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/default/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/default/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.trx \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/default/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/default/
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.7z \
-#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/micro/
+#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/micro/
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.bin \
-#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/micro/
+#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/micro/
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.trx \
-#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/micro/
+#	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/micro/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.7z \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pppoe/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pppoe/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.bin \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pppoe/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pppoe/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.trx \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pppoe/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pppoe/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.7z \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pptp/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pptp/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.bin \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pptp/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pptp/
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.trx \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/pptp/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/pptp/
 echo "Uploading SDk and image builder ..."
 scp \
 	bin/*.tar.bz2 \
-	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/rc6/
+	$SCP_USER@shell.berlios.de:/home/groups/ftp/pub/xwrt/images/white-russian-latest/
 
 
