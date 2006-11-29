c8949492081fa720f51e907d785ea991fe17e41c
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-29 21:18:27 +0000

    some ez-ipupdate fixes thanks kode54

--- trunk/package/webif/files/usr/lib/webif/apply.sh
+++ trunk/package/webif/files/usr/lib/webif/apply.sh
@@ -19,6 +19,7 @@ HANDLERS_config='
 	pptp) reload_pptp;;
 	log) reload_log;;
 	upnpd) reload_upnpd;;
+	ex-ipupdate) reload_ex-ipupdate;;
 
 '
 HANDLERS_file='
@@ -123,6 +124,45 @@ reload_system() {
 reload_upnpd() {
 	echo '@TR<<Reloading>> @TR<<UPNPd>> ...'
 	/etc/init.d/S95miniupnpd
+}
+
+reload_ex-ipupdate() {
+#!/bin/sh
+initfile="/etc/init.d/S52ez-ipupdate"
+
+ddns_dir="/etc/ez-ipupdate"
+ddns_cache="$ddns_dir/ez-ipupdate.cache"
+ddns_conf="$ddns_dir/ez-ipupdate.conf"
+ddns_msg="$ddns_dir/ez-ipupdate.msg"
+
+ddns_enable=$(nvram get ddns_enable)
+ddns_service_type=$(nvram get ddns_service_type)
+ddns_username=$(nvram get ddns_username)
+ddns_passwd=$(nvram get ddns_passwd)
+ddns_hostname=$(nvram get ddns_hostname)
+ddns_server=$(nvram get ddns_server)
+ddns_max_interval=$(nvram get ddns_max_interval)
+
+# (re)start ez-ipupdated
+if [ "$ddns_enable" -eq "1" ]; then
+	mkdir -p $ddns_dir
+	echo "service-type=$ddns_service_type"   > $ddns_conf
+	echo "user=$ddns_username:$ddns_passwd" >> $ddns_conf
+	echo "host=$ddns_hostname"              >> $ddns_conf
+	[ -z "$ddns_server"       ] ||  echo "server=$ddns_server"             >> $ddns_conf
+	[ -z "$ddns_max_interval" ] ||  echo "max-interval=$ddns_max_interval" >> $ddns_conf
+
+	#[ -f $ddns_cache ] && rm -f  $ddns_cache
+
+	[ -f $ddns_cache ] && rm -f $ddns_msg
+	echo "(Re)start DynDNS ez-ipupdate" > $ddns_msg
+	echo "(Re)start ez-ipupdate..."
+
+	$initfile restart
+else
+	#echo "Stop ez-ipupdate..."
+	$initfile stop >&- 2>&-
+fi
 }
 
 mkdir -p "/tmp/.webif"
@@ -251,6 +291,7 @@ reload_log() {
 	killall syslogd >&- 2>&- <&-
 	/sbin/runsyslogd >&- 2>&- <&-
 }
+
 
 # config-*		simple config files
 (
