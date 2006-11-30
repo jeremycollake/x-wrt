0521ab9331df9669d10df12caff6a35520a4074d
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-11-30 01:07:44 +0000

    rm -rf imgbuild/ prior to extract image builder. this was why i saw errors in miniupnpd.postinst, an old package was stuck in there and being used (the image builder apparently does an 'ls ' and grabs the first matching file it sees.

--- trunk/mkimages.sh
+++ trunk/mkimages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # use the image builder to make X-Wrt images.
 echo "Making firmware images ..."
+rm -rf imgbuild
 mkdir -p ./imgbuild
 cd imgbuild
 tar -xjf ../bin/OpenWrt-ImageBuilder-Linux-i686.tar.bz2 
