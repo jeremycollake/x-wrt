ebfc4cb38a3e77db077e380afe6032b32b4c11c2
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-01 00:52:12 +0000

    optimize script and fix newlines.

--- trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
+++ trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 languages_lst="/usr/lib/webif/languages.lst"
-langs=$(echo "option|en|English" && ipkg list | awk '/webif-lang/ { gsub("webif-lang-",""); print "option|"$1"|"$5}')
-langs=$(echo "$langs" | sort | uniq)
+tmplanglst=$(mktemp "/tmp/.webif-XXXXXX")
+echo "option|en|English" && ipkg list | awk '/webif-lang/ { gsub("webif-lang-",""); print "option|"$1"|"$5}' | sort | uniq > "$tmplanglst"
 rm -f "$languages_lst"
-echo "$langs" > "$languages_lst"
+mv -f "$tmplanglst" "$languages_lst"
+
