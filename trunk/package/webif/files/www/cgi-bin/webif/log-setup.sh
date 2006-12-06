70a4a1f0ef4251c60d3ce75292b29553de29585f
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-06 01:10:57 +0000

    @TR'ize

--- trunk/package/webif/files/www/cgi-bin/webif/log-setup.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/log-setup.sh
@@ -61,8 +61,8 @@ EOF
 
 display_form <<EOF
 onchange|modechange
-start_form|Remote Syslog
-field|Server IP Address
+start_form|@TR<<Remote Syslog>>
+field|@TR<<Server IP Address>>
 text|ipaddr|$FORM_ipaddr
 helpitem|Remote Syslog
 helptext|Remote Syslog#IP address and port of the remote logging host. Leave this address blank for no remote logging. The port is set to $DEFAULT_log_port by default
@@ -71,14 +71,14 @@ text|log_port|$FORM_log_port
 end_form
 
 start_form|Syslog Marks
-field|Minutes Between Marks
+field|@TR<<Minutes Between Marks>>
 text|log_mark|$FORM_log_mark
 helpitem|Syslog Marks
 helptext|Syslog Marks#Periodic marks in your log. This parameter sets the time in minutes between the marks. A value of 0 means no mark. Default value: $DEFAULT_log_mark.
 end_form
 
 start_form|@TR<<Local Log>>
-field|Log type
+field|@TR<<Log type>>
 select|type|$FORM_type
 option|circular|@TR<<Circular>>
 option|file|@TR<<File>>
@@ -88,7 +88,7 @@ field|@TR<<Log File>>|logname|hidden
 text|filename|$FORM_filename
 helpitem|Log File
 helptext|Log File#The path and name of your log file. It can be set on any writable filesystem. CAUTION: DO NOT USE A JFFS filesystem because syslog will write A LOT to it. You can use /tmp or any filesystem on an external storage unit. Default value: $DEFAULT_log_file.
-field|Log Size
+field|@TR<<Log Size>>
 text|size|$FORM_size
 helpitem|Log Size
 helptext|Log Size#The size of your log in kilo-bytes. Be carefull with the size of the circular buffer as it is taken from your main memory. Default value: $DEFAULT_log_size kB.
