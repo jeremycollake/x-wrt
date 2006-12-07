a26798cbf9bceab9a31c0e150715020b124738a8
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-07 23:51:46 +0000

    kamikaze and white russian branch synchronize

--- kamikaze/package/webif/files/www/cgi-bin/webif/status-usb.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/status-usb.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/webif-page
 <?
+#
+# This page is synchronized between kamikaze and WR branches. Changes to it *must* 
+# be followed by running the webif-sync.sh script.
+#
 . /usr/lib/webif/webif.sh
 header "Status" "USB" "USB Devices"
-ShowNotUpdatedWarning
 ?>
 <table style="width: 90%; " border="0" cellpadding="2" cellspacing="2" align="center">
 <tbody>
@@ -10,7 +13,7 @@ ShowNotUpdatedWarning
 	<tr><th><b>All connected devices (excluding system hubs)</b></th></tr>
 	<tr><td><table border=1>
 	<col style=\"text-align:center;\"><col align=center><col><col><col align=center><col align=center>
-	<tr style="background:#c0c0c0;"><th>Bus</th><th>Device</th><th>Product</th><th>Manufacturer</th><th>VendorID:ProdID</th><th>USB version</th></tr>
+	<tr><th>Bus</th><th>Device</th><th>Product</th><th>Manufacturer</th><th>VendorID:ProdID</th><th>USB version</th></tr>
 	<?
 	[ -f /proc/bus/usb/devices ] && grep -e "^[TDPS]:" /proc/bus/usb/devices | sed 's/[[:space:]]*=[[:space:]]*/=/g' | sed 's/[[:space:]]\([^ |=]*\)=/|\1=/g' | sed 's/^/|/' | awk '
 	BEGIN { i=0; RS="|"; FS="=";}
