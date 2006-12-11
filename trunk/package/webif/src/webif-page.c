d73aef82d2f5ba7fd73b5727a59f538a4c4ff94c
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-11 05:15:00 +0000

    increase max line size and hash table size.

--- trunk/package/webif/src/webif-page.c
+++ trunk/package/webif/src/webif-page.c
@@ -24,9 +24,9 @@
 #include <bcmnvram.h>
 #endif
 
-#define HASH_MAX	512
-#define LINE_BUF	1024 /* max. buffer allocated for one line */
-#define MAX_TR		32	 /* max. translations done on one line */
+#define HASH_MAX	1536
+#define LINE_BUF	2048	/* max. buffer allocated for one line */
+#define MAX_TR		32	/* max. translations done on one line */
 #define TR_START	"@TR<<"
 #define TR_END		">>"
 
