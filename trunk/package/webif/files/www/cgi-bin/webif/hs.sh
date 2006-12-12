6db96915e8caf18a4e85ab6780447b89fad6b80c
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-12 06:32:04 +0000

    minor

--- trunk/package/webif/files/www/cgi-bin/webif/hs.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/hs.sh
@@ -7,8 +7,10 @@ header "HotSpot" "Configuration" "HotSpot Management" ' onLoad="modechange()" ' 
 if ! empty "$FORM_submit" && ! empty "$FORM_install_hswebif"; then
 	# install_package uses -force-overwrite, which will be necessary for hswebif
 	# over-write this very file.
-	install_package "hswebif"
-	echo "<br />You should now refresh this page."
+	! install_package "hswebif" && {
+		echo "<br /><div class=\"warning\">Error installing package!</div>"
+	}
+	echo "<br />Package installed successfully! You should now refresh this page."
 else
 	display_form <<EOF
 	start_form|Hotspot Webif Extension Pack
