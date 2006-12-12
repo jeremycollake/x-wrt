170592ec5a9e4925932c5ebdc4ec78226bfb4ee2
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-12 06:45:22 +0000

    disable html in log-browse_frame until we figure out why the raw html gets shown sometimes.

--- trunk/package/webif/files/www/cgi-bin/webif/log-read_frame.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/log-read_frame.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/webif-page
 <?
-colorize_script=""
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-	<title>@TR<<Syslog Messages>></title>
-	<link rel="stylesheet" type="text/css" href="/webif.css" />
-	<!--[if lt IE 7]>
-		<link rel="stylesheet" type="text/css" href="/ie_lt7.css" />
-	<![endif]-->
-	<meta http-equiv="Content-Type" content="text/html; charset=@TR<<Encoding|ISO-8859-1>>" />
-	<meta http-equiv="expires" content="-1" />
-	<script type="text/javascript" src="/colorize.js"></script>
-		<script type="text/javascript"> colorize(); </script>
-</head>
-<body $4>
-<div class="logread"><pre>
-<?
 . /usr/lib/webif/webif.sh
 . /etc/runsyslogd.conf
+colorize_script=""
 
 prefix=$(nvram get log_prefix)
 LOG_TYPE=$(nvram get log_type)
@@ -32,6 +15,3 @@ fi
 
 $LOGREAD | sort -r  | sed -e "s| $prefix| |"
 ?>
-</pre></div>
-</body>
-</html>
