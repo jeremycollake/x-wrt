bebd312b136c6b61aa2aa633a2347591a7227b7a
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2006-12-20 13:38:22 +0000

    kamikaze and white russian branch synchronize

--- kamikaze/package/webif/files/usr/lib/webif/pkgfuncs.sh
+++ kamikaze/package/webif/files/usr/lib/webif/pkgfuncs.sh
@@ -2,9 +2,9 @@
 # This file includes all functions applicable to
 # package management.
 #
+# (c) 2006 Jeremy Collake - X-Wrt Project
 #
 #
-
 is_package_installed() {
 	# $1 = package name
 	# returns 0 if package is installed.
@@ -18,14 +18,14 @@ install_package() {
 	# if package is not found, and it isn't a URL, then it'll
 	# try an 'ipkg update' to see if it can locate it. Does
 	# emit output to std devices.
-	ipkg install "$1" -force-overwrite
+	ipkg install "$1" -force-overwrite -force-defaults | uniq
 	! equal "$?" "0" &&
 	{
 		echo "$1" | grep "://" >> /dev/null
 		! equal "$?" "0" && {
 			# wasn't a URL, so update
 			ipkg update
-			ipkg install "$1" -force-overwrite
+			ipkg install "$1" -force-overwrite -force-defaults | uniq
 		}
 	}
 }
@@ -52,25 +52,20 @@ add_package_source() {
 	mv "$ipkgtmp" "/etc/ipkg.conf"
 }
 
-pcnt=0
-nothave=0
-_savebutton_bk=""
-
 has_pkgs() {
-	retval=0;
+	local pcnt=0
+	local nothave=0
+	local retval=0;
 	for pkg in "$@"; do
 		pcnt=$((pcnt + 1))
 		empty $(ipkg list_installed | grep "^$pkg ") && {
-			echo -n "<p>Features on this page require the \"<b>$pkg</b>\" package. &nbsp;<a href=\"/cgi-bin/webif/ipkg.sh?action=install&pkg=$pkg&prev=$SCRIPT_NAME\">install now</a>.</p>"
+			echo -n "<p>@TR<<Features on this page require the>> \"<b>$pkg</b>\" @TR<<package>>. &nbsp;<a href=\"/cgi-bin/webif/ipkg.sh?action=install&pkg=$pkg&prev=$SCRIPT_NAME\">@TR<<install now>></a>.</p>"
 			retval=1;
 			nothave=$((nothave + 1))
 		}
-	done
-	[ -z "$_savebutton_bk" ] && _savebutton_bk=$_savebutton
+	done	
 	if [ "$pcnt" = "$nothave" ]; then
 		_savebutton=""
-	else
-		_savebutton=$_savebutton_bk
 	fi
 	return $retval;
 }
