712e8abcd746479ab0e00a3511939c307bebcd55
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-23 22:02:52 +0000

    fix trash output when in reload_ezipupdate when ez-ipupdate package not installed

--- trunk/package/webif/files/usr/lib/webif/apply.sh
+++ trunk/package/webif/files/usr/lib/webif/apply.sh
@@ -140,42 +140,43 @@ reload_upnpd() {
 
 reload_ezipupdate() {
 	initfile="/etc/init.d/S52ez-ipupdate"
+	exists "$initfile" && {
+		ddns_dir="/etc/ez-ipupdate"
+		ddns_cache="$ddns_dir/ez-ipupdate.cache"
+		ddns_conf="$ddns_dir/ez-ipupdate.conf"
+		ddns_msg="$ddns_dir/ez-ipupdate.msg"
 
-	ddns_dir="/etc/ez-ipupdate"
-	ddns_cache="$ddns_dir/ez-ipupdate.cache"
-	ddns_conf="$ddns_dir/ez-ipupdate.conf"
-	ddns_msg="$ddns_dir/ez-ipupdate.msg"
+		if exists "/usr/sbin/nvram"; then
+			ddns_enable=$(nvram get ddns_enable)
+			ddns_service_type=$(nvram get ddns_service_type)
+			ddns_username=$(nvram get ddns_username)
+			ddns_passwd=$(nvram get ddns_passwd)
+			ddns_hostname=$(nvram get ddns_hostname)
+			ddns_server=$(nvram get ddns_server)
+			ddns_max_interval=$(nvram get ddns_max_interval)
+		else
+			echo "ERROR: ez-ipupdate config apply not updated for non-nvram systems."
+		fi
 
-	if exists "/usr/sbin/nvram"; then
-		ddns_enable=$(nvram get ddns_enable)
-		ddns_service_type=$(nvram get ddns_service_type)
-		ddns_username=$(nvram get ddns_username)
-		ddns_passwd=$(nvram get ddns_passwd)
-		ddns_hostname=$(nvram get ddns_hostname)
-		ddns_server=$(nvram get ddns_server)
-		ddns_max_interval=$(nvram get ddns_max_interval)
-	else
-		echo "ERROR: ez-ipupdate config apply not updated for non-nvram systems."
-	fi
+		# (re)start ez-ipupdated
+		if [ "$ddns_enable" -eq "1" ]; then
+			mkdir -p $ddns_dir
+			echo "service-type=$ddns_service_type"   > $ddns_conf
+			echo "user=$ddns_username:$ddns_passwd" >> $ddns_conf
+			echo "host=$ddns_hostname"              >> $ddns_conf
+			[ -z "$ddns_server"       ] ||  echo "server=$ddns_server"             >> $ddns_conf
+			[ -z "$ddns_max_interval" ] ||  echo "max-interval=$ddns_max_interval" >> $ddns_conf
 
-	# (re)start ez-ipupdated
-	if [ "$ddns_enable" -eq "1" ]; then
-		mkdir -p $ddns_dir
-		echo "service-type=$ddns_service_type"   > $ddns_conf
-		echo "user=$ddns_username:$ddns_passwd" >> $ddns_conf
-		echo "host=$ddns_hostname"              >> $ddns_conf
-		[ -z "$ddns_server"       ] ||  echo "server=$ddns_server"             >> $ddns_conf
-		[ -z "$ddns_max_interval" ] ||  echo "max-interval=$ddns_max_interval" >> $ddns_conf
-
-	#[ -f $ddns_cache ] && rm -f  $ddns_cache
+		#[ -f $ddns_cache ] && rm -f  $ddns_cache
 
 		[ -f $ddns_cache ] && rm -f $ddns_msg
-		echo "(Re)start DynDNS ez-ipupdate" > $ddns_msg
+			echo "(Re)start DynDNS ez-ipupdate" > $ddns_msg
 		
-		$initfile restart >&- 2>&- &
-	else		
-		$initfile stop >&- 2>&- &
-	fi
+			$initfile restart >&- 2>&- &
+		else		
+			$initfile stop >&- 2>&- &
+		fi
+	}
 }
 
 mkdir -p "/tmp/.webif"
