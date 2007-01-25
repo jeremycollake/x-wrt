2ecea467dba79b92c2849c0fd1b18ab4f22adf07
dmytro <dmytro@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-25 16:43:27 +0000

    Some more synchronization with whiterussian

--- trunk/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
@@ -4,17 +4,14 @@
 # Wireless survey page
 #
 # Description:
-#	Perform a wireless survey and display pretty results.
+#        Perform a wireless survey and display pretty results.
 #
 # Author(s) [in order of work date]:
-#	Jeremy Collake <jeremy.collake@gmail.com>
+#        Jeremy Collake <jeremy.collake@gmail.com>
+#        Dmytro Dykhman <dmytro@iroot.ca>
 #
 # Major revisions:
 #
-# NVRAM variables referenced:
-#	wl0_ifname
-#	wl0_mode
-#	wl0_infra  (unnecessary to be 1? -- seems so in tests so far)
 #
 # Configuration files referenced:
 #   none
@@ -25,181 +22,202 @@
 #   and probably more efficient to use awk. Maybe recode someday, but
 #   why fix what's isn't broken...
 #
-#
-#
 
 . "/usr/lib/webif/webif.sh"
-header "Status" "Site Survey" "@TR<<Wireless survey>>"
+header "Status" "Site Survey" "@TR<<<img src=/images/wscan.jpg align=absmiddle>&nbsp;Wireless survey>>"
 ?>
 
 <?
+     if [ -s "/bin/wl" ] ; then
+
+     echo "OK, good to go"
+
+     else
+         echo "no wl package found"
+     exit
+     fi
+
+
+
+
+
+
 MAX_TRIES=4
 MAX_CELLS=100
-WL0_IFNAME=$(nvram get wl0_ifname)
-##################################################
-# Handle switch to sta mode at user request
-#
-if empty "$FORM_clientswitch"; then
-	CLIENT_SWITCH_BUTTON="<form enctype=\"multipart/form-data\" method=\"post\"><input type=\"submit\" value=\" @TR<<Scan>> \" name=\"clientswitch\" /></form>"
-else
-	ORIGINAL_WL_MODE=$(nvram get wl0_mode)
-	nvram set wl0_mode="sta"
-	# tests show scan works in infra or ad-hoc mode, but we'll do this to be safe
-	ORIGINAL_INFRA=$(nvram get wl0_infra)
-	nvram set wl0_infra="1"
-	wifi up 2>/dev/null >/dev/null </dev/null
-fi
-WL_MODE=$(nvram get wl0_mode)
-?>
-<table style="width: 90%; text-align: center;" border="0" cellpadding="2" cellspacing="2">
-<tbody>
-<?
-##################################################
-#
-if equal $WL_MODE "ap" ; then
-	echo "<table><tbody><tr><td>@TR<<HelpText WLAN Survey#Your wireless adaptor is not in client mode. To do a scan it must be put into client mode for a few seconds. Your WLAN traffic will be interrupted during this brief period.>>" \
-	"<tr><td><br /></td></tr><tr><td>$CLIENT_SWITCH_BUTTON</td></tr></tbody></table>"
-else
+Wimg=0
 
 tempfile=$(mktemp /tmp/.survtemp.XXXXXX)
 tempfile2=$(mktemp /tmp/.survtemp.XXXXXX)
+tempscan=$(mktemp /tmp/.survscan.XXXXXX)
 
-#echo " Please wait while scan is performed ... <br /><br />"
+##################################################
+#
+Dopurge ()
+{
+
+        sed 1d < $tempfile > $tempfile2
+        rm $tempfile
+        mv $tempfile2 $tempfile     
+}
+
 counter=0
+
 for counter in $(seq 1 $MAX_TRIES); do
-	#echo "."
-	iwlist scan > $tempfile 2> /dev/null
-	grep -i "Address" < $tempfile >> /dev/null
-	equal "$?" "0" && break
-	sleep 1
+       wl scan
+       wl scanresults > $tempscan 2> /dev/null
+        if equal $(sed '2,$ d' $tempscan | cut -c0-4) "SSID" ; then break ; fi
+       sleep 1
 done
 
-first_hit=1
 if [ $counter -gt $MAX_TRIES ]; then
-	echo "<tr><td>@TR<<Sorry, no scan results.>></td></tr>"
+        echo "<tr><td>@TR<<Sorry, no scan results.>></td></tr>"
 else
-	current=0
-	counter=0
-	for counter in $(seq 1 $MAX_CELLS); do
-		current_line=$(sed '2,$ d' < $tempfile)
-		empty "$current_line" && break
-		# line must contain both "Cell" and "Address" to be considered
-		#  start of a new cell..
-		echo "$current_line" | grep "Cell" >> /dev/null
-		result_one=$?
-		echo "$current_line" | grep "Address" >> /dev/null
-		result_two=$?
-		equal "$result_one" "0" && equal "$result_two" "0" && {
-			equal "$first_hit" "0" && {
-				let "current+=1"
-			}
-			first_hit=0
-		}
-		equal "$first_hit" "0" && {
-			echo "$current_line" >> "$tempfile"_"${current}"
-		}
 
-		sed 1d < $tempfile > $tempfile2
-		rm $tempfile
-		mv $tempfile2 $tempfile
-	done
+#---------------------------------------------
+# We need to add a "break" on the first line!
 
-	current=0
-	counter=0
-	for counter in $(seq 1 $MAX_CELLS); do
-		! exists "$tempfile"_"${current}" && break
-		####################################################
-		# parse out MAC
-		address_pre=$(sed '2,$ d' < "$tempfile"_"${current}" | sed -e s/'Cell'//g -e s/'Address'//g -e s/'-'//g)
-		count=0
-		for i in $address_pre; do
-			case $count in
-				0) CELL_ID=$i;;
-				2) MAC_ID=$i;;
-				3) break;;
-			esac
-			let "count+=1"
-		done
+current_line=$(grep -i '' < $tempscan)
+echo "" > $tempfile
+echo "$current_line" >> $tempfile
+rm $tempscan
+#--------------------------------------------
+current=1
 
-		####################################################
-		# parse out essid
-		ESSID=$(grep -i "ESSID" < "$tempfile"_"${current}" | sed -e s/'ESSID:'//g -e s/'"'//g)
+echo "<br><a href='' onClick='document.location.reload(true)'>Re-scan</a><table width="98%" border="0" cellspacing="1" bgcolor="#999999" >"
+echo "<tr bgcolor="#999999" class="wifiscantitle" >"
+echo "<td width='32'>Signal/</td>"
+echo "<td width='32'>Noise</td>"
+echo "<td>Status</td>"
+echo "<td>SSID</td>"
+echo "<td>MAC</td>"
+echo "<td width='20'>Channel</td>"
+echo "<td>Rate</td>"
+echo "<td>&nbsp;</td>"
+echo "</tr>"
 
-		####################################################
-		# parse out channel
-		CHANNEL_ID=$(grep -i "Channel" < "$tempfile"_"${current}" | sed -e s/'Channel:'//g -e s/' '//g)
 
-		####################################################
-		# parse out signal
-		quality_pre=$(grep -i "Quality" < "$tempfile"_"${current}" | sed -e s/'Quality:'//g -e s/'Signal level:'//g  -e s/'dBm'//g -e s/'Noise level:'//g)
-		count=0
-		for i in $quality_pre; do
-			case $count in
-				0) QUALITY=$i;;
-				1) SIGNAL_DBM=$i;;
-				2) NOISE_DBM=$i
-					break;;
-			esac
-			let "count+=1"
-		done
+# Read File
+#-------------------------
 
-		#
-		# only show quality if it's not 0/0
-		#
-		if ! equal "$QUALITY" "0/0"; then
-			QUALITY_STRING="string|<tr><td>Quality $QUALITY</tr></td>"
-		fi
+while read f
+do
 
-		NOISE_BASE=-99
-		NOISE_DELTA=$(expr $NOISE_BASE - $NOISE_DBM)
+# DEBUG
+#------\/
+        #echo $f "<br>"
 
-		SIGNAL_INTEGRITY=$(expr $SIGNAL_DBM + $NOISE_DELTA)
-		MAC_DASHES=$(echo "$MAC_ID" | sed s/':'/'-'/g)
-		MAC_FIRST_THREE=$(echo "$MAC_DASHES" | cut -c1-8)
-		SNR_PERCENT=$(expr 100 + $SIGNAL_INTEGRITY)
+current_line=$(sed '2,$ d' $tempfile)
 
-		FORM_cells="$FORM_cells
-			#string|<tr><td><strong>Cell</strong> $CELL_ID</tr></td>
-			string|<tr><td><strong>SSID</strong> $ESSID (<div class=mac-address><a href=\"http://standards.ieee.org/cgi-bin/ouisearch?$MAC_FIRST_THREE\" target=\"_new\">$MAC_DASHES</a></div>) <strong>Cell</strong>&nbsp;$CELL_ID</tr></td>
-			string|<tr><td><strong>Channel</strong> $CHANNEL_ID</tr></td>
-			$QUALITY_STRING
-			string|<tr><td><strong>Signal</strong> $SIGNAL_DBM&nbsp;<div class=kb>dBm</div> <strong>Noise</strong> $NOISE_DBM&nbsp;<div class=kb>dBm</div></tr></td>
-			progressbar|SNR|<strong>SNR</strong> $SIGNAL_INTEGRITY&nbsp;<div class=kb>dBm</div>|200|$SNR_PERCENT|$SIGNAL_INTEGRITY dBm
-			string|<tr><td><br /></td></tr>"
+#echo "-> '" $current_line "'<br>"
 
-		rm -f "$tempfile"_"${current}"
-
-		let "current+=1"
-	done
-fi # end if were scan results
-
-rm -f "$tempfile"
-rm -f "$tempfile2"
-
-if ! empty "$FORM_clientswitch"; then
-	#echo "<tr><td>Restoring settings...</tr></td>"
-	# restore radio to its original state
-	nvram set wl0_mode=$ORIGINAL_WL_MODE
-	nvram set wl0_infra=$ORIGINAL_INFRA
-	wifi up 2>/dev/null >/dev/null </dev/null
+if equal "$current_line" "" ; then
+        let "current=1"
+        #echo "->" $current
+else
+        let "current=0"                
 fi
 
-fi # end if is in 'allowed to scan' mode
-?>
+if equal "$current" "1" ;then
 
-<br /></tbody></table>
+Dopurge
 
-<?
-if ! empty "$FORM_clientswitch"; then
-display_form <<EOF
-start_form|@TR<<Survey Results>>
-$FORM_cells
-end_form|
-EOF
+current_line=$(sed '2,$ d' $tempfile)
+
+  if equal "$current_line" "" ;then
+        echo ""
+  else
+
+
+#-------------------------
+#current_line=$(sed '2,$ d' $tempfile)
+SSID=$(sed '2,$ d' $tempfile | sed -e s/'SSID: '//g -e s/'"'//g)
+Dopurge
+RSSI=$(sed '2,$ d' $tempfile | cut -c22-23)
+NOISE=$(sed '2,$ d' $tempfile | cut -c37-38)
+CHANNEL=$(sed '2,$ d' $tempfile  | cut -c53-55 )
+Dopurge
+BSSID=$(sed '2,$ d' $tempfile  | cut -c7-24 )
+SEC=$(sed '2,$ d' $tempfile  | cut -c38-44 )
+Dopurge
+RATE=$(sed '2,$ d' $tempfile  | wc -c)
+RATED=$(sed '2,$ d' $tempfile)
+
+echo "<tr bgcolor="#FFFFFF" class="wifiscanrow">"
+if [ $RSSI -lt 60 ]; then 
+Wimg=5
+elif [ $RSSI -lt 72 ]; then
+Wimg=4
+elif [ $RSSI -lt 81 ]; then
+Wimg=3
+elif [ $RSSI -lt 85 ]; then
+Wimg=2
+elif [ $RSSI -lt 92 ]; then
+Wimg=1
+else
+Wimg=0
 fi
-?>
 
-<? footer ?>
+echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $RSSI "dBm'></center></td>"
+
+if [ $NOISE -gt 95 ]; then
+Wimg=0
+elif [ $NOISE -gt 92 ]; then
+Wimg=1
+elif [ $NOISE -gt 88 ]; then
+Wimg=2
+elif [ $NOISE -gt 85 ]; then
+Wimg=3
+elif [ $NOISE -gt 80 ]; then 
+Wimg=4
+else
+Wimg=5
+fi
+
+echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $NOISE "dBm'></center></td>"
+
+if  [ "$SEC" == "ESS WEP" ] ; then
+Wimg="wep"
+else
+Wimg="opn"
+fi
+
+echo "<td><center><img src="/images/$Wimg.gif" ALT='$SEC'></center></td>"
+echo "<td>&nbsp;&nbsp;" $SSID
+#echo "$current_line" | cut -c8-20 | cut -d '"' -f1
+echo "</td>"
+echo "<td><center>" $BSSID "</center></td>"
+echo "<td><center>" $CHANNEL "</center></td>"
+
+if  [ "$RATE" == "66" ] ; then
+Wimg="54 Mbps"
+elif  [ "$RATE" == "75" ] ; then
+Wimg="54 Mbps"
+elif  [ "$RATE" == "44" ] ; then
+Wimg="11 Mbps"
+else
+#Wimg="("$RATE") $RATED"
+Wimg="(?)"
+fi
+
+echo "<td><center>$Wimg</center></td>"
+echo "<td><center>"
+echo "<input type='submit' style='border: 1px solid #000000; font-size:8pt; ' name='joinwifi' value='Join' disabled>"
+echo "</center></td></tr>"
+
+fi
+fi
+
+Dopurge
+
+done < $tempfile
+
+rm $tempfile
+#rm $tempfile2
+echo "</table><a href='' onClick='document.location.reload(true)'>Re-scan</a>"
+fi
+
+        
+footer ?>
 <!--
 ##WEBIF:name:Status:980:Site Survey
 -->
