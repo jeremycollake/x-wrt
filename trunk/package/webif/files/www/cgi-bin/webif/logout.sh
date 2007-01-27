63098224c4d7b112056cb258d83051121cb1b03b
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-27 06:30:03 +0000

    edit wording

--- trunk/package/webif/files/www/cgi-bin/webif/logout.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/logout.sh
@@ -19,7 +19,7 @@
 #
 header "Logout" "Logout" "@TR<<You must close the web browser to log out>>!" '' ''
 cat <<EOF
-@TR<<Since authentication is handled by the httpd on a per-session basis and cached by your web browser, it is not possible to log a user out. You must close the web browser to force it to forget the credentials for this session.>>
+@TR<<Since basic httpd authentication is cached by your web browser, it is not possible to log a user out. You must close the web browser to force it to forget the credentials you have supplied. We will probably switch to cookie based authentication due to this problem.>>
 <br /><br />
 <div class="smalltext">
 @TR<<For a reference, see http://httpd.apache.org/docs/1.3/howto/auth.html>>
