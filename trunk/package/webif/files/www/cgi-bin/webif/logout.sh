5643b8c9965ae1a781d7e1f1c89c4b2555929475
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-27 18:22:00 +0000

    switch logout's @TRs to symbols

--- trunk/package/webif/files/www/cgi-bin/webif/logout.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/logout.sh
@@ -17,12 +17,12 @@
 # Configuration files referenced:
 #   none
 #
-header "Logout" "Logout" "@TR<<You must close the web browser to log out>>!" '' ''
+header "Logout" "Logout" "@TR<<logout_close_window#You must close the web browser to log out!>>" '' ''
 cat <<EOF
-@TR<<Since basic httpd authentication is cached by your web browser, it is not possible to log a user out. You must close the web browser to force it to forget the credentials you have supplied. We will probably switch to cookie based authentication due to this problem.>>
+@TR<<logout_explanation#Since basic httpd authentication is cached by your web browser, it is not possible to log a user out. You must close the web browser to force it to forget the credentials you have supplied. We will probably switch to cookie based authentication due to this problem.>>
 <br /><br />
 <div class="smalltext">
-@TR<<For a reference, see http://httpd.apache.org/docs/1.3/howto/auth.html>>
+@TR<<logout_reference#For a reference, see http://httpd.apache.org/docs/1.3/howto/auth.html>>
 </div>
 EOF
 #logout_user
