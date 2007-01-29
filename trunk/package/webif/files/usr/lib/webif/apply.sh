5bbd36f751996d2a4f8616bb9d04ccc054f6b22d
liran <liran@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-29 15:17:56 +0000

    changed the reload_hotspot() settings to accommodate the new hotspot interface files.

--- trunk/package/webif/files/usr/lib/webif/apply.sh
+++ trunk/package/webif/files/usr/lib/webif/apply.sh
@@ -292,24 +292,18 @@ init_theme() {
 }
 
 reload_hotspot() {
-	echo '@TR<<Reloading>> @TR<<hotspot settings>> ...'
-	grep -v '^hs_cframe' config-hotspot | grep '^hs_' >&- 2>&- && {
-	[ -e "/usr/sbin/chilli" ] && {
-		/etc/init.d/S??chilli stop  >&- 2>&-
-		/etc/init.d/S??chilli start >&- 2>&-
-	}
-	[ -e "/usr/bin/wdctl" ] && {
-		wdctl stop >&- 2>&-
-		/etc/init.d/S??wifidog start >&- 2>&-
-	}
-	}
-	grep '^hs_cframe' config-hotspot >&- 2>&- && {
-	[ -e /etc/init.d/S??cframe ] && {
-		/etc/init.d/S??cframe stop  >&- 2>&-
-		/etc/init.d/S??cframe start >&- 2>&-
-	}
-	}
+        echo '@TR<<Exporting>> @TR<<hotspot settings>> ...'
+        [ -e "/bin/save_hotspot" ] && {
+                /bin/save_hotspot >&- 2>&-
+        }
+
+        echo '@TR<<Reloading>> @TR<<hotspot settings>> ...'
+        [ -e "/usr/sbin/chilli" ] && {
+                /etc/init.d/chilli stop  >&- 2>&-
+                /etc/init.d/chilli start >&- 2>&-
+        }
 }
+
 
 reload_pptp() {
 	echo '@TR<<Reloading>> @TR<<PPTP settings>> ...'
