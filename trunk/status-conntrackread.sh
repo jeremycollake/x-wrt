81db605818cb9bcc42d5125718097688f7e4e5a8
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-29 11:01:18 +0000

    clean up errant commit

--- trunk/status-conntrackread.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/webif-page
-<?
-. /usr/lib/webif/webif.sh
-header "Status" "Conntrack Table" "@TR<<Conntrack Table>>"
-echo "<table><tbody><tr><td><div class=smalltext><pre>"
-cat /proc/net/ip_conntrack | sort
-echo "</pre></div></td></tr></tbody></table>"
-footer ?>
