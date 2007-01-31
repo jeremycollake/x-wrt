c318aa6f1f3d6b5c3d4e9eedbc10b440a536df9d
dmytro <dmytro@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-01-31 15:16:30 +0000

    "Apply Changes" button wait effect ...fix from lost changes during syncs

--- kamikaze/package/webif/files/usr/lib/webif/webif.sh
+++ kamikaze/package/webif/files/usr/lib/webif/webif.sh
@@ -272,7 +272,7 @@ footer() {
 	<legend><strong>@TR<<Proceed Changes>></strong></legend>
 		$_savebutton
 		<ul class="apply">
-			<li><a href="config.sh?mode=save&amp;cat=$_category&amp;prev=$SCRIPT_NAME">@TR<<Apply Changes>> &laquo;</a></li>
+			<li><a href="config.sh?mode=save&amp;cat=$_category&amp;prev=$SCRIPT_NAME" rel="lightbox" >@TR<<Apply Changes>> &laquo;</a></li>
 			<li><a href="config.sh?mode=clear&amp;cat=$_category&amp;prev=$SCRIPT_NAME">@TR<<Clear Changes>> &laquo;</a></li>
 			<li><a href="config.sh?mode=review&amp;cat=$_category&amp;prev=$SCRIPT_NAME">@TR<<Review Changes>> $_changes &laquo;</a></li>
 		</ul>
