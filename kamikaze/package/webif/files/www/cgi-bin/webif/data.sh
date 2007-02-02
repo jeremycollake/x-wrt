f4aad99c102e538f55d92b326a3b8a352c3d523f
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-02 03:43:18 +0000

    fix cpu graph

--- kamikaze/package/webif/files/www/cgi-bin/webif/data.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/webif-page
-<? date ?><?if [ "$FORM_if" ] ?><? grep "${FORM_if}:" /proc/net/dev ?><?el?><? head -n 1 /proc/stat ?><?fi?>
+<? date ?><?if [ "$FORM_if" ] ?><? grep "${FORM_if}:"cat /proc/net/dev ?><?el?><? head -n 1 /proc/stat ?><?fi?>
