6769ef39672784c55e99c22e15dc75656a1306be
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-02 02:45:38 +0000

    throw in ugly linebreaks to give spacing at bottom - seitel: check theme for appropriate fix.

--- trunk/package/webif/files/www/cgi-bin/webif/info-about.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/info-about.sh
@@ -72,7 +72,7 @@ this_revision=$(cat "/www/.version")
 <p>@TR<<GPL_Text#This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.>>
 
 </div></div> <!-- End scrollBox -->
-
+<br /><br />
 <? footer ?>
 <!--
 ##WEBIF:name:Info:950:About
