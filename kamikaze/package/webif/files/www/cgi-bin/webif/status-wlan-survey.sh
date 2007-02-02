9c696d81d278e47862ef2bbc6f5be68971503cf4
dmytro <dmytro@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-02 22:07:09 +0000

    [WIFI survey wl + iwlist] YES! finally i bricked my router doing this. someone please test this.

--- kamikaze/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
@@ -24,58 +24,7 @@
 #
 . "/usr/lib/webif/webif.sh"
 
-header "Status" "Site Survey" "<img src=/images/wscan.jpg align=absmiddle>&nbsp;@TR<<Wireless survey>>"
-
-
-if is_package_installed "wl" ; then #--->[ -s "/usr/sbin/wl" ] ||
-
-if [ "$FORM_joinwifi" != "" ]; then
-. "/lib/config/uci.sh"
-
-config_cb() {
-config_get TYPE "$CONFIG_SECTION" TYPE
-case "$TYPE" in
-        wifi-iface)
-                config_get device "$CONFIG_SECTION" device
-                config_get vifs "$device" vifs
-                append vface "$CONFIG_SECTION" "$N"
-        ;;
-esac
-}
-
-config_load wireless
-
-for vcfg in $vface; do
-config_get FORM_device $vcfg device
-
-	if [ "$FORM_device" = "$device" ]; then
-		uci_set "wireless" "$vcfg" "network" "lan"
-		uci_set "wireless" "$vcfg" "mode" "sta"
-		uci_set "wireless" "$vcfg" "ssid" "$FORM_wifi"
-			if [ "$FORM_wepkey" != "" ]; then
-				uci_set "wireless" "$vcfg" "encryption" "$FORM_keytype"
-				uci_set "wireless" "$vcfg" "key" "$FORM_wepkey"
-			else
-				uci_set "wireless" "$vcfg" "encryption" "none"
-				uci_set "wireless" "$vcfg" "key" ""
-			fi
-	fi
-done
-uci_commit "wireless"
-
-#iwconfig wl0 mode "repeater"
-#iwconfig wl0 ESSID "$FORM_wifi"
-
-cat <<EOF
-<meta http-equiv="refresh" content="4;url=reboot.sh?reboot=do">
-<br>
-<b>Successfully joined "$FORM_wifi" network. Router must reboot ...<b>
-EOF
-
-footer
-exit
-
-fi
+##### Variables
 
 MAX_TRIES=4
 MAX_CELLS=100
@@ -88,34 +37,86 @@ tempfile=$(mktemp /tmp/.survtemp.XXXXXX)
 tempfile2=$(mktemp /tmp/.survtemp.XXXXXX)
 tempscan=$(mktemp /tmp/.survscan.XXXXXX)
 
-##################################################
-#
-Dopurge ()
+#######################################
+
+header "Status" "Site Survey" "<img src=/images/wscan.jpg align=absmiddle>&nbsp;@TR<<Wireless survey>>"
+
+
+######## Join WIFI ########
+
+if [ "$FORM_joinwifi" != "" ]; then
+
+if is_kamikaze; then
+
+		. "/lib/config/uci.sh"
+
+		config_cb() {
+		config_get TYPE "$CONFIG_SECTION" TYPE
+	case "$TYPE" in
+		wifi-iface)
+		config_get device "$CONFIG_SECTION" device
+		config_get vifs "$device" vifs
+		append vface "$CONFIG_SECTION" "$N"
+		;;
+	esac
+	}
+
+		config_load wireless
+
+	for vcfg in $vface; do
+		config_get FORM_device $vcfg device
+
+		if [ "$FORM_device" = "$device" ]; then
+			uci_set "wireless" "$vcfg" "network" "lan"
+			uci_set "wireless" "$vcfg" "mode" "sta"
+			uci_set "wireless" "$vcfg" "ssid" "$FORM_wifi"
+				if [ "$FORM_wepkey" != "" ]; then
+					uci_set "wireless" "$vcfg" "encryption" "$FORM_keytype"
+					uci_set "wireless" "$vcfg" "key" "$FORM_wepkey"
+				else
+					uci_set "wireless" "$vcfg" "encryption" "none"
+					uci_set "wireless" "$vcfg" "key" ""
+				fi
+		fi
+	done
+	uci_commit "wireless"
+
+	#iwconfig wl0 mode "repeater"
+	#iwconfig wl0 ESSID "$FORM_wifi"
+
+	cat <<EOF
+	<meta http-equiv="refresh" content="4;url=reboot.sh?reboot=do">
+	<br>
+	<b>Successfully joined "$FORM_wifi" network. Router must reboot ...<b>
+EOF
+	footer
+	exit
+
+else #<- if kamikaze
+
+	nvram set wl0_mode="sta"
+	nvram set wl0_infra="1"
+	nvram set wl_ssid="$FORM_wifi"
+	nvram commit
+
+	WL_MODE=$(nvram get wl0_ifname)
+	#wifi up 2>/dev/null >/dev/null </dev/null
+	iwconfig $WL_MODE essid "$FORM_wifi"
+
+	cat <<EOF
+	<meta http-equiv="refresh" content="4;url=$SCRIPT_NAME">
+	<br>
+	<b>Successfully joined "$FORM_wifi" network ...<b>
+EOF
+	footer
+	exit
+fi #<- end if kamikaze
+
+fi #<- end if Join WIFI
+
+
+DisplayTable()
 {
-	sed 1d < $tempfile > $tempfile2
-	rm $tempfile
-	mv $tempfile2 $tempfile     
-}
-
-for counter in $(seq 1 $MAX_TRIES); do
-       wl scan
-       wl scanresults > $tempscan 2> /dev/null
-	if equal $(sed '2,$ d' $tempscan | cut -c0-4) "SSID" ; then break ; fi
-       sleep 1
-done
-
-if [ $counter -gt $MAX_TRIES ]; then
-	echo "<tr><td>@TR<<Sorry, no scan results.>></td></tr>"
-else
-
-#---------------------------------------------
-# We need to add a "break" on the first line!
-
-current_line=$(grep -i '' < $tempscan)
-echo "" > $tempfile
-echo "$current_line" >> $tempfile
-rm $tempscan
-#--------------------------------------------
 
 cat <<EOF
 <script type="text/javascript" src="/js/window.js">
@@ -157,8 +158,89 @@ document.wepkeyform.wifi.value = target
 <td>@TR<<Rate>></td>
 <td>&nbsp;</td>
 </tr>
-
 EOF
+
+}
+
+DisplayTR()
+{
+
+if [ "$color" = "1" ] ; then color="2" ; else color="1" ; fi
+
+echo "<tr bgcolor="#FFFFFF" class="wifiscanrow$color">"
+
+##### Signal Ratio
+
+if [ $RSSI -lt 60 ]; then Wimg=5 ; elif [ $RSSI -lt 72 ]; then Wimg=4 ; elif [ $RSSI -lt 81 ]; then Wimg=3 ; elif [ $RSSI -lt 85 ]; then Wimg=2 ; elif [ $RSSI -lt 92 ]; then Wimg=1 ; else Wimg=0 ; fi
+echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $RSSI "dBm'></center></td>"
+
+##### Noise Ratio
+if [ $NOISE -gt 95 ]; then Wimg=0 ; elif [ $NOISE -gt 92 ]; then Wimg=1 ; elif [ $NOISE -gt 88 ]; then Wimg=2 ; elif [ $NOISE -gt 85 ]; then Wimg=3 ; elif [ $NOISE -gt 80 ]; then Wimg=4 ; else Wimg=5 ; fi
+echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $NOISE "dBm'></center></td>"
+
+##### Security
+if  [ "$SEC" = "ESS WEP" ] || [ "$SEC" = "on" ] ; then Wimg="wep" ; else Wimg="opn" ; fi
+
+echo "<td><center><img src="/images/$Wimg.gif" ALT='$SEC'></center></td>"
+echo "<td>&nbsp;&nbsp;" $SSID
+#echo "$current_line" | cut -c8-20 | cut -d '"' -f1
+echo "</td>"
+echo "<td><center>" $BSSID "</center></td>"
+echo "<td><center>" $CHANNEL "</center></td>"
+
+##### Speed (needs improvements!)
+if  [ "$RATE" = "66" ] ; then Wimg="54 Mbps" ; elif  [ "$RATE" = "75" ] ; then Wimg="54 Mbps" ; elif  [ "$RATE" = "44" ] ; then Wimg="11 Mbps" ; else Wimg="(?)" ; fi
+
+echo "<td><center>$Wimg</center></td>"
+echo "<td><center>"
+
+	if  [ "$SEC" = "ESS WEP" ] || [ "$SEC" = "on" ] ; then
+		echo "<input type='submit' style='border: 1px solid #000000; font-size:8pt; ' name='joinwifi' value='@TR<<Join>>' onClick=loadwindow('$SCRIPT_NAME/?wep=1&ssid=$SSID',300,100);java1('$SSID')>"
+	else
+		echo "<form action='$SCRIPT_NAME' method='post'><input type="hidden" name='wifi' value='$SSID'><input type='submit' style='border: 1px solid #000000; font-size:8pt; ' name='joinwifi' value='@TR<<Join>>'></form>"
+	fi
+echo "</center></td></tr>"
+
+
+}
+
+
+######################### The Scanning Part #####
+##### wl scanning
+
+WL()
+{
+
+if is_package_installed "wl" ; then 
+
+Dopurge ()
+{
+sed 1d < $tempfile > $tempfile2
+rm $tempfile
+mv $tempfile2 $tempfile     
+}
+
+for counter in $(seq 1 $MAX_TRIES); do
+	wl scan
+	wl scanresults > $tempscan 2> /dev/null
+	if equal $(sed '2,$ d' $tempscan | cut -c0-4) "SSID" ; then break ; fi
+	sleep 1
+done
+
+if [ $counter -gt $MAX_TRIES ]; then
+	echo "<tr><td>@TR<<Sorry, no scan results.>></td></tr>"
+else
+
+#---------------------------------------------
+# We need to add a "break" on the first line!
+
+current_line=$(grep -i '' < $tempscan)
+echo "" > $tempfile
+echo "$current_line" >> $tempfile
+rm $tempscan
+#--------------------------------------------
+
+DisplayTable
 
 # Read File
 #-------------------------
@@ -168,7 +250,7 @@ do
 
 # DEBUG
 #------\/
-	#echo $f "<br>"
+#echo $f "<br>"
 
 current_line=$(sed '2,$ d' $tempfile)
 
@@ -187,9 +269,9 @@ Dopurge
 
 current_line=$(sed '2,$ d' $tempfile)
 
-  if equal "$current_line" "" ;then
-	echo ""
-  else
+if equal "$current_line" "" ;then
+echo ""
+else
 
 ##### Set Variables
 #current_line=$(sed '2,$ d' $tempfile)
@@ -203,43 +285,10 @@ BSSID=$(sed '2,$ d' $tempfile  | cut -c7-24 )
 SEC=$(sed '2,$ d' $tempfile  | cut -c38-44 )
 Dopurge
 RATE=$(sed '2,$ d' $tempfile  | wc -c)
-RATED=$(sed '2,$ d' $tempfile)
+#RATED=$(sed '2,$ d' $tempfile)
 
+DisplayTR
 
-if [ "$color" = "1" ] ; then color=2 ; else color=1 ; fi
-
-echo "<tr bgcolor="#FFFFFF" class="wifiscanrow$color">"
-
-##### Signal Ratio
-if [ $RSSI -lt 60 ]; then Wimg=5 ; elif [ $RSSI -lt 72 ]; then Wimg=4 ; elif [ $RSSI -lt 81 ]; then Wimg=3 ; elif [ $RSSI -lt 85 ]; then Wimg=2 ; elif [ $RSSI -lt 92 ]; then Wimg=1 ; else Wimg=0 ; fi
-echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $RSSI "dBm'></center></td>"
-
-##### Noise Ratio
-if [ $NOISE -gt 95 ]; then Wimg=0 ; elif [ $NOISE -gt 92 ]; then Wimg=1 ; elif [ $NOISE -gt 88 ]; then Wimg=2 ; elif [ $NOISE -gt 85 ]; then Wimg=3 ; elif [ $NOISE -gt 80 ]; then Wimg=4 ; else Wimg=5 ; fi
-echo "<td><center><img src="/images/wifi$Wimg.gif" ALT='-" $NOISE "dBm'></center></td>"
-
-##### Security
-if  [ "$SEC" == "ESS WEP" ] ; then Wimg="wep" ; else Wimg="opn" ; fi
-
-echo "<td><center><img src="/images/$Wimg.gif" ALT='$SEC'></center></td>"
-echo "<td>&nbsp;&nbsp;" $SSID
-#echo "$current_line" | cut -c8-20 | cut -d '"' -f1
-echo "</td>"
-echo "<td><center>" $BSSID "</center></td>"
-echo "<td><center>" $CHANNEL "</center></td>"
-
-##### Speed (needs improvements!)
-if  [ "$RATE" == "66" ] ; then Wimg="54 Mbps" ; elif  [ "$RATE" == "75" ] ; then Wimg="54 Mbps" ; elif  [ "$RATE" == "44" ] ; then Wimg="11 Mbps" ; else Wimg="(?)" ; fi
-
-echo "<td><center>$Wimg</center></td>"
-echo "<td><center>"
-
-if  [ "$SEC" == "ESS WEP" ] ; then
-echo "<input type='submit' style='border: 1px solid #000000; font-size:8pt; ' name='joinwifi' value='@TR<<Join>>' onClick=loadwindow('$SCRIPT_NAME/?wep=1&ssid=$SSID',300,100);java1('$SSID')>"
-else
-echo "<form action='$SCRIPT_NAME' method='post'><input type="hidden" name='wifi' value='$SSID'><input type='submit' style='border: 1px solid #000000; font-size:8pt; ' name='joinwifi' value='@TR<<Join>>'></form>"
-fi
-echo "</center></td></tr>"
 fi
 fi
 
@@ -253,15 +302,15 @@ echo "</table><br><a href='$SCRIPT_NAME'>@TR<<Re-scan>></a><br>"
 fi
 else
 
-if ! empty "$FORM_install_package"; then
+	if ! empty "$FORM_install_package"; then
 	echo "Installing wl package ...<pre>"
 	install_package "wl"
 	echo "</pre>"
-fi
+	fi
 
 echo "<form enctype='multipart/form-data' action='$SCRIPT_NAME' method='post'>"
 install_package_button="string|<div class=warning>Wireless Scanning will not work until you install "wl": </div>
-		submit|install_package| Install "wl" Package |"
+submit|install_package| Install "wl" Package |"
 
 display_form <<EOF
 $install_package_button
@@ -269,7 +318,114 @@ EOF
 echo "</form>"
 
 fi
-	
+}
+
+######### iwlist scanning
+IWLIST()
+{
+#echo " Please wait while scan is performed ... <br /><br />"
+found_networks=0
+counter=0
+for counter in $(seq 1 $MAX_TRIES); do
+        #echo "."
+        iwlist scan > $tempfile 2> /dev/null
+        grep -i "Address" < $tempfile >> /dev/null
+        equal "$?" "0" && break
+        sleep 1
+done
+
+first_hit=1
+if [ $counter -gt $MAX_TRIES ]; then
+        echo "<tr><td>@TR<<Sorry, no scan results.>></td></tr>"
+else
+
+DisplayTable
+        current=0
+        counter=0
+        for counter in $(seq 1 $MAX_CELLS); do
+                current_line=$(sed '2,$ d' < $tempfile)
+                empty "$current_line" && break
+                # line must contain both "Cell" and "Address" to be considered
+                #  start of a new cell..
+                echo "$current_line" | grep "Cell" >> /dev/null
+                result_one=$?
+                echo "$current_line" | grep "Address" >> /dev/null
+                result_two=$?
+                if equal "$result_one" "0" && equal "$result_two" "0" ; then
+                        if equal "$first_hit" "0" ; then
+                                let "current+=1"
+                        fi
+                        first_hit=0
+                fi
+                if equal "$first_hit" "0" ; then
+                        echo "$current_line" >> "$tempfile"_"${current}"
+                fi
+
+                sed 1d < $tempfile > $tempfile2
+                rm $tempfile
+                mv $tempfile2 $tempfile
+        done
+
+        current=0
+        counter=0
+        for counter in $(seq 1 $MAX_CELLS); do
+                ! exists "$tempfile"_"${current}" && break
+                ####################################################
+                # parse out MAC
+                address_pre=$(sed '2,$ d' < "$tempfile"_"${current}" | sed -e s/'Cell'//g -e s/'Address'//g -e s/'-'//g)
+                count=0
+                for i in $address_pre; do
+                        case $count in
+                                0) CELL_ID=$i;;
+                                2) BSSID=$i;;
+                                3) break;;
+                        esac
+                        let "count+=1"
+                done
+
+####################################################
+
+SSID=$(grep -i "ESSID" < "$tempfile"_"${current}" | sed -e s/'ESSID:'//g -e s/'"'//g -e s/'  '//g)
+CHANNEL=$(grep -i "Channel" < "$tempfile"_"${current}" | sed -e s/'Channel:'//g -e s/' '//g)
+
+	quality_pre=$(grep -i "Quality" < "$tempfile"_"${current}" | sed -e s/'Quality:'//g -e s/'Signal level:'//g  -e s/'dBm'//g -e s/'Noise level:'//g -e s/'-'//g)
+                count=0
+	for i in $quality_pre; do
+		case $count in
+			0) QUALITY=$i;;
+			1) RSSI=$i;;
+			2) NOISE=$i
+			break;;
+		esac
+			let "count+=1"
+	done
+
+SEC=$(grep -i "Encryption key" < "$tempfile"_"${current}" | sed -e s/'Encryption key:'//g -e s/' '//g)
+RATE=1
+
+DisplayTR
+rm -f "$tempfile"_"${current}"
+let "found_networks+=1"
+let "current+=1"
+done
+fi #<- end if were scan results
+
+rm -f "$tempfile"
+rm -f "$tempfile2"
+}
+
+
+if is_kamikaze; then
+
+	if is_package_installed "wl" ; then #<- for Broadcom units //[ -s "/usr/sbin/wl" ]
+		WL
+	else
+		IWLIST
+	fi
+else
+	IWLIST
+fi
+
 footer ?>
 <!--
 ##WEBIF:name:Status:980:Site Survey
