bb49e8b32e3df6d94f343e76d99604c0b80e1fdb
jcollake <jcollake@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-02 03:19:40 +0000

    some minor code formatting tweaks

--- trunk/package/webif/files/www/cgi-bin/webif/reboot.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/reboot.sh
@@ -2,26 +2,26 @@
 <?
 . /usr/lib/webif/webif.sh
 header "System" "Reboot" ""
-timeout=40
+
+timeout=45
+
 if empty "$FORM_reboot"; then
-reboot_msg="<form enctype=\"multipart/form-data\" method=\"post\"><input type=\"submit\" value=\" @TR<<Yes, really reboot now>> \" name=\"reboot\" /></form>"
+	reboot_msg="<form enctype=\"multipart/form-data\" method=\"post\"><input type=\"submit\" value=\" @TR<<Yes, really reboot now>> \" name=\"reboot\" /></form>"
 else
-
-
-if is_kamikaze; then
-uci_load "network"
-router_ip="$CONFIG_lan_ipaddr"
-else
-router_ip=$(nvram get lan_ipaddr)
-fi
-
-echo "<meta http-equiv="refresh" content='$timeout;http://$router_ip'>"
-reboot_msg="@TR<<Rebooting now>>...<br><br>@TR<<reboot_wait#Please wait about>> $timeout @TR<<reboot_seconds#seconds.>> @TR<<reboot_reload#The webif should automatically reload.>>
-<br><br><center><script type='text/javascript'>
-var bar1= createBar(350,15,'white',1,'black','blue',85,7,3,'');
-</script></center>"
+	if is_kamikaze; then
+		uci_load "network"
+		router_ip="$CONFIG_lan_ipaddr"
+	else
+		router_ip=$(nvram get lan_ipaddr)
+	fi
+	echo "<meta http-equiv="refresh" content='$timeout;http://$router_ip'>"
+	reboot_msg="@TR<<Rebooting now>>...<br><br>@TR<<reboot_wait#Please wait about>> $timeout @TR<<reboot_seconds#seconds.>> @TR<<reboot_reload#The webif should automatically reload.>>
+	<br><br><center><script type='text/javascript'>
+	var bar1= createBar(350,15,'white',1,'black','blue',85,7,3,'');
+	</script></center>"
 fi
 ?>
+
 <table style="width: 90%; border="0" cellpadding="2" cellspacing="2" align="center">
 <tbody>
 <br><br><br><tr><td>
@@ -34,8 +34,8 @@ fi
 <? footer ?>
 <?
 if ! empty "$FORM_reboot"; then
-sleep 3
-reboot
+	sleep 3
+	reboot
 fi
 ?>
 <!--
