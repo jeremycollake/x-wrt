ee1295d7b18f1af748c25c5c5a8765b3abfe5d4e
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-05 22:27:29 +0000

    make w3c compliant

--- kamikaze/package/webif/files/usr/lib/webif/webif.sh
+++ kamikaze/package/webif/files/usr/lib/webif/webif.sh
@@ -274,7 +274,7 @@ footer() {
         <legend><strong>@TR<<Proceed Changes>></strong></legend>
         $_savebutton
         <ul class="apply">
-                <script type="text/javascript" src="/js/waitbox.js"></script>
+                <li><script type="text/javascript" src="/js/waitbox.js"></script></li>
                 <li><a href="config.sh?mode=save&amp;cat=$_category&amp;prev=$SCRIPT_NAME" rel="lightbox" >@TR<<Apply Changes>> &laquo;</a></li>
                 <li><a href="config.sh?mode=clear&amp;cat=$_category&amp;prev=$SCRIPT_NAME">@TR<<Clear Changes>> &laquo;</a></li>
                 <li><a href="config.sh?mode=review&amp;cat=$_category&amp;prev=$SCRIPT_NAME">@TR<<Review Changes>> $_changes &laquo;</a></li>
