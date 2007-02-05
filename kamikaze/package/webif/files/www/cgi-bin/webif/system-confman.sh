1019c1090d30f38c608e94d4fe742c97952129e9
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-05 17:23:35 +0000

    only trying to get it into shape, unprefixed quotes inside quotes work accidentally only when all paired, kamikaze and white russian branch synchronize

--- kamikaze/package/webif/files/www/cgi-bin/webif/system-confman.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/system-confman.sh
@@ -40,13 +40,13 @@ boot_wait"
 
 . /usr/lib/webif/webif.sh
 
-header "System" "Backup &amp; Restore" "<img src='/images/bkup.jpg' align='middle' alt />&nbsp;@TR<<Backup and Restore>>" ''
+header "System" "Backup &amp; Restore" "<img src=\"/images/bkup.jpg\" align=\"middle\" alt=\"@TR<<Backup and Restore>>\" />&nbsp;@TR<<Backup and Restore>>" ''
 
 case "$FORM_action" in
 	download)
 
-DOWNLOAD="&nbsp;&nbsp;&nbsp;If downloading does not start automaticly, click here ... <a href=\"/config.tgz\">config.tgz</a><br><br>
-<script language="JavaScript" type=\"text/javascript\">
+DOWNLOAD="&nbsp;&nbsp;&nbsp;If downloading does not start automatically, click here ... <a href=\"/config.tgz\">config.tgz</a><br><br>
+<script language=\"JavaScript\" type=\"text/javascript\">
 window.setTimeout('window.location=\"/config.tgz\"', 1000);
 </script>"
 
@@ -90,8 +90,8 @@ fi
 display_form <<EOF
 start_form|Install Configuration
 EOF
-	if [ -n "$dir" ] && [ -d $dir ] && [ -e "$dir/config.name" ] && [ -e "$dir/config.boardtype" ]; then
-			echo "<tr><td colspan=2>installing configuration<br><pre>"
+	if [ -n "$dir" ] && [ -d "$dir" ] && [ -e "$dir/config.name" ] && [ -e "$dir/config.boardtype" ]; then
+			echo "<tr><td colspan=\"2\">installing configuration<br><pre>"
 			cd $dir
 			for file in $(find etc); do
 				if [ -d $file ]; then
@@ -137,9 +137,9 @@ EOF
 	chkconfig)
 
 
-		if [ -n "$FORM_configfile" ] && [ -e $FORM_configfile ]; then
+		if [ -n "$FORM_configfile" ] && [ -e "$FORM_configfile" ]; then
 			
-		echo "<form method="get" name="install" action=\"$SCRIPT_NAME\">"
+		echo "<form method=\"get\" name=\"install\" action=\"$SCRIPT_NAME\">"
 
 			display_form <<EOF
 start_form|Install Configuration
@@ -151,23 +151,23 @@ EOF
 			rm $FORM_configfile
 
 			if [ ! -e "$tmp/config.name" ] || [ ! -e "$tmp/config.boardtype" ]; then
-				echo "<tr><td colspan=2>Invalid config.tgz file!</td></tr>"
+				echo "<tr><td colspan=\"2\">Invalid config.tgz file!</td></tr>"
 			else
 				nm=$(cat $tmp/config.name)
 				bd=$(cat $tmp/config.boardtype)
 				dt=$(cat $tmp/config.date)
 
-			CFGGOOD="<tr><td colspan=2>configuration looks good!<br><br></td></tr>"
+			CFGGOOD="<tr><td colspan=\"2\">configuration looks good!<br><br></td></tr>"
 
 if is_kamikaze; then
 	if [ "$bd" != $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g) ]; then
-		echo "<tr><td colspan=2><font color=red>WARNING</font>: different board type (ours: $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g), file: $bd)!</td></tr>"
+		echo "<tr><td colspan=\"2\"><font color=\"red\">WARNING</font>: different board type (ours: $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g), file: $bd)!</td></tr>"
 	else
 		echo $CFGGOOD
 	fi
 else
 	if [ "$bd" != $(nvram get boardtype) ]; then
-		echo "<tr><td colspan=2><font color=red>WARNING</font>: different board type (ours: $(nvram get boardtype), file: $bd)!</td></tr>"
+		echo "<tr><td colspan=\"2\"><font color=\"red\">WARNING</font>: different board type (ours: $(nvram get boardtype), file: $bd)!</td></tr>"
 	else
 		echo $CFGGOOD
 	fi
@@ -197,22 +197,22 @@ EOF
 fi
 fi
 
-echo "<br><input type='hidden' name='action' value='instconfig'>"
-echo "<input type='hidden' name='dir' value=\"$tmp\">"
-echo "<input type='submit' class='flatbtn'  value='@TR<<Install Config>>'><br><br></form>"
+echo "<br><input type=\"hidden\" name=\"action\" value=\"instconfig\">"
+echo "<input type=\"hidden\" name=\"dir\" value=\"$tmp\">"
+echo "<input type=\"submit\" class=\"flatbtn\"  value=\"@TR<<Install Config>>\"><br><br></form>"
 		fi
 
 		;;
 	esac
-echo "<form method="get" name="download" action="$SCRIPT_NAME">"
+echo "<form method=\"get\" name=\"download\" action=\"$SCRIPT_NAME\">"
 display_form <<EOF
 start_form|@TR<<Backup Configuration>>
 EOF
 
 cat <<EOF
 <tr>
-<td width=70%>@TR<<Name this configuration>>&nbsp;&nbsp;&nbsp;<input name="name" class='flatbtn' value="${FORM_name:-$(nvram get wan_hostname)}"/></td>	
-<td><input type="hidden" name="action" value="download" /><input class='flatbtn' type="submit" name="submit" value="@TR<<Backup>>" /></td>
+<td width="70%">@TR<<Name this configuration>>&nbsp;&nbsp;&nbsp;<input name="name" class="flatbtn" value="${FORM_name:-$(nvram get wan_hostname)}"/></td>	
+<td><input type="hidden" name="action" value="download" /><input class="flatbtn" type="submit" name="submit" value="@TR<<Backup>>" /></td>
 </tr>
 EOF
 
@@ -221,7 +221,7 @@ end_form|
 string|</form>
 EOF
 
-echo "<form method='post' name="instconfig" action="$SCRIPT_NAME" enctype="multipart/form-data">"
+echo "<form method=\"post\" name=\"instconfig\" action=\"$SCRIPT_NAME\" enctype=\"multipart/form-data\">"
 display_form <<EOF
 start_form|@TR<<Restore Configuration>>
 
@@ -229,8 +229,8 @@ EOF
 
 cat<<EOF
 <tr>
-<td width=70%>@TR<<Saved config.tgz file:>>&nbsp;&nbsp;&nbsp;<input type="file" class='flatbtn' name="configfile" /></td>
-<td><input type="hidden" name="action" value="chkconfig" /><input class='flatbtn' type="submit" name="submit" value="@TR<<Restore>>" /></td>
+<td width="70%">@TR<<Saved config.tgz file:>>&nbsp;&nbsp;&nbsp;<input type="file" class="flatbtn" name="configfile" /></td>
+<td><input type="hidden" name="action" value="chkconfig" /><input class="flatbtn" type="submit" name="submit" value="@TR<<Restore>>" /></td>
 </tr>
 EOF
 
