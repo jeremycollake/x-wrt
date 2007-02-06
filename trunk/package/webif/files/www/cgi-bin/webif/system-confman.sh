165a90488a600e05b85196edff64079756ce962e
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-06 15:21:42 +0000

    new @TRs

--- trunk/package/webif/files/www/cgi-bin/webif/system-confman.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/system-confman.sh
@@ -45,7 +45,7 @@ header "System" "Backup &amp; Restore" "<img src=\"/images/bkup.jpg\" align=\"mi
 case "$FORM_action" in
 	download)
 
-DOWNLOAD="&nbsp;&nbsp;&nbsp;If downloading does not start automatically, click here ... <a href=\"/config.tgz\">config.tgz</a><br><br>
+DOWNLOAD="&nbsp;&nbsp;&nbsp;@TR<<confman_noauto_click#If downloading does not start automatically, click here>> ... <a href=\"/config.tgz\">config.tgz</a><br><br>
 <script language=\"JavaScript\" type=\"text/javascript\">
 window.setTimeout('window.location=\"/config.tgz\"', 1000);
 </script>"
@@ -88,10 +88,10 @@ fi
 
 	dir=$FORM_dir
 display_form <<EOF
-start_form|Install Configuration
+start_form|@TR<<Restore Configuration>>
 EOF
 	if [ -n "$dir" ] && [ -d "$dir" ] && [ -e "$dir/config.name" ] && [ -e "$dir/config.boardtype" ]; then
-			echo "<tr><td colspan=\"2\">installing configuration<br><pre>"
+			echo "<tr><td colspan=\"2\">@TR<<confman_restoring_conf#Restoring configuration.>><br><pre>"
 			cd $dir
 			for file in $(find etc); do
 				if [ -d $file ]; then
@@ -99,7 +99,7 @@ EOF
 				else
 					[ -e /$file ] && rm /$file
 					cp $file /$file
-					echo "restoring $file"
+					echo "@TR<<confman_restoring_file#restoring>> $file"
 				fi
 			done
 if is_kamikaze; then
@@ -116,12 +116,12 @@ else
 				{
 					v=$2
 					gsub(/[$]/,"\\$",v)
-					print "echo \"setting " $1 "=\\\"" $2 "\\\"\"" >> "nvram.sh"
+					print "echo \"@TR<<confman_setting#setting>> " $1 "=\\\"" $2 "\\\"\"" >> "nvram.sh"
 					print "nvram set " $1 "=\"" v "\"" >> "nvram.sh"
 				}
 			' nvram.set
 		sh nvram.sh
-		echo "committing NVRAM settings"
+		echo "@TR<<confman_comitting_nvram#Committing NVRAM settings.>>"
 		nvram commit
 
 		}
@@ -142,7 +142,7 @@ EOF
 		echo "<form method=\"get\" name=\"install\" action=\"$SCRIPT_NAME\">"
 
 			display_form <<EOF
-start_form|Install Configuration
+start_form|@TR<<Restore Configuration>>
 EOF
 			rm -rf /tmp/config.* 2>/dev/null
 			tmp=/tmp/config.$$
@@ -151,23 +151,23 @@ EOF
 			rm $FORM_configfile
 
 			if [ ! -e "$tmp/config.name" ] || [ ! -e "$tmp/config.boardtype" ]; then
-				echo "<tr><td colspan=\"2\">Invalid config.tgz file!</td></tr>"
+				echo "<tr><td colspan=\"2\">@TR<<confman_invalid_file#Invalid file>>: config.tgz!</td></tr>"
 			else
 				nm=$(cat $tmp/config.name)
 				bd=$(cat $tmp/config.boardtype)
 				dt=$(cat $tmp/config.date)
 
-			CFGGOOD="<tr><td colspan=\"2\">configuration looks good!<br><br></td></tr>"
+			CFGGOOD="<tr><td colspan=\"2\">@TR<<confman_good_conf#The configuration looks good>>!<br><br></td></tr>"
 
 if is_kamikaze; then
 	if [ "$bd" != $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g) ]; then
-		echo "<tr><td colspan=\"2\"><font color=\"red\">WARNING</font>: different board type (ours: $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g), file: $bd)!</td></tr>"
+		echo "<tr><td colspan=\"2\"><font color=\"red\">@TR<<big_warning#WARNING>></font>: @TR<<confman_other_board#different board type>> (@TR<<confman_board_ours#ours>>: $(dmesg | grep "CPU revision is:" | sed -e s/'CPU revision is: '//g), @TR<<confman_board_file#file>>: $bd)!</td></tr>"
 	else
 		echo $CFGGOOD
 	fi
 else
 	if [ "$bd" != $(nvram get boardtype) ]; then
-		echo "<tr><td colspan=\"2\"><font color=\"red\">WARNING</font>: different board type (ours: $(nvram get boardtype), file: $bd)!</td></tr>"
+		echo "<tr><td colspan=\"2\"><font color=\"red\">@TR<<big_warning#WARNING>></font>: @TR<<confman_other_board#different board type>> (@TR<<confman_board_ours#ours>>: $(nvram get boardtype), @TR<<confman_board_file#file>>: $bd)!</td></tr>"
 	else
 		echo $CFGGOOD
 	fi
@@ -199,7 +199,7 @@ fi
 
 echo "<br><input type=\"hidden\" name=\"action\" value=\"instconfig\">"
 echo "<input type=\"hidden\" name=\"dir\" value=\"$tmp\">"
-echo "<input type=\"submit\" class=\"flatbtn\"  value=\"@TR<<Install Config>>\"><br><br></form>"
+echo "<input type=\"submit\" class=\"flatbtn\"  value=\"@TR<<Restore>>\"><br><br></form>"
 		fi
 
 		;;
@@ -211,7 +211,7 @@ EOF
 
 cat <<EOF
 <tr>
-<td width="70%">@TR<<Name this configuration>>&nbsp;&nbsp;&nbsp;<input name="name" class="flatbtn" value="${FORM_name:-$(nvram get wan_hostname)}"/></td>	
+<td width="70%">@TR<<Name this configuration>>&nbsp;&nbsp;&nbsp;<input name="name" class="flatbtn" value="${FORM_name:-$(nvram get wan_hostname)}"/></td>
 <td><input type="hidden" name="action" value="download" /><input class="flatbtn" type="submit" name="submit" value="@TR<<Backup>>" /></td>
 </tr>
 EOF
@@ -224,7 +224,6 @@ EOF
 echo "<form method=\"post\" name=\"instconfig\" action=\"$SCRIPT_NAME\" enctype=\"multipart/form-data\">"
 display_form <<EOF
 start_form|@TR<<Restore Configuration>>
-
 EOF
 
 cat<<EOF
