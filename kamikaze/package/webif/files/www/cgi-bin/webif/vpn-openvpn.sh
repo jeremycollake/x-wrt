fa60c2d74bb85c1d6c15827647bc76825db6097b
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-02-07 15:55:10 +0000

    fix earlier attempt to convert to kamikaze

--- kamikaze/package/webif/files/www/cgi-bin/webif/vpn-openvpn.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/vpn-openvpn.sh
@@ -4,10 +4,9 @@
 # command line parsing code!
 
 . /usr/lib/webif/webif.sh
-load_settings "openvpn"
+uci_load "openvpn"
 
 header "VPN" "OpenVPN" "@TR<<OpenVPN>>" ' onload="modechange()" ' "$SCRIPT_NAME"
-ShowUntestedWarning
 
 if ! empty "$FORM_install_package"; then
 	echo "Installing openvpn package ...<pre>"
@@ -25,14 +24,14 @@ if empty "$FORM_submit"; then
 		NOCERT=1
 	[ -f /etc/openvpn/shared.key ] ||
 		NOPSK=1
-	FORM_openvpn_cli=${openvpn_cli:-$(uci get openvpn.general.mode)}
-	FORM_openvpn_cli_server=${openvpn_cli_server:-$(uci get openvpn.client.ipaddr)}
-	FORM_openvpn_cli_proto=${openvpn_cli_proto:-$(uci get openvpn.general.proto)}
-	FORM_openvpn_cli_port=${openvpn_cli_port:-$(uci get openvpn.general.port)}
+	FORM_openvpn_cli="$CONFIG_general_mode"
+	FORM_openvpn_cli_server="$CONFIG_client_ipaddr"
+	FORM_openvpn_cli_proto="$CONFIG_general_proto"
+	FORM_openvpn_cli_port="$CONFIG_general_port"
 	FORM_openvpn_cli_port=${FORM_openvpn_cli_port:-1194}
-	FORM_openvpn_cli_auth=${openvpn_cli_auth:-$(uci get openvpn.client.auth)}
+	FORM_openvpn_cli_auth="$CONFIG_client_auth"
 	FORM_openvpn_cli_auth=${FORM_openvpn_cli_auth:-cert)}
-	FORM_openvpn_cli_psk=${openvpn_cli_psk:-$(uci get openvpn.client.psk)}
+	FORM_openvpn_cli_psk="$CONFIG_client_psk"
 else
 	[ -d /etc/openvpn ] || mkdir /etc/openvpn
 	[ -f "$FORM_openvpn_pkcs12file" ] && {
