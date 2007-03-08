25f81e64551e24bb1ff87d1c360dda2417640baf
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-08 22:45:46 +0000

    add kernel ring buffer viewer, kamikaze and white russian branch synchronize

---
+++ kamikaze/package/webif/files/www/cgi-bin/webif/log-dmesg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/webif-page
+<?
+. /usr/lib/webif/webif.sh
+
+header "Log" "Kernel" "@TR<<log_dmesg_Kernel_Ring_Buffer#Kernel Ring Buffer>>"
+?>
+<iframe src="log-dmesg_frame.sh" width="90%" height="300" scrolling="auto">
+@TR<<HelpText Browser_Frames#Your browser does not support frames,<br>please follow this link>>: <a href="log-dmesg_frame.sh" target="blank" >@TR<<log_dmesg_View_Kernel_Ring_Buffer#View Kernel Ring Buffer>></a>
+</iframe>
+<? footer ?>
+<!--
+##WEBIF:name:Log:300:Kernel
+-->
