b03f64aefb06a8b5e8f8ec6af77808bfdb7dd535
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-09 20:24:39 +0000

    add broadcom 2.6 to ipkg and webif update function

--- kamikaze/package/webif/files/www/cgi-bin/webif/info.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/info.sh
@@ -18,6 +18,10 @@ if is_kamikaze; then
 	[ $? = 0 ] && {
 		version_url="http://downloads.x-wrt.org/xwrt/kamikaze/brcm-2.4/"
 	}
+	uname -a |"grep 2.6.*" |grep -q mips
+	[ $? = 0 ] && {
+		version_url="http://downloads.x-wrt.org/xwrt/kamikaze/brcm-2.6/"
+	}
 	uname -a |grep 2.6 |grep -q "i\?86"
 	[ $? = 0 ] && {
 		version_url="http://downloads.x-wrt.org/xwrt/kamikaze/x86-2.6/"
