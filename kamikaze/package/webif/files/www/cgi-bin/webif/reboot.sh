ef28508c170001d43c2ab38a7d00d68a64242ba2
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-18 15:07:25 +0000

    return to the proper address, kamikaze and white russian branch synchronize

--- kamikaze/package/webif/files/www/cgi-bin/webif/reboot.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/reboot.sh
@@ -1,39 +1,48 @@
 #!/usr/bin/webif-page
 <?
 . /usr/lib/webif/webif.sh
-header "System" "Reboot" ""
+
 timeout=40
 if empty "$FORM_reboot"; then
-reboot_msg="<form method="post" action='$SCRIPT_NAME'><input type=\"submit\" value=\" @TR<<Yes, really reboot now>> \" name=\"reboot\" /></form>"
+	reboot_msg="<form method=\"post\" action=\"$SCRIPT_NAME\"><input type=\"submit\" value=\" @TR<<Yes, really reboot now>> \" name=\"reboot\" /></form>"
 else
-
-
-if is_kamikaze; then
-uci_load "network"
-router_ip="$CONFIG_lan_ipaddr"
-else
-router_ip=$(nvram get lan_ipaddr)
+	router_ip=$(echo "$HTTP_USER_AGENT" | cut -d / -f 3)
+	[ -z "$router_ip" ] && {
+		if is_kamikaze; then
+			uci_load "network"
+			router_ip="$CONFIG_lan_ipaddr"
+		else
+			router_ip=$(nvram get lan_ipaddr)
+		fi
+	}
+	header_inject_head="<meta http-equiv=\"refresh\" content=\"$timeout;http://$router_ip\" />"
+	reboot_msg="@TR<<Rebooting now>>...
+<br/><br/>
+@TR<<reboot_wait#Please wait about>> $timeout @TR<<reboot_seconds#seconds.>> @TR<<reboot_reload#The webif should automatically reload.>>
+<br/><br/>
+<center>
+<script type=\"text/javascript\">
+<!--
+var bar1=createBar(350,15,'white',1,'black','blue',85,7,3,'');
+-->
+</script>
+</center>"
 fi
 
-echo "<meta http-equiv="refresh" content='$timeout;http://$router_ip' />"
-reboot_msg="@TR<<Rebooting now>>...<br/><br/>@TR<<reboot_wait#Please wait about>> $timeout @TR<<reboot_seconds#seconds.>> @TR<<reboot_reload#The webif should automatically reload.>>
-<br/><br/><center><script type='text/javascript'>
-var bar1= createBar(350,15,'white',1,'black','blue',85,7,3,'');
-</script></center>"
-fi
+header "System" "Reboot" ""
 ?>
-<br/><br/><br/><table width="90%" border="0" cellpadding="2" cellspacing="2" align="center">
-<tr><td>
-<script type="text/javascript" src="/js/progress.js"></script>
-<? echo "$reboot_msg" ?>
-<br/><br/><br/></td></tr>
+<br/><br/><br/>
+<table width="90%" border="0" cellpadding="2" cellspacing="2" align="center">
+<tr>
+	<td><script type="text/javascript" src="/js/progress.js"></script><? echo -n "$reboot_msg" ?><br/><br/><br/></td>
+</tr>
 </table>
 <? footer ?>
 <?
-if ! empty "$FORM_reboot"; then
-sleep 3
-reboot
-fi
+! empty "$FORM_reboot" && {
+	reboot &
+	exit
+}
 ?>
 <!--
 ##WEBIF:name:System:910:Reboot
