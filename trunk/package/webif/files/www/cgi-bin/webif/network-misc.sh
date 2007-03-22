3659becd7294d205f8a4f2ce14db5109eef00685
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-22 22:16:49 +0000

    fixes and enhancements

--- trunk/package/webif/files/www/cgi-bin/webif/network-misc.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/network-misc.sh
@@ -18,41 +18,54 @@
 #   /etc/sysctl.conf
 #
 
-header "Network" "Tweaks" "@TR<<Networking Tweaks>>" '' "$SCRIPT_NAME"
+save_setting_sysctl() {
+	# $1 = group
+	# $2 = name
+	# $3 = value
+	# $4 = old value
+	exists /tmp/.webif/* || mkdir -p /tmp/.webif
+	grep "^$2=" /tmp/.webif/config-$1 >&- 2>&- && {
+		grep -v "^$2=" /tmp/.webif/config-$1 > /tmp/.webif/config-$1-new 2>&-
+		mv /tmp/.webif/config-$1-new /tmp/.webif/config-$1 2>&- >&-
+	}
+	equal "$4" "$3" || echo "$2=\"$3\"" >> /tmp/.webif/config-$1
+}
 
-! empty "$FORM_submit" && {
+empty "$FORM_submit" && {
+	load_settings "conntrack"
+	FORM_ip_conntrack_max="${ip_conntrack_max:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_max)}"
+	FORM_ip_conntrack_generic_timeout="${ip_conntrack_generic_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_generic_timeout)}"
+	FORM_ip_conntrack_icmp_timeout="${ip_conntrack_icmp_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_icmp_timeout)}"
+	FORM_ip_conntrack_tcp_timeout_established="${ip_conntrack_tcp_timeout_established:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established)}"
+	FORM_ip_conntrack_udp_timeout="${ip_conntrack_udp_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout)}"
+	FORM_ip_conntrack_udp_timeout_stream="${ip_conntrack_udp_timeout_stream:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout_stream)}"
+} || {
+	SAVED=1
 	validate <<EOF
-int|FORM_ip_conntrack_max|Maximum Connections|min=512 max=32768|$FORM_ip_conntrack_max
-int|FORM_ip_conntrack_udp_timeout_stream|UDP Stream Timeout|min=30 max=134217728|$FORM_ip_conntrack_udp_timeout_stream
-int|FORM_ip_conntrack_udp_timeout|UDP Timeout|min=30 max=134217728|$FORM_ip_conntrack_udp_timeout
-int|FORM_ip_conntrack_tcp_timeout_established|TCP Established Timeout|min=30 max=134217728|$FORM_ip_conntrack_tcp_timeout_established
-int|FORM_ip_conntrack_generic_timeout|Generic Timeout|min=1 max=134217728|$FORM_ip_conntrack_generic_timeout
-int|FORM_ip_conntrack_icmp_timeout|ICMP Timeout|min=1 max=134217728|$FORM_ip_conntrack_icmp_timeout
+int|FORM_ip_conntrack_max|@TR<<Maximum Connections>>|min=512 max=32768|$FORM_ip_conntrack_max
+int|FORM_ip_conntrack_generic_timeout|@TR<<Generic Timeout>>|min=1 max=134217728|$FORM_ip_conntrack_generic_timeout
+int|FORM_ip_conntrack_icmp_timeout|@TR<<ICMP Timeout>>|min=1 max=134217728|$FORM_ip_conntrack_icmp_timeout
+int|FORM_ip_conntrack_tcp_timeout_established|@TR<<TCP Established Timeout>>|min=30 max=134217728|$FORM_ip_conntrack_tcp_timeout_established
+int|FORM_ip_conntrack_udp_timeout|@TR<<UDP Timeout>>|min=30 max=134217728|$FORM_ip_conntrack_udp_timeout
+int|FORM_ip_conntrack_udp_timeout_stream|@TR<<UDP Stream Timeout>>|min=30 max=134217728|$FORM_ip_conntrack_udp_timeout_stream
 EOF
 	equal "$?" "0" && {
-		save_setting conntrack ip_conntrack_udp_timeout_stream "$FORM_ip_conntrack_udp_timeout_stream"
-		save_setting conntrack ip_conntrack_udp_timeout "$FORM_ip_conntrack_udp_timeout"
-		save_setting conntrack ip_conntrack_tcp_timeout_established "$FORM_ip_conntrack_tcp_timeout_established"
-		save_setting conntrack ip_conntrack_max "$FORM_ip_conntrack_max"
-		save_setting conntrack ip_conntrack_generic_timeout "$FORM_ip_conntrack_generic_timeout"
-		save_setting conntrack ip_conntrack_icmp_timeout "$FORM_ip_conntrack_icmp_timeout"
+		save_setting_sysctl conntrack ip_conntrack_max "$FORM_ip_conntrack_max" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_max)"
+		save_setting_sysctl conntrack ip_conntrack_generic_timeout "$FORM_ip_conntrack_generic_timeout" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_generic_timeout)"
+		save_setting_sysctl conntrack ip_conntrack_icmp_timeout "$FORM_ip_conntrack_icmp_timeout" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_icmp_timeout)"
+		save_setting_sysctl conntrack ip_conntrack_tcp_timeout_established "$FORM_ip_conntrack_tcp_timeout_established" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established)"
+		save_setting_sysctl conntrack ip_conntrack_udp_timeout "$FORM_ip_conntrack_udp_timeout" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout)"
+		save_setting_sysctl conntrack ip_conntrack_udp_timeout_stream "$FORM_ip_conntrack_udp_timeout_stream" "$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout_stream)"
 	}
 }
 
-load_settings "conntrack"
-
-FORM_ip_conntrack_max="${ip_conntrack_max:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_max)}"
-FORM_ip_conntrack_generic_timeout="${ip_conntrack_generic_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_generic_timeout)}"
-FORM_ip_conntrack_icmp_timeout="${ip_conntrack_icmp_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_icmp_timeout)}"
-FORM_ip_conntrack_tcp_timeout_established="${ip_conntrack_tcp_timeout_established:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established)}"
-FORM_ip_conntrack_udp_timeout_stream="${ip_conntrack_udp_timeout_stream:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout_stream)}"
-FORM_ip_conntrack_udp_timeout="${ip_conntrack_udp_timeout:-$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout)}"
-
 equal "$FORM_ip_conntrack_tcp_timeout_established" "432000" && {
 	tcp_warning_text='<table width="55%"><tbody><tr><td>
-	<div class="warning">WARNING: Your default TCP established timeout value is very high (5 days). Most peer-2-peer users should lower it. A safe setting is probably 1 day (86400), though some users prefer 1 hour (3600).</div>
-	</td></tr></tbody></table>'
+<div class="warning">@TR<<big_warning#WARNING>>: @TR<<network_misc_too_bi_timeout#Your default TCP established timeout value is very high (5 days). Most peer-2-peer users should lower it. A safe setting is probably 1 day (86400), though some users prefer 1 hour (3600).>></div>
+</td></tr></tbody></table>'
 }
+
+header "Network" "Tweaks" "@TR<<Networking Tweaks>>" '' "$SCRIPT_NAME"
 
 display_form <<EOF
 start_form|@TR<<Conntrack Settings>>
