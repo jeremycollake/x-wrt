9d121cc4ae5d19d410bac6950340e57de3694a69
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-24 18:53:19 +0000

    revert sync with kamikaze, more vital packages

--- trunk/package/webif/files/www/cgi-bin/webif/system-ipkg.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/system-ipkg.sh
@@ -33,7 +33,7 @@ header "System" "Packages" "@TR<<system_ipkg_Packages#Packages>>" '' "$SCRIPT_NA
 cat <<EOF
 <script type="text/javascript">
 function confirmT(action,pkg) {
-if ( pkg == "uclibc" || pkg == "base-files" || pkg == "bridge" || pkg == "busybox" || pkg == "dnsmasq" || pkg == "dropbear" || pkg == "haserl" || pkg == "hotplug" || pkg == "iptables" || pkg == "kernel" || pkg == "mtd" || pkg == "wireless-tools" || pkg == "wlc") {
+if ( pkg == "base-files" || pkg == "base-files-brcm-2.4" || pkg == "bridge" || pkg == "busybox" || pkg == "dnsmasq" || pkg == "dropbear" || pkg == "haserl" || pkg == "hotplug" || pkg == "ipkg" || pkg == "ipkg-upgrade-fix" || pkg == "iptables" || pkg == "iwlib" || pkg == "kernel" || pkg == "kmod-brcm-wl" || pkg == "kmod-switch" || pkg == "kmod-wl-compat" || pkg == "mtd" || pkg == "nvram" || pkg == "uclibc" || pkg == "webif" || pkg == "wificonf" || pkg == "wireless-tools") {
 alert ("             <<< WARNING >>> \n\nPackage \"" + pkg + "\" should not be removed!\n\n>>> Removing may brick your router. <<<\n\nSystem requires \"" + pkg + "\" package to run.\n\n") ;
 }
 if (window.confirm("Please Confirm!\n\nDo you want to " + action + " \"" + pkg + "\" package?")){
