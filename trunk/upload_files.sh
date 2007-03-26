428b562bf247c4e0fb195a6997f6de06c43f8d2f
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-03-26 15:55:12 +0000

    bump revision number

--- trunk/upload_files.sh
+++ trunk/upload_files.sh
@@ -40,70 +40,70 @@ echo "Uploading firmware images ..."
 date > /tmp/build-date.txt
 scp \
 	/tmp/build-date.txt \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/
 #scp \
 #	../../ht_docs/howtoflash-7z.txt \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/
 md5sum imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/* > imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/md5sums.txt
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/md5sums.txt \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/default
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/default
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.7z \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/default
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/default
 rm imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*v5*.bin
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.bin \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/default
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/default
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/default/*.trx \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/default
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/default
 
 md5sum imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/* > imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/md5sums.txt
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/md5sums.txt \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/micro
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/micro
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.7z \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/micro
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/micro
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.bin \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/micro
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/micro
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/micro/*.trx \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/micro
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/micro
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.7z \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pppoe
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pppoe
 rm imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*v5*.bin
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.bin \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pppoe
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pppoe
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pppoe/*.trx \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pppoe
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pppoe
 
 md5sum imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/* > imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/md5sums.txt
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/md5sums.txt \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pptp
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pptp
 #scp \
 #	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.7z \
-#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pptp
+#	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pptp
 rm imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*v5*.bin
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.bin \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pptp
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pptp
 scp \
 	imgbuild/OpenWrt-ImageBuilder-Linux-i686/bin/pptp/*.trx \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/pptp
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/pptp
 echo "Uploading SDk and image builder ..."
 scp \
 	bin/*.tar.bz2 \
-	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/imagebuilder
+	xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/imagebuilder
 
-xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/milestone_2-5/default/
-xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/pre-0.9/latest-daily-build/
+#xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/milestone_2-5/default/
+xwrt@xwrt.kicks-ass.org:/www/xwrt/firmware_images/whiterussian/0.9/latest-daily-build/
 
 
 
