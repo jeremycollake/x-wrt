f539d19ee4c962dc385382c3a5f14ff1aec8cc35
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-03 15:43:15 +0000

    generate and store txpower values for atheros wireless cards because we can't get them without a ath interface being created.

--- kamikaze/package/webif/files/www/cgi-bin/webif/network-wlan.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/network-wlan.sh
@@ -357,10 +357,7 @@ for device in $DEVICES; do
 			append forms "$hidden" "$N"
 			
 			if [ "$iftype" = "atheros" ]; then
-			txpowers="$(iwlist ath0 txpower 2>&-|grep "Current Tx-Power" -v |grep "ath0" -v | cut -d'd' -f 1 |cut -d' ' -f 3)"
-			if [ -z "$txpowers" ];then
-				txpowers="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16"
-			fi
+			eval txpowers="\$CONFIG_wireless_${device}_txpower"
 			txpower_field="field|@TR<<Tx Power>>
 					select|txpower_$vcfg|$FORM_txpower"
 			for txpower in $txpowers; do
