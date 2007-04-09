c133a5aaa2d21f2cbda9e0a7acfdafdf16a0e1bc
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-09 16:12:33 +0000

    complete page redesign

--- trunk/package/webif/files/www/cgi-bin/webif/system-nvram.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/system-nvram.sh
@@ -1,121 +1,206 @@
 #!/usr/bin/webif-page
 <?
-#
-# credit goes to luke-jr+openwrt@utopios.org
-#
-# TODO: this page violates our CSS standards by having style defined in it.
-#
-#
 . /usr/lib/webif/webif.sh
-header "System" "NVRAM" "@TR<<NVRAM>>" '' '' ?>
+###################################################################
+# NVRAM settings page
+#
+# Description:
+#	Allows modification of the NVRAM settings.
+#
+# Author(s) [in order of work date]:
+#   OpenWrt developers (??)
+#   luke-jr+openwrt@utopios.org
+#   Lubos Stanek <lubek@users.berlios.de>
+#
+# Sorry, no credit, the page was awful:
+#   credit goes to luke-jr+openwrt@utopios.org
+#
 
+# max length of the displayed value
+MAX_VALUE_LEN=248
+
+header_inject_head=$(cat <<EOF
 <style type="text/css">
-	td {
-		vertical-align: top;
-		padding-left: 1em;
-	}
+<!--
+#nvramtable * {
+}
+#nvramtable table {
+	width: 96%;
+	margin-left: auto;
+	margin-right: auto;
+	font-size: 0.9em;
+}
+#nvramtable td {
+	padding: 0;
+}
+#nvramtable .nr {
+	text-align: right;
+	font-size: 0.7em;
+	padding-left: 2px;
+}
+-->
 </style>
 
-<div class="half noBorderOnLeft">
-<?
-MAX_VALUE_LEN=60
-myself=$(basename "$SCRIPT_NAME")
-live=false
+EOF
+)
 
-echo '<form method="POST" action="'"$myself"'">'
+# add new variable name
+! empty "$FORM_action_new" && {
+	validate <<EOF
+string|FORM_nvramvar|@TR<<system_nvram_New_Variable#New Variable Name>>|required nospaces|$FORM_nvramvar
+EOF
+	equal "$?" 0 && {
+		FORM_action_add="$FORM_action_new"
+		FORM_nvramvar="$FORM_new_nvramvar"
+		unset FORM_action_new
+	}
+}
+# delete variable
+! equal "$FORM_action_delete" "" && ! equal "$FORM_confirm" "" && {
+	SAVED=1
+	#save_setting system "$FORM_nvramvar" ""
+	nvram unset "$FORM_nvramvar" 2>/dev/null
+	nvram commit
+	unset FORM_action_delete
+	unset FORM_confirm
+}
+# change value
+! equal "$FORM_action_change" "" && ! equal "$(set | grep FORM_newvalue)" "" && {
+	SAVED=1
+	#sanitize - it needs more work and sync with save_settings!
+	#temp_value=$(echo "$FORM_newvalue" | sed 's/"/\\"/;')
+	save_setting system "$FORM_nvramvar" "$FORM_newvalue"
+	unset temp_value
+	unset FORM_action_change
+}
 
-cancel="<a href=\"$myself\" style=\"border: 1px solid; background-color: grey; padding: 0.4ex 1em; margin: 0; color: black;\">Cancel</a>"
-if [ "$FORM_action" = 'Delete' ]; then
-	echo '<h3>@TR<<Delete NVRAM Setting>></h3>'
-	echo '<p style="margin-top: 1em; margin-bottom: 1em;">'
-	if [ "$FORM_confirm" = 'yes' ]; then
-		if $live; then
-			if nvram unset $FORM_var; then
-				echo "<strong>$FORM_var</strong> has been deleted."
-			else
-				echo "<br /><br />ERROR: Unable to delete <strong>$FORM_var</strong>!"
-			fi
-		else
-			save_setting system "$FORM_var" ""
-			echo "When changes are applied, <strong>$FORM_var</strong> will be deleted."
-		fi
-		echo '</p>'
-		echo "<p><a href=\"$myself\">Return to setting list</a></p>"
+header "System" "system_nvram_NVRAM#NVRAM" "@TR<<system_nvram_NVRAM#NVRAM>>"
+
+echo "<form enctype=\"multipart/form-data\" action=\"$SCRIPT_NAME\" method=\"post\">"
+
+if [ "$FORM_action_delete" != "" ]; then
+	FORM_nvramvalue=$(nvram get "$FORM_nvramvar" 2>/dev/null)
+	display_form <<EOF
+start_form|@TR<<system_nvram_Delete_Setting#Delete NVRAM Setting>>
+field|@TR<<system_nvram_Variable_Name#Variable Name>>
+text|nvramvar|$FORM_nvramvar||readonly="readonly"
+field|@TR<<system_nvram_Variable_Value#Variable Value>>
+text|nvramvalue|$FORM_nvramvalue||readonly="readonly"
+string|<input type="hidden" name="confirm" value="yes" />
+field|@TR<<system_nvram_Are_you_sure#Are you sure you wish to delete the variable&#63;>>
+submit|action_delete|@TR<<system_nvram_action_Delete#Delete>>
+submit|action_cancel|@TR<<system_nvram_action_Cancel#Cancel>>
+helpitem|system_nvram_Delete_Setting#Delete NVRAM Setting
+helptext|system_nvram_Delete_Setting_helptext#The configuration system is not equipped with functions for deleting the NVRAM variable. Therefore the variable is deleted by this script immediately after pressing the button. Be sure you know what you are doing before pressing the button!
+end_form
+EOF
+elif [ "$FORM_action_change" != "" ] || [ "$FORM_action_add" != "" ]; then
+	FORM_newvalue=$(nvram get "$FORM_nvramvar" 2>/dev/null)
+	if [ "$FORM_newvalue" == "" ]; then
+		form_heading="@TR<<system_nvram_Add_New_NVRAM_Setting#Add New NVRAM Setting>>"
 	else
-		echo "Are you sure you wish to delete <strong>$FORM_var</strong>?"
-		echo '</p>'
-		echo "$cancel"
-		echo '<input type="hidden" name="var" value="'"$FORM_var"'" />'
-		echo '<input type="hidden" name="confirm" value="yes" />'
-		echo '<input type="submit" name="action" value="Delete" />'
+		form_heading="@TR<<system_nvram_Change_NVRAM_Setting#Change NVRAM Setting>>"
 	fi
-elif [ "$FORM_action" = 'Change' ] || [ "$FORM_action" = 'Add' ]; then
-	value=$(nvram get "$FORM_var")
-	if [ "$value" = '' ]; then
-		echo '<h3>@TR<<Add New NVRAM Setting>></h3>'
-	else
-		echo '<h3>@TR<<Change NVRAM Setting>></h3>'
-	fi
-	if [ "$FORM_newvalue" = '' ]; then
-		echo "<div>Set <strong>$FORM_var</strong> to:<br />"
-		echo '<input type="hidden" name="var" value="'"$FORM_var"'" />'
-		echo "<textarea style=\"width: 90%; height: 10em;\" name=\"newvalue\">$value</textarea><br />"
-		echo '<div style="width: 90%; text-align: right;">'
-		echo "$cancel"
-		echo '<input type="submit" name="action" value="Change" />'
-		echo '</div>'
-	else
-		if $live; then
-			if nvram set "$FORM_var"="$FORM_newvalue"; then
-				echo "<strong>$FORM_var</strong> set to:<pre>$FORM_newvalue</pre>"
-			else
-				echo "<br /><br />ERROR: Unable to set <strong>$FORM_var</strong>!"
-			fi
-		else
-			save_setting system "$FORM_var" "$FORM_newvalue"
-			echo "When changes are applied, <strong>$FORM_var</strong> will be set to:<pre>$FORM_newvalue</pre>"
-		fi
-		echo '</p>'
-		echo "<p><a href=\"$myself\">Return to setting list</a></p>"
-	fi
+	display_form <<EOF
+start_form|$form_heading
+field|@TR<<system_nvram_Variable_Name#Variable Name>>
+text|nvramvar|$FORM_nvramvar||readonly="readonly"
+field|<strong>@TR<<system_nvram_Variable_Value#Variable Value>></strong>
+text|newvalue|$FORM_newvalue||style="width: 100%"
+field|&nbsp;
+submit|action_change|@TR<<system_nvram_action_Change#Change>>
+submit|action_cancel|@TR<<system_nvram_action_Cancel#Cancel>>
+end_form
+EOF
 else
-	echo '<h3>@TR<<General System Settings>></h3>'
-	echo '<br><font color="red" size=-1>@TR<<warn_nvram#Warning: Changing these settings may result in permanent damage to your router.>></font><br><br>'
-	if $live; then
-		echo '<h4>Changes take effect immediately.</h4><br>'
-	else
-		echo '<h4>@TR<<inform_apply#Changes will not take effect until you choose "Apply Changes">></h4><br>'
-	fi
-	echo '<table style="width: 90%; margin: auto;">'
-	echo '<tr><th>Setting Variable</th><th>Current Value</th></tr>'
-	actions='Change Delete'
-	echo '<tr><td><input style="width: 100%;" name="var"></td><td style="vertical-align: middle;"><em style="font-size: 75%;">New Variable</em></td>'
-	echo '<td colspan="2"><input type="submit" name="action" value="Add" style="width: 100%;" /></td>'
-	echo '</tr>'
+	display_form <<EOF
+start_form|@TR<<system_nvram_Add_System_Setting#Add System Setting>>
+field|@TR<<system_nvram_New_Variable#New Variable Name>>
+text|new_variable|$FORM_new_variable
+field|&nbsp;
+submit|add_variable|@TR<<system_nvram_Add_new_variable#Add new variable>>
+helpitem|system_nvram_NVRAM#NVRAM
+helptext|system_nvram_NVRAM_helptext#NVRAM stands for Non-Volatile RAM, in this case the last 64K of the flash chip used to store various configuration information in a name=value format.
+end_form
+EOF
+	cat << EOF
+<div class="settings">
+<h3>@TR<<system_nvram_General_System_Settings#General System Settings>></h3>
+<h4 class="warning">@TR<<Warning>>: @TR<<system_nvram_warn_nvram#Changing these settings may result in permanent damage to your device.>></h4>
+<h4>@TR<<system_nvram_inform_apply#Changes will not take effect until you choose "Apply Changes".>></h4><br />
 
-	nvram show 2>/dev/null | sed 's,^\([^=]\+\)=.*$,\1,;t;d' | sed '/eou_private_key/d' | sed '/eou_public_key/d' | sed '/sdram_/d' | sort |
-	while IFS='=' read name; do
-		value=$(nvram get "$name")		
-		empty "$value" && continue;
-		value_size=$(echo "$value" | wc -c)				
-		[ "$value_size" -gt $MAX_VALUE_LEN ] && {
-			value="$(echo "$value" | cut -c1-$MAX_VALUE_LEN)..."
+<div id="nvramtable">
+<table>
+<tbody>
+<tr>
+	<th class="var">@TR<<system_nvram_th_Change_Variable#Change Variable>></th>
+	<th>&nbsp;</th>
+	<th>@TR<<system_nvram_th_Current_Value#Current Value>></th>
+	<th>@TR<<system_nvram_th_Action#Action>></th>
+	<th class="nr">@TR<<system_nvram_th_Nr#Nr.>></th>
+</tr>
+EOF
+	nvram show 2>/dev/null | sort | awk -F "=" \
+		-v max_val_len="$MAX_VALUE_LEN" -v url="$SCRIPT_NAME" '
+	BEGIN {
+		odd = 1
+	}
+	{
+		if (odd == 1) {
+			print tr_ind "<tr>"
+			odd--
+		} else {
+			print tr_ind "<tr class=\"odd\">"
+			odd++
 		}
-		echo '<tr>'
-		echo "<td>$name</td>"
-		echo "<td><code>$value</code></td>"
-		for action in $actions; do
-			echo "<td><a href=\"$myself?action=$action&var=$name\">$action</a></td>"
-		done
-		echo "</tr>"
-	done
-	echo '	</table>'
+		value=$2
+		for (i=3; i<=NF; i++) value=value "=" $i
+		if (length(value) > max_val_len) value=substr(value, 1, max_val_len) "..."
+		if ((length(value) == 0) || (value == " ")) value="&nbsp;"
+		else {
+			gsub(/&/, "&amp;", value)
+			gsub(/</, "&lt;", value)
+			gsub(/>/, "&gt;", value)
+		}
+		print "<td><a href=\"" url "?action_change=Change&amp;nvramvar=" $1 "\" title=\"@TR<<system_nvram_Change_the_value#Change the value>>\">" $1 "</a></td>"
+		print "<td>=</td>"
+		print "<td>" value "</td>"
+		print "<td><a href=\"" url "?action_delete=Delete&amp;nvramvar=" $1 "\" title=\"@TR<<system_nvram_Delete_the_variable#Delete the variable>>\">@TR<<system_nvram_action_Delete#Delete>></a></td>"
+		print "<td class=\"nr\">" NR "</td>"
+		print "</tr>"
+	}
+	END {
+		if (NR < 1) print "<td colspan=\"4\">&nbsp;</td>"
+	}
+'
+	cat << EOF
+</tbody>
+</table>
+</div>
+</div>
+<br />
+EOF
+
+	nvram_totals=$(nvram show 2>&1 | sed '/^size: /!d')
+	USED_NVRAM=$(echo "$nvram_totals" | sed 's/size: \([[:digit:]]\{1,5\}\).*/\1/')
+	FREE_NVRAM=$(echo "$nvram_totals" | sed 's/.*(\([[:digit:]]\{1,5\}\) .*).*/\1/')
+	[ "$USED_NVRAM" -ge 0 ] >/dev/null 2>&1 &&  [ "$FREE_NVRAM" -ge 0 ] >/dev/null 2>&1 && {
+		TOTAL_NVRAM=$(($USED_NVRAM + $FREE_NVRAM))
+		NVRAM_PERCENT_USED=$((100 -($FREE_NVRAM/($TOTAL_NVRAM/100))))
+
+		display_form <<EOF
+start_form|@TR<<system_nvram_NVRAM_Usage#NVRAM Usage>>
+string|<tr><td>@TR<<system_nvram_Total#Total>>: $TOTAL_NVRAM B</td><td>
+progressbar|nvramuse|@TR<<system_nvram_Used#Used>>: $USED_NVRAM B ($NVRAM_PERCENT_USED%)|200|$MEM_PERCENT_USED|$NVRAM_PERCENT_USED%||
+helpitem|system_nvram_NVRAM_Usage#NVRAM Usage
+helptext|system_nvram_NVRAM_Usage_helptext#This is the current NVRAM usage. The graph shows how much memory is available for other variables.
+end_form
+EOF
+	}
 fi
 ?>
 </form>
-</div>
-
 <? footer ?>
 <!--
-##WEBIF:name:System:190:NVRAM
+##WEBIF:name:System:190:system_nvram_NVRAM#NVRAM
 -->
