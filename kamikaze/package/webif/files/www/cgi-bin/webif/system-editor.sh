35701d5db0fe81c50939afd579bb43359c83d9db
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-16 10:35:29 +0000

    fix shell escapes in javascript functions, thanks pier11

--- kamikaze/package/webif/files/www/cgi-bin/webif/system-editor.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/system-editor.sh
@@ -42,7 +42,7 @@ var webif_printf=function() {
 	var num = arguments.length;
 	var output = arguments[0];
 	for (var i = 1; i < num; i++) {
-		var pattern = "\\\{" + (i-1) + "\\\}";
+		var pattern = "\\\\{" + (i-1) + "\\\\}";
 		var re = new RegExp(pattern, "g");
 		output = output.replace(re, arguments[i]);
 	}
--- kamikaze/package/webif/files/www/cgi-bin/webif/system-startup.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/system-startup.sh
@@ -45,7 +45,7 @@ var webif_printf=function() {
 	var num = arguments.length;
 	var output = arguments[0];
 	for (var i = 1; i < num; i++) {
-		var pattern = "\\\{" + (i-1) + "\\\}";
+		var pattern = "\\\\{" + (i-1) + "\\\\}";
 		var re = new RegExp(pattern, "g");
 		output = output.replace(re, arguments[i]);
 	}
