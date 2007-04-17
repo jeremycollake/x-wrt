58e5737f8b3b4f12322e1fde64177573b9e42032
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-17 22:00:41 +0000

    svggraphs prepared for the translation

--- trunk/package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
@@ -6,14 +6,13 @@
 . /usr/lib/webif/webif.sh
 . ./graphs-subcategories.sh
 
-header "Graphs" "CPU" "@TR<<CPU Usage>>" "" "$SCRIPT_NAME"
+header "Graphs" "graphs_cpu_subcategory#CPU" "@TR<<graphs_cpu_CPU_Usage#CPU Usage>>" "" ""
 ?>
 <center>
-	<embed src="/svggraph/graph_cpu.svg?/cgi-bin/webif/data.sh"
-		width="500" height="250" type="image/svg+xml"
-	/>
+	<object data="/cgi-bin/webif/graph_cpu_svg.sh?data.sh"
+		width="500" height="250" type="image/svg+xml">@TR<<graphs_svg_required#This object requires the SVG support.>></object>
 </center>
 <? footer ?>
 <!--
-##WEBIF:name:Graphs:1:CPU
+##WEBIF:name:Graphs:1:graphs_cpu_subcategory#CPU
 -->
