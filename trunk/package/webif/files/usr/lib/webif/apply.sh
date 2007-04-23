27c7b43b387b979dab0d49091b624fda152c90bb
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-23 15:48:06 +0000

    redesigned apply

--- trunk/package/webif/files/usr/lib/webif/apply.sh
+++ trunk/package/webif/files/usr/lib/webif/apply.sh
@@ -14,96 +14,163 @@
 . /lib/config/uci.sh
 cron_init="/etc/init.d/S60cron"
 
+# log apply messages to syslog
+apply_enable_logging="0"
+
 HANDLERS_config='
-	wireless) reload_wireless;;
-	network) reload_network;;
-	system) reload_system;;
-	cron) reload_cron;;
-	syslog) reload_syslog;;
-	wifi-enable) reload_wifi_enable;;
-	wifi-disable) reload_wifi_disable;;
-	pptp) reload_pptp;;
-	log) reload_log;;
+	cron) restart_cron;;
 	ezipupdate) reload_ezipupdate;;
+	log) reload_log;;
+	network) reload_network;;
+	pptp) reload_pptp;;
 	snmp) reload_snmp;;
+	syslog) reload_syslog;;
+	system) reload_system;;
+	wifi-disable) reload_wifi_disable;;
+	wifi-enable) reload_wifi_enable;;
+	wireless) reload_wireless;;
 '
 
 HANDLERS_file='
-	hosts) rm -f /etc/hosts; mv $config /etc/hosts; killall -HUP dnsmasq ;;
-	ethers) rm -f /etc/ethers; mv $config /etc/ethers; killall -HUP dnsmasq ;;
-	firewall) mv /tmp/.webif/file-firewall /etc/config/firewall && /etc/init.d/S??firewall;;
-	dnsmasq.conf) mv /tmp/.webif/file-dnsmasq.conf /etc/dnsmasq.conf && /etc/init.d/S??dnsmasq;;
+	dnsmasq.conf) mv /tmp/.webif/file-dnsmasq.conf /etc/dnsmasq.conf && restart_dnsmasq;;
+	ethers) rm -f /etc/ethers; mv $config /etc/ethers; reload_dnsmasq;;
+	firewall) mv /tmp/.webif/file-firewall /etc/config/firewall && reload_firewall;;
+	hosts) rm -f /etc/hosts; mv $config /etc/hosts; reload_dnsmasq;;
 '
+
+HANDLERS_edited_file='
+	/etc/dnsmasq.conf) restart_dnsmasq;;
+	/etc/ethers) reload_dnsmasq;;
+	/etc/firewall.user) reload_firewall;;
+	/etc/hosts) reload_dnsmasq;;
+	/etc/config/firewall) reload_firewall;;
+	/etc/crontabs/root) restart_cron;;
+'
+
+HANDLERS_uci='
+	qos) apply_uci_qos;;
+	upnpd) reload_upnpd;;
+	webif) apply_uci_webif;;
+'
+
+# common messaging functions
+log_message() {
+	[ -z "$2" ] && {
+		echo "$1"
+	} || {
+		echo -n "$1"
+	}
+	[ "$apply_enable_logging" = "1" ] && logger -t "webif^2-apply" "$(echo "$1" | sed 's/@TR<<\([^#>]\)*#//g; s/@TR<<//g; s/>>//g;')"
+}
+echo_reloading_settings() {
+	log_message "@TR<<apply_Reloading_settings_for#Reloading settings for>>: $1" "$2"
+}
+echo_exporting_settings() {
+	log_message "@TR<<apply_Exporting_settings_for#Exporting settings for>>: $1" "$2"
+}
+echo_applying_settings() {
+	log_message "@TR<<apply_Applying_settings_for#Applying settings for>>: $1" "$2"
+}
+echo_restarting_service() {
+	log_message "@TR<<apply_Restarting_service#Restarting service>>: $1" "$2"
+}
+echo_stopping_service() {
+	log_message "@TR<<apply_Stopping_service#Stopping service>>: $1" "$2"
+}
+echo_processing_file() {
+	log_message "@TR<<apply_Processing_file#Processing file>>: $1" "$2"
+}
+echo_processing_config() {
+	log_message "@TR<<apply_Processing_config_file#Processing config file>>: $1" "$2"
+}
+echo_action_done() {
+	log_message "@TR<<apply_Done#Done>>." "$2"
+}
 
 # for some reason a for loop with "." doesn't work
 eval "$(cat /usr/lib/webif/apply-*.sh 2>&-)"
 
+reload_dnsmasq() {
+	echo_reloading_settings "@TR<<apply_dnsmasq#dnsmasq>>"
+	killall -q -HUP dnsmasq
+	[ -z "$(ps | grep "[d]nsmasq ")" ] && /etc/init.d/S??dnsmasq
+	echo_action_done
+}
+restart_dnsmasq() {
+	echo_restarting_service "@TR<<apply_dnsmasq#dnsmasq>>"
+	killall -q dnsmasq
+	/etc/init.d/S??dnsmasq
+	echo_action_done
+}
 reload_wifi_enable() {
+	echo_applying_settings "@TR<<apply_wifi_enable#wifi enable>>"
 	ifup lan
 	ifup wifi
-	killall dnsmasq
-	/etc/init.d/S??dnsmasq
+	restart_dnsmasq
+	echo_action_done
 }
 
 reload_wifi_disable() {
+	echo_applying_settings "@TR<<apply_wifi_disable#wifi disable>>"
 	ifup lan
-	killall dnsmasq
-	/etc/init.d/S??dnsmasq
+	restart_dnsmasq
+	echo_action_done
 }
 
 reload_network() {
-	echo '@TR<<Reloading>> @TR<<networking settings>> ...'
+	echo_reloading_settings "@TR<<apply_network#network>>"
 	grep '^wan_' config-network >&- 2>&- && {
 		ifdown wan
 		ifup wan
-		killall -HUP dnsmasq
+		reload_dnsmasq
 	}
 
 	grep '^lan_' config-network >&- 2>&- && {
 		ifdown lan
 		ifup lan
-		killall dnsmasq
-		/etc/init.d/S??dnsmasq
+		restart_dnsmasq
 	}
+	echo_action_done
 }
 
 reload_wireless() {
-	echo '@TR<<Reloading>> @TR<<wireless settings>> ...'
+	echo_reloading_settings "@TR<<apply_wireless#wireless>>"
 	killall nas >&- 2>&- && sleep 2
 	(
 		/sbin/wifi
 		[ -f /etc/init.d/S41wpa ] && /etc/init.d/S41wpa
 	) >&- 2>&- <&-
+	echo_action_done
 }
 
-reload_cron() {
-	echo '@TR<<Reloading Cron>> ...'
+reload_firewall() {
+	echo_applying_settings "@TR<<apply_firewall#firewall>>"
+	/etc/init.d/S??firewall
+	echo_action_done
+}
+
+restart_cron() {
 	# (re)start crond
-	if [ -x $cron_init ]; then
-		echo "(Re)start cron..."
+	if [ -x "$cron_init" ]; then
+		echo_restarting_service "@TR<<apply_cron#cron>>"
 		$cron_init restart
+		echo_action_done
 	fi
 }
 
 reload_syslog() {
 	getPID(){
-		echo `ps -elf | grep 'syslogd' | grep -v grep | awk '{ print $1 }'`
+		echo `ps -elf | grep '[s]yslogd' | awk '{ print $1 }'`
 	}
 	# (re)start syslogd
-	echo "(Re)start syslogd..."
+	echo_restarting_service "@TR<<apply_syslogd#syslogd>>"
 	pid=$(getPID)
-	if [ -n "$pid" ]; then
-		echo -n "Stopping syslogd: "
-		( {
-			kill $pid >/dev/null 2>&1
-		} && echo "OK" ) || echo "ERROR"
-	fi
-	echo -n "Start syslogd: "
+	[ -n "$pid" ] && kill $pid >/dev/null 2>&1
 	syslog_ip=$(nvram get log_ipaddr)
 	if [ "$(nvram get firmware_version)" = "0.9" ]; then
-	ipcalc.sh -s "$syslog_ip" || syslog_ip=""
+		ipcalc.sh -s "$syslog_ip" || syslog_ip=""
 	else
-	ipcalc -s "$syslog_ip" || syslog_ip=""
+		ipcalc -s "$syslog_ip" || syslog_ip=""
 	fi
 	log_port=$(nvram get log_port)
 	log_port=${log_port:+:$log_port}
@@ -114,27 +181,27 @@ reload_syslog() {
 	[ -z "$can_prefix" ] || log_prefix=$(nvram get log_prefix)
 	log_prefix=${log_prefix:+-P "$log_prefix"}
 	syslogd -C 16 ${syslog_ip:+-L -R $syslog_ip$log_port} $log_mark $log_prefix
-
-	echo "OK"
+	echo_action_done
 }
 
 reload_system() {
-	echo '@TR<<Applying>> @TR<<system settings>> ...'
+	echo_applying_settings "@TR<<apply_system#system>>"
 	echo "$(nvram get wan_hostname)" > /proc/sys/kernel/hostname
 	grep '_admin' config-system >&- 2>&- && {
-		echo '@TR<<Reloading>> @TR<<firewall settings>> ...'
-		/etc/init.d/S??firewall
+		reload_firewall
 	}
+	echo_action_done
 }
 
 reload_upnpd() {
-	echo '@TR<<Reloading>> @TR<<UPNPd>> ...'
+	echo_restarting_service "@TR<<apply_upnpd#upnpd>>"
 	killall miniupnpd upnpd 2>&- >&-
 	exists "/etc/init.d/S95miniupnpd" && /etc/init.d/S95miniupnpd
 	exists "/etc/init.d/S65upnpd" && {
 		/etc/init.d/S65upnpd stop 2>&- >&-
 		/etc/init.d/S65upnpd start
 	}
+	echo_action_done
 }
 
 reload_ezipupdate() {
@@ -155,7 +222,7 @@ reload_ezipupdate() {
 			ddns_max_interval=$(nvram get ddns_max_interval)
 			ddns_wildcard=$(nvram get ddns_wildcard)
 		else
-			echo "ERROR: ez-ipupdate config apply not updated for non-nvram systems."
+			log_message "@TR<<aplly_Error#Error>>: @TR<<apply_ez-ipupdate_config_not_updated#ez-ipupdate config apply not updated for non-nvram systems.>>"
 		fi
 
 		# (re)start ez-ipupdated
@@ -177,9 +244,13 @@ reload_ezipupdate() {
 		[ -f $ddns_cache ] && rm -f $ddns_msg
 			echo "(Re)start DynDNS ez-ipupdate" > $ddns_msg
 		
+			echo_restarting_service "@TR<<apply_ez-ipupdate#ez-ipupdate>>"
 			$initfile restart >&- 2>&- &
+			echo_action_done
 		else		
+			echo_stopping_service "@TR<<apply_ez-ipupdate#ez-ipupdate>>"
 			$initfile stop >&- 2>&- &
+			echo_action_done
 		fi
 	}
 }
@@ -191,12 +262,16 @@ cd "/tmp/.webif"
 # edited-files/*		user edited files - stored with directory tree in-tact
 for edited_file in $(find "/tmp/.webif/edited-files/" -type f 2>&-); do
 	target_file=$(echo "$edited_file" | sed s/'\/tmp\/.webif\/edited-files'//g)
-	echo "@TR<<Processing>> $target_file"
+	echo_processing_file "$target_file"
 	fix_symlink_hack "$target_file"
 	if tr -d '\r' <"$edited_file" >"$target_file"; then
 		rm "$edited_file" 2>&-
+		echo_action_done
+		eval 'case "$target_file" in
+			'"$HANDLERS_edited_file"'
+		esac'
 	else
-		echo "@TR<<Critical Error>> : @TR<<Could not replace>> $target_file. @TR<<Media full>>?"
+		log_message "@TR<<aplly_Error#Error>>: @TR<<apply_Could_not_replace#Could not replace>> $target_file. @TR<<apply_Media_full#Media full>>?"
 	fi
 done
 # leave if some files not applied
@@ -205,130 +280,95 @@ rm -r "/tmp/.webif/edited-files" 2>&-
 # file-*		other config files
 for config in $(ls file-* 2>&-); do
 	name=${config#file-}
-	echo "@TR<<Processing>> @TR<<config file>>: $name"
+	echo_processing_config "$name"
 	eval 'case "$name" in
 		'"$HANDLERS_file"'
 	esac'
+	echo_action_done
 done
 
 # config-wifi-enable		QOS Config file
 for config in $(ls config-wifi-enable 2>&-); do
+	echo_applying_settings "@TR<<apply_wifi_enable#wifi enable>>"
 	ifdown wifi
 	ifdown lan
+	echo_action_done
 done
 
 # config-wifi-disable		QOS Config file
 for config in $(ls config-wifi-disable 2>&-); do
+	echo_applying_settings "@TR<<apply_wifi_disable#wifi disable>>"
 	ifdown wifi
 	br_int=$(nvram get wifi_ifname)
 	brctl delbr $br_int
 	ifdown lan
+	echo_action_done
 done
 
 # config-conntrack	  Conntrack Config file
 for config in $(ls config-conntrack 2>&-); do
-	echo '@TR<<Applying>> @TR<<conntrack settings>> ...'
+	echo_applying_settings "@TR<<apply_conntrack#conntrack>>"
 	fix_symlink_hack "/etc/sysctl.conf"
 	# set any and all net.ipv4.netfilter settings.
 	for conntrack in $(grep ip_ /tmp/.webif/config-conntrack); do
 		variable_name=$(echo "$conntrack" | cut -d '=' -f1)
 		variable_value=$(echo "$conntrack" | cut -d '"' -f2)
-		echo "&nbsp;@TR<<Setting>> $variable_name to $variable_value"
+		echo "&nbsp;$variable_name=$variable_value"
 		remove_lines_from_file "/etc/sysctl.conf" "net.ipv4.netfilter.$variable_name"
 		echo "net.ipv4.netfilter.$variable_name=$variable_value" >> /etc/sysctl.conf
 	done
 	sysctl -p 2>&- 1>&- # reload sysctl.conf
 	rm -f /tmp/.webif/config-conntrack
-	echo '@TR<<Done>>'
+	echo_action_done
 done
 
-# init_theme - initialize a new theme
-init_theme() {
-	echo '@TR<<Initializing theme ...>>'	
-	uci_load "webif"
-	newtheme="$CONFIG_theme_id"	
-	# if theme isn't present, then install it		
-	! exists "/www/themes/$newtheme/webif.css" && {
-		install_package "webif-theme-$newtheme"	
-	}
-	if ! exists "/www/themes/$newtheme/webif.css"; then
-		# if theme still not installed, there was an error
-		echo "@TR<<Error>>: @TR<<installing theme package>>."
-	else
-		# create symlink to new active theme if its not already set right
-		current_theme=$(ls /www/themes/active -l | cut -d '>' -f 2 | sed s/'\/www\/themes\/'//g)
-		! equal "$current_theme" "$newtheme" && {
-			rm /www/themes/active
-			ln -s /www/themes/$newtheme /www/themes/active
-		}
-	fi		
-	echo '@TR<<Done>>'
-}
-
 reload_snmp() {
-	echo '@TR<<Exporting>> @TR<<snmp settings>> ...'
 	[ -e "/bin/save_snmp" ] && {
+		echo_exporting_settings "@TR<<apply_snmp#snmp>>"
 		/bin/save_snmp >&- 2>&-
+		echo_action_done
 	}
 
-	echo '@TR<<Reloading>> @TR<<snmp settings>> ...'
+	echo_restarting_service "@TR<<apply_snmp#snmp>>"
 	[ ! -e "/etc/init.d/S??snmpd" ] && {
 		ln -s "/etc/init.d/snmpd" "/etc/init.d/S92snmpd" 2>/dev/null
 	}
 	/etc/init.d/S??snmpd restart >&- 2>&-
+	echo_action_done
 }
 
 reload_pptp() {
-	echo '@TR<<Reloading>> @TR<<PPTP settings>> ...'
 	grep '_cli' config-pptp >&- 2>&- && [ -e /etc/init.d/S??pptp ] && {
+		echo_restarting_service "@TR<<apply_pptp#pptp>>"
 		/etc/init.d/S??pptp stop >&- 2>&-
 		/etc/init.d/S??pptp start >&- 2>&-
+		echo_action_done
 	}
 	grep '_srv' config-pptp >&- 2>&- && [ -e /etc/init.d/S??pptpd ] && {
+		echo_restarting_service "@TR<<apply_pptpd#pptpd>>"
 		/etc/init.d/S??pptpd stop  >&- 2>&-
 		/etc/init.d/S??pptpd start >&- 2>&-
+		echo_action_done
 	}
 }
 
 reload_log() {
-	echo '@TR<<Reloading syslogd>> ...'
+	echo_restarting_service "@TR<<apply_syslogd#syslogd>>"
 	killall syslogd >&- 2>&- <&-
 	/sbin/runsyslogd >&- 2>&- <&-
-}
-
-# switch_language (old_lang)  - switches language if changed
-switch_language() {
-	oldlang="$1"
-	uci_load "webif"
-	newlang="$CONFIG_general_lang"
-	! equal "$newlang" "$oldlang" && {
-		echo '@TR<<Applying>> @TR<<Installing language pack>> ...'
-		# if not English then we install language pack
-		! equal "$newlang" "en" && {
-			# build URL for package
-			#  since the original webif may be installed to, have to make sure we get latest ver
-			webif_version=$(ipkg status webif | awk '/Version:/ { print $2 }')
-			xwrt_repo_url=$(cat /etc/ipkg.conf | grep X-Wrt | cut -d' ' -f3)
-			# always install language pack, since it may have been updated without package version change
-			ipkg install "${xwrt_repo_url}/webif-lang-${newlang}_${webif_version}_mipsel.ipk" -force-reinstall -force-overwrite | uniq
-			# switch to it if installed, even old one, otherwise return to previous
-			if equal "$(ipkg status "webif-lang-${newlang}" |grep "Status:" |grep " installed" )" ""; then
-				echo '@TR<<Error installing language pack>>!'
-			fi
-		}
-		echo '@TR<<Done>>'
-	}
+	echo_action_done
 }
 
 # config-*		simple config files
 (
 	cd /proc/self
 	cat /tmp/.webif/config-* 2>&- | grep '=' >&- 2>&- && {
+		log_message "@TR<<apply_Committing_NVRAM#Committing NVRAM>>"
 		exists "/usr/sbin/nvram" && {
 			cat /tmp/.webif/config-* 2>&- | tee fd/1 | xargs -n1 nvram set	
-			echo "@TR<<Committing>> NVRAM ..."
 			nvram commit
 		}
+		echo_action_done
 	}
 )
 
@@ -339,32 +379,101 @@ for config in $(ls config-* 2>&-); do
 	esac'
 done
 
+uci_load_originals() {
+	local cfsection
+	config_load "$1"
+	for cfsection in $CONFIG_SECTIONS; do
+		config_rename "$cfsection" "orig_$cfsection"
+	done
+	CONFIG_orig_SECTION="$CONFIG_SECTIONS"
+}
+
+uci_unset_originals() {
+	local cfsection
+	local oldvar
+	for cfsection in $CONFIG_orig_SECTION; do
+		for oldvar in $(set | grep "^CONFIG_${cfsection}_" | sed -e 's/\(.*\)=.*$/\1/'); do
+			unset "$oldvar"
+		done
+	done
+	unset CONFIG_orig_SECTION
+}
+
+# switch languages if changed
+switch_language() {
+	! equal "$CONFIG_general_lang" "$CONFIG_orig_general_lang" && {
+		# if not English then we install language pack
+		! equal "$CONFIG_general_lang" "en" && {
+			log_message "@TR<<apply_Installing_language_pack#Installing language pack>>"
+			# build URL for package
+			#  since the original webif may be installed to, have to make sure we get latest ver
+			webif_version=$(ipkg status webif | awk '/Version:/ { print $2 }')
+			xwrt_repo_url=$(cat /etc/ipkg.conf | grep -i "^src[[:space:]]*X-Wrt[[:space:]]*" | cut -d' ' -f3)
+			# always install language pack, since it may have been updated without package version change
+			ipkg install "${xwrt_repo_url}/webif-lang-${newlang}_${webif_version}_mipsel.ipk" -force-reinstall -force-overwrite | uniq
+			if equal "$(ipkg status "webif-lang-${newlang}" |grep "Status:" |grep " installed" )" ""; then
+				log_message "@TR<<apply_Error_installing_language#Error installing language pack>>!"
+				uci_set "webif" "general" "lang" "en"
+				uci_commit "webif"
+			fi
+			echo_action_done
+		}
+	}
+}
+
+# init_theme - initialize a new theme
+init_theme() {
+	! equal "$CONFIG_theme_id" "$CONFIG_orig_theme_id" && {
+		log_message "@TR<<apply_Initializing_theme#Initializing theme>>"
+		# if theme isn't present, then install it		
+		! exists "/www/themes/$CONFIG_theme_id/webif.css" && {
+			install_package "webif-theme-$CONFIG_theme_id"	
+		}
+		if ! exists "/www/themes/$CONFIG_theme_id/webif.css"; then
+			# if theme still not installed, there was an error
+			log_message "@TR<<aplly_Error#Error>>: @TR<<aplly_theme_installation_failed#theme package installation failed>>."
+		else
+			# create symlink to new active theme if its not already set right
+			current_theme=$(ls /www/themes/active -l | cut -d '>' -f 2 | sed s/'\/www\/themes\/'//g)
+			! equal "$current_theme" "$CONFIG_theme_id" && {
+				rm /www/themes/active
+				ln -s /www/themes/$CONFIG_theme_id /www/themes/active
+			}
+		fi		
+		echo_action_done
+	}
+}
+
+apply_uci_webif() {
+	switch_language
+	init_theme
+	if exists "/etc/init.d/S??webif_deviceid"; then
+		/etc/init.d/S??webif_deviceid
+	fi
+	if exists "/etc/init.d/S??opendns"; then
+		/etc/init.d/S??opendns restart
+	fi
+}
+
+apply_uci_qos() {
+	echo_applying_settings "@TR<<apply_qos#qos>>"
+	qos-start
+	echo_action_done
+}
+
 #
 # now apply any UCI config changes
 #
-for package in $(ls /tmp/.uci/* 2>&-); do
-	# store original language before committing new one so we know if changed
-	equal "$package" "webif" && {
-		uci_load "webif"
-		oldlang="$CONFIG_general_lang"
-	}
-	echo "@TR<<Committing>> ${package#/tmp/.uci/} ..."
+for packagefile in $(ls /tmp/.uci/* 2>&-); do
+	package="${packagefile#/tmp/.uci/}"
+	log_message "@TR<<apply_Committing_settings#Committing settings>>: $package"
+	uci_load_originals "$package"
 	uci_commit "$package"
-	case "$package" in
-		"/tmp/.uci/qos") qos-start;;
-		"/tmp/.uci/webif") 
-			switch_language "$oldlang"
-			init_theme
-			#whiterussian only
-			if exists "/etc/init.d/S??webif_deviceid"; then
-				/etc/init.d/S??webif_deviceid
-			fi
-			if exists "/etc/init.d/S??opendns"; then
-				/etc/init.d/S??opendns restart
-			fi
-			;;
-		"/tmp/.uci/upnpd") reload_upnpd;;
-	esac
+	uci_load "$package"
+	eval 'case "$package" in
+		'"$HANDLERS_uci"'
+	esac'
+	uci_unset_originals "$package"
 done
 
 #
