f40bc4bd0fe83db51302ce70996968919d562dbf
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-04-27 02:54:09 +0000

    provide initial languages.lst and do not overwrite unless unless the list changes

--- kamikaze/package/webif/files/usr/lib/webif/webif-mkthemelist.sh
+++ kamikaze/package/webif/files/usr/lib/webif/webif-mkthemelist.sh
@@ -2,6 +2,10 @@
 themes_lst="/etc/themes.lst"
 tmpthemeslst=$(mktemp "/tmp/.webif-XXXXXX")
 ipkg list | awk '/webif-theme/ { gsub("webif-theme-",""); print "option|"$1"|"$5}' | sort | uniq >> "$tmpthemeslst"
-rm -f "$themes_lst"
-mv -f "$tmpthemeslst" "$themes_lst"
-.
+if [ "'cat $tmpthemeslst'" != "" ]; then
+	if [ "'cat $tmpthemeslst'" != "'cat $themes_lst'" ]; then
+		rm -f "$themes_lst"
+		mv -f "$tmpthemeslst" "$themes_lst"
+	fi
+fi
+rm -f $tmpthemeslst
--- trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
+++ trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
@@ -3,5 +3,10 @@ languages_lst="/etc/languages.lst"
 tmplanglst=$(mktemp "/tmp/.webif-XXXXXX")
 echo "option|en|English" >  "$tmplanglst"
 ipkg list | awk '/webif-lang/ { gsub("webif-lang-",""); print "option|"$1"|"$5}' | sort | uniq >> "$tmplanglst"
-rm -f "$languages_lst"
-mv -f "$tmplanglst" "$languages_lst"
+if [ "'cat $tmplanglst'" != "option|en|English" ]; then
+	if [ "'cat $tmplanglst'" != "'cat $languages_lst'" ]; then
+		rm -f "$languages_lst"
+		mv -f "$tmplanglst" "$languages_lst"
+	fi
+fi
+rm -f "$tmplanglst"
