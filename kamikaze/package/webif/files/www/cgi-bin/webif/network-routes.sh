2cab9f8f1610e820e1bfad44f0f04a9536ec3752
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-05-04 16:57:32 +0000

    add fields, simplify update routine

--- kamikaze/package/webif/files/www/cgi-bin/webif/network-routes.sh
+++ kamikaze/package/webif/files/www/cgi-bin/webif/network-routes.sh
@@ -16,15 +16,18 @@ update_routes() {
 		ROUTES_FILE="/tmp/.webif/file-routes"
 	}
 	local temp_line
-	! empty "$3" && temp_line="-net $2" || temp_line="-host $2"
-	! empty "$3" && temp_line="$temp_line netmask $3"
-	! empty "$4" && temp_line="$temp_line gw $4"
-	! empty "$5" && temp_line="$temp_line $5"
+	! empty "$FORM_netmask" && temp_line="-net $FORM_ipaddr" || temp_line="-host $FORM_ipaddr"
+	! empty "$FORM_netmask" && temp_line="$temp_line netmask $FORM_netmask"
+	! empty "$FORM_gateway" && temp_line="$temp_line gw $FORM_gateway"
+	! empty "$FORM_metric" && temp_line="$temp_line metric $FORM_metric"
+	! empty "$FORM_interface" && temp_line="$temp_line dev $FORM_interface"
+	! empty "$FORM_additional" && temp_line="$temp_line $FORM_additional"
 	[ "$1" = "add" ] && {
 		echo "$temp_line" >> "$ROUTES_FILE" 2>/dev/null
 	}
 	[ "$1" = "del" ] && {
-		sed -e "/$temp_line/d" -i "$ROUTES_FILE" 2>/dev/null
+		sed_line=$(echo "$temp_line" | sed 's/ /[[:space:]]/g; s/\./\\./')
+		sed -e "/$sed_line/d" -i "$ROUTES_FILE" 2>/dev/null
 	}
 }
 
@@ -35,13 +38,13 @@ update_routes() {
 	}
 	equal "$FORM_action" "editroute" && {
 		EDIT_FLAG=1
-		update_routes del "$FORM_ipaddr" "$FORM_netmask" "$FORM_gateway" "$FORM_additional"
+		update_routes del
 	}
 	equal "$FORM_action" "addroute" && {
 		EDIT_FLAG=1
 	}
 	equal "$FORM_action" "removeroute" && {
-		update_routes del "$FORM_ipaddr" "$FORM_netmask" "$FORM_gateway" "$FORM_additional"
+		update_routes del
 	}
 }
 
@@ -50,15 +53,54 @@ update_routes() {
 ip|FORM_ipaddr|@TR<<network_routes_IP_Address#IP Address>>|required|$FORM_ipaddr
 netmask|FORM_netmask|@TR<<network_routes_Netmask#Netmask>>||$FORM_netmask
 ip|FORM_gateway|@TR<<network_routes_Gateway#Gateway>>||$FORM_gateway
+int|FORM_metric|@TR<<network_routes_Metric#Metric>>||$FORM_metric
+string|FORM_interface|@TR<<network_routes_Interface#Interface>>||$FORM_interface
+string|FORM_additional|@TR<<network_routes_Additional_commands#Additional commands>>||$FORM_additional
 EOF
 	equal "$?" 0 && {
 		# maybe also check with route command?
-		update_routes add "$FORM_ipaddr" "$FORM_netmask" "$FORM_gateway" "$FORM_additional"
-		unset FORM_ipaddr FORM_netmask FORM_gateway FORM_additional
+		update_routes add
+		unset FORM_ipaddr FORM_netmask FORM_gateway FORM_metric FORM_interface FORM_additional
 	} || {
 		EDIT_FLAG=1
 	}
 }
+
+header_inject_head=$(cat <<EOF
+<style type="text/css">
+<!--
+#stylebuttons .cssbutton {
+	background-color: #f6f4f1;
+	border: 2px #888785 outset;
+	border-color: #c6c4c1 #888785 #888785 #c6c4c1;
+	padding: 1px 3px 1px 2px;
+	font-size: 0.83em;
+	text-decoration: none;
+}
+#stylebuttons .cssbutton, .cssbutton:visited, .cssbutton:active {
+	color: #000000;
+}
+#stylebuttons .delbut, .delbut:visited, .delbut:active {
+	color: #d44533;
+}
+#stylebuttons .cssbutton:active {
+	border-style: inset;
+	border-color: #f6f4f1 #f6f4f1 #888785 #888785;
+	background-color: #f6f4f1;
+	padding: 1px 2px 1px 3px;
+}
+#stylebuttons span {
+	display: none;
+}
+td {
+	padding-top: 1px;
+	padding-bottom: 2px;
+}
+-->
+</style>
+
+EOF
+)
 
 header "Network" "Routes" "@TR<<network_routes_Static_Routes#Static Routes>>" '' "$SCRIPT_NAME"
 
@@ -71,12 +113,20 @@ field|@TR<<network_routes_Netmask#Netmask>>|field_wan_netmask|
 text|netmask|$FORM_netmask
 field|@TR<<network_routes_Gateway#Gateway>>|field_gateway|
 text|gateway|$FORM_gateway
+field|@TR<<network_routes_Metric#Metric>>|field_metric|
+text|metric|$FORM_metric
+field|@TR<<network_routes_Interface#Interface>>|field_interface|
+text|interface|$FORM_interface
 field|@TR<<network_routes_Additional_commands#Additional commands>>|field_additional|
 text|additional|$FORM_additional
 helpitem|network_routes_IP_Address_netmask#IP Address/Netmask
 helptext|network_routes_IP_Address_helptext#Helptext.
 helpitem|network_routes_Gateway#Gateway
 helptext|network_routes_Gateway_helptext#Helptext.
+helpitem|network_routes_Metric#Metric
+helptext|network_routes_Metric_helptext#Helptext.
+helpitem|network_routes_Interface#Interface
+helptext|network_routes_Interface_helptext#Helptext.
 helpitem|network_routes_Additional_commands#Additional commands
 helptext|network_routes_Additional_commands_helptext#Helptext.
 end_form
@@ -86,12 +136,15 @@ EOF
 ?>
 <div class="settings">
 <h3><strong>@TR<<network_routes_Configured_Static_Routes#Configured Static Routes>></strong></h3>
+<div id="stylebuttons">
 <table style="text-align: left;" border="0" cellpadding="1" cellspacing="3">
 <tbody>
 <tr>
 <th>@TR<<network_routes_IP_Address#IP Address>></th>
 <th>@TR<<network_routes_Gateway#Gateway>></th>
 <th>@TR<<network_routes_Netmask#Netmask>></th>
+<th>@TR<<network_routes_Metric#Metric>></th>
+<th>@TR<<network_routes_Interface#Interface>></th>
 <th>@TR<<network_routes_Additional_commands#Additional commands>></th>
 <th>@TR<<network_routes_Action#Action>></th>
 </tr>
@@ -128,6 +181,16 @@ BEGIN {
 			$i=""
 			$(i+1)=""
 			href=href "&amp;gateway=" gateway
+		} else if ($i == "metric") {
+			metric=$(i+1)
+			$i=""
+			$(i+1)=""
+			href=href "&amp;metric=" metric
+		} else if ($i == "dev") {
+			interface=$(i+1)
+			$i=""
+			$(i+1)=""
+			href=href "&amp;interface=" interface
 		}
 	}
 	if (gateway != "")
@@ -136,6 +199,14 @@ BEGIN {
 		print "<td>&nbsp;</td>"
 	if (netmask != "")
 		print "<td>" netmask "</td>"
+	else
+		print "<td>&nbsp;</td>"
+	if (metric != "")
+		print "<td>" metric "</td>"
+	else
+		print "<td>&nbsp;</td>"
+	if (interface != "")
+		print "<td>" interface "</td>"
 	else
 		print "<td>&nbsp;</td>"
 	additional=""
@@ -152,23 +223,23 @@ BEGIN {
 	# buttons
 	print "<td>"
 	if (editflag == 0)
-		print "<a href=\"" url "?action=editroute&amp;" href "\">@TR<<network_routes_Edit#Edit>></a>"
+		print "<a class=\"cssbutton\" href=\"" url "?action=editroute&amp;" href "\">@TR<<network_routes_Edit#Edit>></a>"
 	print "&nbsp;"
 	if (editflag == 0)
-		print "<a href=\"" url "?action=removeroute&amp;" href "\">@TR<<network_routes_Delete#Delete>></a>"
+		print "<a class=\"cssbutton delbut\" href=\"" url "?action=removeroute&amp;" href "\">@TR<<network_routes_Delete#Delete>></a>"
 	print "</td>"
 	print "</tr>"
 }
 END {
 	if (records == 0)
-		print "<tr><td colspan=\"5\">@TR<<network_routes_No_configured_routes#No configured static routes exist.>></td></tr>"
+		print "<tr><td colspan=\"7\">@TR<<network_routes_No_configured_routes#No configured static routes exist.>></td></tr>"
 	if (editflag == 0)
-		print "<tr><td colspan=\"5\" align=\"right\"><a href=\"" url "?action=addroute\">@TR<<network_routes_Add_new_route#Add new route>></a></td></tr>"
-
+		print "<tr><td colspan=\"7\" align=\"right\"><a class=\"cssbutton\" href=\"" url "?action=addroute\">@TR<<network_routes_Add_new_route#Add new route>></a></td></tr>"
 }'
 ?>
 </tbody>
 </table>
+</div>
 </div>
 
 <br />
@@ -182,13 +253,13 @@ END {
 <th>@TR<<Gateway>></th>
 <th>@TR<<Genmask>></th>
 <th>@TR<<Flags>></th>
-<th>@TR<<MSS>></th>
-<th>@TR<<Window>></th>
-<th>@TR<<irtt>></th>
+<th>@TR<<Metric>></th>
+<th>@TR<<Ref>></th>
+<th>@TR<<Use>></th>
 <th>@TR<<Interface>></th>
 </tr>
 <?
-        route -ne | awk '
+        route -n | awk '
 BEGIN {
 	odd=1
 }
