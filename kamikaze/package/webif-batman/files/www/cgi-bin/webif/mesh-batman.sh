49d5056cf55da7b8f77bde468a4656f98f213a74
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-05-06 23:59:46 +0000

    add the first interface option, thanks jnc

--- kamikaze/package/webif-batman/files/www/cgi-bin/webif/mesh-batman.sh
+++ kamikaze/package/webif-batman/files/www/cgi-bin/webif/mesh-batman.sh
@@ -6,6 +6,9 @@ header "Mesh" "BATMAN" "@TR<<BATMAN status and configuration>>" ' onload="modech
 
 uci_load "batman"
 
+
+available_interfaces=$(ifconfig | grep inet -B1 | awk '/HWaddr/ {printf "%s ",$1}')
+
 #####################################################################
 # If this is a form submit..
 ! empty "$FORM_submit" && {
@@ -17,7 +20,6 @@ uci_load "batman"
 
 	# Get interfaces selected in the form
 	interfaces_to_configure=""
-	available_interfaces=$(ifconfig | grep inet -B1 | awk '/HWaddr/ {printf "%s ",$1}'
 	for interface in $available_interfaces; do
 		sane_iface=$(echo "$interface" | tr '.-' 'PH')
 		eval checkbox_status="\$FORM_batman_interface_${sane_iface}"
@@ -25,6 +27,15 @@ uci_load "batman"
 			interfaces_to_configure=$interfaces_to_configure" "$interface
 		fi
 	done
+
+	# Make primary interface ordered first and clean up whitespace
+	first_interface=$(echo "$FORM_batman_primary_interface" | tr 'PH' '.-')
+	if echo $interfaces_to_configure | grep -q $first_interface; then
+		a="$interfaces_to_configure"
+		f="$first_interface"
+		interfaces_to_configure="${f} ${a%%$f*}${a#*$f}"
+	fi
+	interfaces_to_configure=$(echo $interfaces_to_configure)
 
 	# Get current configured values
 	batman_interfaces=$(uci get batman.general.interface)
@@ -81,10 +92,10 @@ if [ -e "/var/run/batmand.socket" ]; then
 		batmanstatus=$batmanstatus$(cat "$batmantmpfile" | awk ' {printf "%s %s %s %s %s","<tr><td width=\"30%\">",$1,"</td><td width=\"35%\">",$3,"</td><td width=\"35%\">"} {for (i = 5; i <= NF; i++) printf " %s <br>",$i } { printf "<br></td></tr>"}')
 		batmanstatus=$batmanstatus"</table>"
 	else
-		batmanstatus="string|No BATMAN nodes in range"
+		batmanstatus="string|<tr><td>No BATMAN nodes in range</td></tr>"
 	fi
 else
-	batmanstatus="string|BATMAN daemon not running or daemon socket not available<br>"
+	batmanstatus="string|<tr><td>BATMAN daemon not running or daemon socket not available</td></tr>"
 fi
 
 
@@ -92,16 +103,22 @@ fi
 # Prepare form Batman configuration
 
 form_batman_interface=""
-available_interfaces=$(ifconfig | grep inet -B1 | awk '/HWaddr/ {printf "%s ",$1}'
+form_batman_primary_interface=""
+batman_primary_interface=""
 for interface in $available_interfaces; do
 	sane_iface=$(echo "$interface" | tr '.-' 'PH')
+	uci_iface=$(uci get batman.general.interface)
 	form_batman_interface=$form_batman_interface$(printf "field| - %s\ncheckbox|batman_interface_%s|"  $interface $sane_iface)
-	if uci get batman.general.interface | grep -q $interface; then
+	if echo $uci_iface | grep -q $interface; then
 		form_batman_interface=$form_batman_interface"1"
 	else
 		form_batman_interface=$form_batman_interface"0"
 	fi
 	form_batman_interface=$form_batman_interface$(printf "|1|\n ")
+	form_batman_primary_interface=$form_batman_primary_interface$(printf "\noption|%s|%s"  $sane_iface $interface)
+	if echo $uci_iface | awk '{print $1}' | grep -q $interface; then
+		batman_primary_interface=$sane_iface
+	fi
 done
 
 batman_announce=$(uci get batman.general.announce)
@@ -132,6 +149,12 @@ field|@TR<<Batman interfaces>>
 $form_batman_interface
 helpitem|Batman interface
 helptext| interface on which BATMAN will work.
+
+field|@TR<<Primary interface>>
+select|batman_primary_interface|$batman_primary_interface
+option|none|@TR<<(none)>>$form_batman_primary_interface|
+helpitem|Primary interface
+helptext| Primary interface TTL=50, non-primary interface TTL=2
 
 field|@TR<<Announce networks>>
 text|batman_announce|$batman_announce|
