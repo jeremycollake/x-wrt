8418540aa756a94d0edd6d88cb8f34ba1b7bf235
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-07-11 18:17:53 +0000

    update upload script

--- trunk/upload_files.sh
+++ trunk/upload_files.sh
@@ -4,18 +4,8 @@ if [ $# -lt 1 ]; then
         exit 1
 fi
 export SCP_USER="$1"
-rm bin/packages/w?bf??e*
+kamikaze_version="snapshots"
 chmod -R 775 bin/*
-# for my personal shit
-if [ -d "/mnt/whale/xwrt" ]; then
-        cp /mnt/whale/xwrt/xwrt.htm ht_docs
-        cp /mnt/whale/xwrt/xwrt.asp ht_docs
-fi
-#echo "Uploading xwrt web pages ..."
-#scp \
-#       ht_docs/xwrt.htm \
-#       ht_docs/xwrt.asp \
-#       $SCP_USER@shell.berlios.de:/home/groups/xwrt/htdocs/
 
 #Broadcom 2.4
 ls bin/openwrt-brcm-2.4* >/dev/null 2>&-
@@ -27,9 +17,9 @@ echo $version
 }
 
 #Broadcom 2.6
-ls bin/openwrt-brcm-2.6* >/dev/null 2>&-
+ls bin/openwrt-brcm47xx-2.6* >/dev/null 2>&-
 [ $? = "0" ] && {
-version="brcm-2.6"
+version="brcm47xx-2.6"
 chmod 775 build_mipsel/webif-0.3/ipkg/webif/www/.version
 version_file="build_mipsel/webif-0.3/ipkg/webif/www/.version"
 echo $version
@@ -53,23 +43,67 @@ version_file="build_mips/webif-0.3/ipkg/webif/www/.version"
 echo $version
 }
 
+#rb532 2.6
+ls bin/openwrt-rb532-2.6* >/dev/null 2>&-
+[ $? = "0" ] && {
+version="rb532-2.6"
+chmod 775 build_mips/webif-0.3/ipkg/webif/www/.version
+version_file="build_mips/webif-0.3/ipkg/webif/www/.version"
+echo $version
+}
+
+#ixp4xx 2.6
+ls bin/openwrt-ixp4xx-2.6* >/dev/null 2>&-
+[ $? = "0" ] && {
+version="ixp4xx-2.6"
+chmod 775 build_armeb/webif-0.3/ipkg/webif/www/.version
+version_file="build_armeb/webif-0.3/ipkg/webif/www/.version"
+echo $version
+}
+
+#magicbox 2.6
+ls bin/openwrt-magicbox-2.6* >/dev/null 2>&-
+[ $? = "0" ] && {
+version="magicbox-2.6"
+chmod 775 build_powerpc/webif-0.3/ipkg/webif/www/.version
+version_file="build_powerpc/webif-0.3/ipkg/webif/www/.version"
+echo $version
+}
+
+#au1000 2.6
+ls bin/openwrt-au1000-2.6* >/dev/null 2>&-
+[ $? = "0" ] && {
+version="au1000-2.6"
+chmod 775 build_mipsel/webif-0.3/ipkg/webif/www/.version
+version_file="build_mipsel/webif-0.3/ipkg/webif/www/.version"
+echo $version
+}
+
+
 echo "Uploading webif version info ..."
 scp \
 	$version_file \
-        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$version/
+        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/
 echo "Uploading package repository ..."
 scp \
         bin/packages/* \
-        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$version/packages/
+        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/packages/
 echo "Uploading X-Wrt patches ..."
 scp \
         patches/* \
-        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$version/patches/
-#echo "Uploading firmware images ..."
-#date > /tmp/build-date.txt
-#scp \
-#       /tmp/build-date.txt \
-#       $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$version/images/
-#scp \
-#       bin/openwrt* \
-#       $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$version/images/
+        $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/patches/
+echo "Uploading firmware images ..."
+cd bin
+md5sum openwrt* > /tmp/md5sums.txt
+md5sum OpenWrt* >> /tmp/md5sums.txt
+cd ..
+scp \
+	/tmp/md5sums.txt \
+	$SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/
+date > /tmp/build-date.txt
+scp \
+       /tmp/build-date.txt \
+       $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/
+scp \
+       bin/openwrt* \
+       $SCP_USER@downloads.x-wrt.org:/www/xwrt/kamikaze/$kamikaze_version/$version/
