871c2c2a97035825158260a53a1411e5cb454eab
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-10-16 11:57:47 +0000

    process only required files when updating the svn revision

--- package/webif/embed_rev.sh
+++ package/webif/embed_rev.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 #
-# this script embeds the global revision number in document(s) given on command line
-# document should have __SVN_REVISION__ where it wants the revision number
-# to be placed. Wildcards okay.
+# this script embeds the global revision number in document(s) given 
+# on command line
+# document should have __SVN_REVISION__ where it wants the revision 
+# number to be placed. Wildcards okay.
 #
 if [ $# -lt 1 ]; then
 	echo " Invalid usage. Must supply one or more files."
 	exit 1
 fi
 revision_number=$(svn info | grep Revision | cut -c11-)
-tempfile=$(mktemp)
 echo " Revision number is $revision_number"
-echo " Temporary file is $tempfile"
 until [ -z "$1" ]  
 do
-	for curfile in $1; do
+	for curfile in $(grep -H "__SVN_REVISION__" -r "$1" | cut -d':' -f1 | sed '/\.svn\//d'); do
 		echo " Processing $curfile ..."
-		cp $curfile $tempfile	
-		cat $tempfile | sed s/__SVN_REVISION__/$revision_number/ > $curfile
-		rm $tempfile	
+		sed -i -e "s/__SVN_REVISION__/$revision_number/" "$curfile"
 	done
-  shift
+	shift
 done
 
