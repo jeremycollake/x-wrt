09db874ad4a74bbe1de4edf6f899b361092cad8d
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-10-25 03:18:56 +0000

    launch /etc/init.d/network restart into the background otherwise apply.sh hangs

--- package/webif/files/usr/lib/webif/apply.sh
+++ package/webif/files/usr/lib/webif/apply.sh
@@ -256,7 +256,7 @@ for package in $process_packages; do
 			;;
 		"network")
 			echo '@TR<<Reloading>> @TR<<network>> ...'
-			/etc/init.d/network restart
+			/etc/init.d/network restart &
 			killall dnsmasq
 			if exists "/etc/rc.d/S??dnsmasq"; then
 				/etc/init.d/dnsmasq start
