1fbed9dd2dcf0eea6608d64756549fc45a1d8580
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2007-11-28 19:23:25 +0000

    add default of 0 to config_get_bool calls

--- package/webif/files/www/cgi-bin/webif/log-dmesg.sh
+++ package/webif/files/www/cgi-bin/webif/log-dmesg.sh
@@ -95,10 +95,10 @@ let "buffersize *= 1024"
 [ $buffersize -gt 1024 ] || buffersize=""
 dmesg ${buffersize:+-s$buffersize} 2>/dev/null | show_messages
 echo " </pre>"
-config_get_bool dmesgbackup_enabled "${dmesgbackup_cfg}" enabled
+config_get_bool dmesgbackup_enabled "${dmesgbackup_cfg}" enabled 0
 if [ 1 -eq "$dmesgbackup_enabled" ]; then
 	eval dmesgbackup_file="\$CONFIG_${dmesgbackup_cfg}_file"
-	config_get_bool dmesgbackup_gzip "${dmesgbackup_cfg}" gzip
+	config_get_bool dmesgbackup_gzip "${dmesgbackup_cfg}" gzip 0
 	[ 1 -eq "$dmesgbackup_gzip" ] && dmesgbackup_file="$dmesgbackup_file.gz"
 	if [ -f "$dmesgbackup_file" ]; then
 		echo "<div class=\"clearfix\">&nbsp;</div></div>"
