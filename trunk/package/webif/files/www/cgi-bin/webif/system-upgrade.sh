60af5e2aa3ea503c4f2a07a2e7a4fa60f9de3722
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-01-09 02:19:47 +0000

    add upgrade option for x86, prevent attempted upgrades on non supported platforms (currently on brcm-2.4 and x86 are supported)

--- trunk/package/webif/files/www/cgi-bin/webif/system-upgrade.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/system-upgrade.sh
@@ -1,8 +1,29 @@
-#!/usr/bin/webif-page -p /bin/sh
+#!/usr/bin/webif-page "-U /tmp -u 16384"
+<?
 . /usr/lib/webif/webif.sh
 
-header "System" "Upgrade" "<img src=/images/upd.jpg align=absmiddle>&nbsp;@TR<<Firmware Upgrade>>"
+header "System" "Upgrade" "<img src=/images/upd.jpg align=absmiddle>&nbsp;@TR<<Firmware Upgrade>>" ' onload="modechange()" ' "$SCRIPT_NAME"
+board_type=$(cat /proc/cpuinfo 2>/dev/null | sed 2,20d | cut -c16-)
+machinfo=$(uname -a 2>/dev/null)
+if $(echo "$machinfo" | grep -q "mips"); then
+	if $(echo "$board_type" | grep -q "Atheros"); then
+		target="atheros-2.6"
+	elif $(echo "$board_type" | grep -q "WP54"); then
+		target="adm5120-2.6"
+	elif $(echo "$machinfo" | grep -q "2\.4"); then
+		target="brcm-2.4"
+	elif $(echo "$machinfo" | grep -q "2\.6"); then
+		target="brcm-2.6"
+	fi
+elif $(echo "$machinfo" | grep -q " i[0-9]86 "); then
+	target="x86-2.6"
+elif $(echo "$machinfo" | grep -q " avr32 "); then
+	target="avr32-2.6"
+elif $(cat /proc/cpuinfo 2>/dev/null | grep -q "IXP4"); then
+	target="ixp4xx-2.6"
+fi
 
+if [ "$target" = "brcm-2.4" ]; then
 #####################################################################
 do_upgrade() {
 	echo "<br />Upgrading firmware, please wait ... <br />"
@@ -113,7 +134,24 @@ cat <<EOF
 	</div>
 EOF
 }
+elif [ "$target" = "x86-2.6" ]; then
+	if empty "$FORM_submit"; then
+display_form <<EOF
+start_form
+field|@TR<<Firmware Image>>
+upload|upgradefile
+submit|upgrade| @TR<<Upgrade>> |
+end_form
+EOF
+	else
+		echo "<br />Upgrading firmware, please wait ... <br />"
+		sysupgrade $upgradefile
+		echo "@TR<<done>>."
+	fi
+else
+	echo "<br />The ability to upgrade your platform has not been implemented<br />"
+fi
 
 footer
-
+?>
 ##WEBIF:name:System:900:Upgrade
