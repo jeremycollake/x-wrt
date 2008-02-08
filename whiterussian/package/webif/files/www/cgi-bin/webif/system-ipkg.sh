6dc1d20d2aa8cbc8d9f6d565a217271c5fe51703
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-02-08 20:10:14 +0000

    backport r3630 - remove old code that was causing packages to be installed and reinstalled on url install

--- whiterussian/package/webif/files/www/cgi-bin/webif/system-ipkg.sh
+++ whiterussian/package/webif/files/www/cgi-bin/webif/system-ipkg.sh
@@ -128,10 +128,6 @@ equal "$repo_update_needed" "1" && {
 if [ "$FORM_action" = "install" ]; then
 	echo "<pre>@TR<<system_ipkg_pleasewait#Please wait>> ...<br />"
 	install_package `echo "$FORM_pkg" | sed -e 's, ,+,g'`
-	if [ "$?" != "0" ]; then
-		ipkg update
-		install_package `echo "$FORM_pkg" | sed -e 's, ,+,g'`
-	fi
 	echo "</pre><br />"
 elif [ "$FORM_action" = "remove" ]; then
 	echo "<pre>@TR<<system_ipkg_pleasewait#Please wait>> ...<br />"
