9cbf2a67105055aab3064ace7af6ed33fdad1967
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-02-14 16:00:10 +0000

    fix page errors; thanks fofware

--- trunk/package/webif/files/www/cgi-bin/webif/system-upgrade.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/system-upgrade.sh
@@ -2,7 +2,6 @@
 <?
 . /usr/lib/webif/webif.sh
 
-header "System" "Upgrade" "<img src=/images/upd.jpg align=absmiddle>&nbsp;@TR<<Firmware Upgrade>>" ' onload="modechange()" ' "$SCRIPT_NAME"
 board_type=$(cat /proc/cpuinfo 2>/dev/null | sed 2,20d | cut -c16-)
 machinfo=$(uname -a 2>/dev/null)
 if $(echo "$machinfo" | grep -q "mips"); then
@@ -22,6 +21,8 @@ elif $(echo "$machinfo" | grep -q " avr32 "); then
 elif $(cat /proc/cpuinfo 2>/dev/null | grep -q "IXP4"); then
 	target="ixp4xx-2.6"
 fi
+
+header "System" "Upgrade" "<img src=\"/images/upd.jpg\" alt=\"@TR<<Firmware Upgrade>>\" />&nbsp;@TR<<Firmware Upgrade>>" '' "$SCRIPT_NAME"
 
 if [ "$target" = "brcm" ]; then
 #####################################################################
@@ -65,8 +66,8 @@ NOINPUT=1
 #####################################################################
 equal "$REQUEST_METHOD" "GET" && {
 	cat <<EOF
-	<script type="text/javascript">
-
+<script type="text/javascript">
+<!--
 function statusupdate() {
 	document.getElementById("form_submit").style.display = "none";
 	document.getElementById("status_text").style.display = "inline";
@@ -76,32 +77,34 @@ function statusupdate() {
 function printStatus() {
 	document.write('<div style="display: none; font-size: 14pt; font-weight: bold;" id="status_text" />@TR<<Upgrading...>>&nbsp;</div>');
 }
-	</script>
-	<form method="POST" name="upgrade" action="$SCRIPT_NAME" enctype="multipart/form-data" onSubmit="statusupdate()">
-	<table style="width: 90%; text-align: left;" border="0" cellpadding="2" cellspacing="2" align="center">
-	<tbody>
-		<tr>
-			<td>@TR<<Options>>:</td>
-			<td>
-				<input type="checkbox" name="erase_fs" value="1" />@TR<<Erase_JFFS2|Erase JFFS2 partition>>
-			</td>
-		</tr>
-		<tr>
-			<td>@TR<<Firmware_image|Firmware image to upload:>></td>
-			<td>
-				<input type="file" name="firmware" />
-			</td>
-		</tr>
-		<tr>
-			<td />
-			<td>
-				<script type="text/javascript">printStatus()</script>
-				<input id="form_submit" type="submit" name="submit" value="@TR<<Upgrade>>" onClick="statusupdate()" />
-			</td>
-		</tr>
-	</tbody>
-	</table>
-	</form>
+//-->
+</script>
+
+<form method="POST" name="upgrade" action="$SCRIPT_NAME" enctype="multipart/form-data" onSubmit="statusupdate()">
+<table style="width: 90%; text-align: left;" border="0" cellpadding="2" cellspacing="2" align="center">
+<tbody>
+	<tr>
+		<td>@TR<<Options>>:</td>
+		<td>
+			<input type="checkbox" name="erase_fs" value="1" />@TR<<Erase_JFFS2|Erase JFFS2 partition>>
+		</td>
+	</tr>
+	<tr>
+		<td>@TR<<Firmware_image|Firmware image to upload:>></td>
+		<td>
+			<input type="file" name="firmware" />
+		</td>
+	</tr>
+	<tr>
+		<td />
+		<td>
+			<script type="text/javascript">printStatus()</script>
+			<input id="form_submit" type="submit" name="submit" value="@TR<<Upgrade>>" onClick="statusupdate()" />
+		</td>
+	</tr>
+</tbody>
+</table>
+</form>
 EOF
 }
 #####################################################################
@@ -149,9 +152,11 @@ EOF
 		echo "@TR<<done>>."
 	fi
 else
-	echo "<br />The ability to upgrade your platform has not been implemented<br />"
+	echo "<br />The ability to upgrade your platform has not been implemented.<br />"
 fi
 
 footer
 ?>
+<!--
 ##WEBIF:name:System:900:Upgrade
+-->
