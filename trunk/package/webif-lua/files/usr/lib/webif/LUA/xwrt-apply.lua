58851b138c6b236024fa5c8264a03de3d418c9c8
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-02-24 02:22:10 +0000

    fix for compatibility with lua apply

--- trunk/package/webif-lua/files/usr/lib/webif/LUA/xwrt-apply.lua
+++ trunk/package/webif-lua/files/usr/lib/webif/LUA/xwrt-apply.lua
@@ -24,72 +24,68 @@ require("translator")
 tr_load()
 self = uciUpdatedClass.new()
 require("uci")
-
---pepe = uciClass.new("freeradius")
-  local handler_list = {}
-  handler_dir = io.popen("ls /usr/lib/webif/apply")
-  for line in handler_dir:lines() do
-    handler_list[#handler_list+1] = line
-  end
-  for k,t in pairsByKeys(self) do
-    if type(t) == "table" then
-      local found = false
-      for i = 1, #handler_list do
-        if handler_list[i] == k then
-          print (k)
-          found = true
-          dofile("/usr/lib/webif/apply/"..k)
-          break
-        end
+local handler_list = {}
+handler_dir = io.popen("ls /usr/lib/webif/apply")
+for line in handler_dir:lines() do
+  handler_list[#handler_list+1] = line
+end
+for k,t in pairsByKeys(self) do
+  if type(t) == "table" then
+    local found = false
+    for i = 1, #handler_list do
+      if handler_list[i] == k then
+        print (k)
+        found = true
+        dofile("/usr/lib/webif/apply/"..k)
+        break
       end
-		end
-	end
-	for i, t in pairs(__RESTART) do 
-    print("Restarting...<br>")
-    local ucifile = uciClass.new(t.pkg)
-    if ucifile[t.cfg][t.opt] == "1" then
-      local myexec = io.popen(t.init.." enable")
-      print ("Enabling "..i.." service...<br>")
-      for li in myexec:lines() do
-        if string.len(li) > 1 then
-          print(li,"<br>")
-        end
-      end
-      myexec:close()
---  print("<br>")
-      myexec = io.popen(t.init.." stop")
-      print ("Stopping "..i.." service...<br>")
-      for li in myexec:lines() do
-        if string.len(li) > 1 then
-          print(li,"<br>")
-        end
-      end
-      myexec:close()
---  print("<br>")
-      myexec = io.popen(t.init.." start")
-      print ("Starting "..i.." service...<br>")
-      for li in myexec:lines() do
-        if string.len(li) > 1 then
-          print(li,"<br>")
-        end
-      end
-      myexec:close()
-    else
-      myexec = io.popen(t.init.." stop")
-      print ("Stopping "..i.." service...<br>")
-      for li in myexec:lines() do
-        if string.len(li) > 1 then
-          print(li,"<br>")
-        end
-      end
-      myexec:close()
-      local myexec = io.popen(t.init.." disable")
-      print ("Disabling "..i.." service...<br>")
-      for li in myexec:lines() do
-        if string.len(li) > 1 then
-          print(li,"<br>")
-        end
-      end
-      myexec:close()
     end
+	end
+end
+for i, t in pairs(__RESTART) do 
+  print("Restarting...")
+  local ucifile = uciClass.new(t.pkg)
+  if ucifile[t.cfg][t.opt] == "1" then
+    local myexec = io.popen(t.init.." enable")
+    print ("Enabling "..i.." service...<br>")
+    for li in myexec:lines() do
+      if string.len(li) > 1 then
+        print(li,"<br>")
+      end
+    end
+    myexec:close()
+    myexec = io.popen(t.init.." stop")
+    print ("Stopping "..i.." service...")
+    for li in myexec:lines() do
+      if string.len(li) > 1 then
+        print(li,"<br>")
+      end
+    end
+    myexec:close()
+    myexec = io.popen(t.init.." start")
+    print ("Starting "..i.." service...<br>")
+    for li in myexec:lines() do
+      if string.len(li) > 1 then
+        print(li,"<br>")
+      end
+    end
+    myexec:close()
+  else
+    myexec = io.popen(t.init.." stop")
+    print ("Stopping "..i.." service...<br>")
+    for li in myexec:lines() do
+      if string.len(li) > 1 then
+        print(li,"<br>")
+      end
+    end
+    myexec:close()
+    local myexec = io.popen(t.init.." disable")
+    print ("Disabling "..i.." service...<br>")
+    for li in myexec:lines() do
+      if string.len(li) > 1 then
+        print(li,"<br>")
+      end
+    end
+    myexec:close()
   end
+end
