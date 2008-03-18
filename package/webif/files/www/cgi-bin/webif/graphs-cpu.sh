5d5a46ad5518b0dd9b635d3e6be2e86d2d4ea51f
thepeople <thepeople@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-03-18 22:22:07 +0000

    add link to svg viewer from packetprotector r288

--- package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
+++ package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
@@ -13,6 +13,9 @@ header "Graphs" "graphs_cpu_subcategory#CPU" "@TR<<graphs_cpu_CPU_Usage#CPU Usag
 <center>
 	<embed src="/cgi-bin/webif/graph_cpu_svg.sh"
 		width="500" height="250" type="image/svg+xml" />
+	<object>
+		<br><a href=http://www.adobe.com/svg/viewer/install/main.html>If the graph is not fuctioning download the viewer here.</a>
+	</object>
 </center>
 <? footer ?>
 <!--
