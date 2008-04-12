cb120f49aefe0c7040bb99aa5b6e12d42c580f0e
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-04-12 13:56:14 +0000

    captive potral coova-chilli style

---
+++ trunk/package/coova-chilli-xwrt/files/www/cgi-bin/webif/coova-chilli.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/lua
+--------------------------------------------------------------------------------
+-- coovachilli.sh
+-- This script is writen in LUA, the extension is ".sh" for compatibilities
+-- reasons width menu system of X-Wrt
+--
+-- Description:
+--        Administrative console to Chillispot
+--
+-- Author(s) [in order of work date]:
+--       Fabián Omar Franzotti
+--         
+-- Configuration files referenced:
+--    hotspot
+--
+--------------------------------------------------------------------------------
+--[[
+##WEBIF:name:IW:220:Coova-Chilli
+]]--
+-- config.lua 
+-- LUA settings and load some functions files
+-- 
+dofile("/usr/lib/webif/LUA/config.lua")
+require("coovaportal")
+-- pageClass is part of the framework 
+page.title = tr("chilli_main_title#Coova-Chilli")
+cportal.set_menu()
+local forms = {}
+__FORM.option = string.trim(__FORM.option)
+if __FORM.option == "net" then
+  forms[1] = cportal.net_form()
+elseif __FORM.option == "uam" then
+  forms[1] = cportal.uam_form()
+elseif __FORM.option == "radius" then
+  forms[1] = cportal.radius_form()
+elseif __FORM.option == "nasid" then
+  forms[1] = cportal.nasid_form()
+elseif __FORM.option == "users" then
+  require("radius")
+  forms[1] = radius.add_usr_form()
+  forms[2] = radius.user_form()
+elseif __FORM.option == "communities" then
+  require("radius")
+  forms[1] = radius.community_form()
+
+elseif __FORM.option == "connections" then
+  forms[1] = cportal.connect_form()
+else
+  forms[1] = cportal.core_form()
+end
+
+print(page:header())
+for i=1, #forms do
+  forms[i]:print()
+end
+print (page:footer())
