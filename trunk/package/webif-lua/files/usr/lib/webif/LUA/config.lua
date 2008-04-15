182f8894d70fd67279e29a3d7dd463aaae095379
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-04-15 16:20:23 +0000

    modify search path

--- trunk/package/webif-lua/files/usr/lib/webif/LUA/config.lua
+++ trunk/package/webif-lua/files/usr/lib/webif/LUA/config.lua
@@ -11,7 +11,9 @@
 -- Configuration files referenced:
 --   none
 --------------------------------------------------------------------------------
-package.path = package.path .. ";/usr/lib/webif/LUA/?.lua;/usr/lib/webif/LUA/pkgs/?.lua;/usr/share//lua/5.1/?.lua"
+package.cpath = "./?.so;/usr/lib/lua/5.1/?.so" 
+package.path = "./?.lua;/usr/lib/webif/LUA/?.lua;/usr/lib/webif/LUA/pkgs/?.lua;/usr/lib/lua/5.1/?.lua;/usr/lib/lua/5.1/?/init.lua;/usr/lib/lua/5.1/?.lua;/usr/lib/lua/5.1/?/init.lua" 
+--package.path = package.path .. ";/usr/lib/webif/LUA/?.lua;/usr/lib/webif/LUA/pkgs/?.lua;/usr/share//lua/5.1/?.lua"
 -- Common Functions
 require("common")
 require("lpkg")
