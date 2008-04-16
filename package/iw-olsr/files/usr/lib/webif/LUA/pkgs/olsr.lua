7f056bfef91f10d06faf68e0dbaced33c6b952c1
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-04-16 20:16:02 +0000

    remove unnecesary scripts

--- package/iw-olsr/files/usr/lib/webif/LUA/pkgs/olsr.lua
+++ package/iw-olsr/files/usr/lib/webif/LUA/pkgs/olsr.lua
@@ -51,10 +51,10 @@ function set_menu()
 --  __MENU.IW.OLSR:Add("Ip Connect","olsr.sh?option=ipconnect")
     __MENU.IW.OLSR:Add("Hna4","olsr.sh?option=hna4")
     __MENU.IW.OLSR:Add("Hna6","olsr.sh?option=hna6")
+    __MENU.IW.OLSR:Add("Interfaces","olsr.sh?option=interfaces")
   end
-  __MENU.IW.OLSR:Add("Interfaces","olsr.sh?option=interfaces")
-  __MENU.IW.OLSR:Add("Status","olsr-status.sh")
-  __MENU.IW.OLSR:Add("Visualization","olsr-viz.sh")
+  __MENU.IW.OLSR:Add("Status","olsr.sh?option=status")
+--  __MENU.IW.OLSR:Add("Visualization","olsr-viz.sh")
 end
 
 function check_pkg()
@@ -438,3 +438,104 @@ function interfaces_form(form,user_level)
   return form
 end
 
+function status_form()
+  local str = status_str()
+  local vizstr = viz_str()
+  form = formClass.new("Status")
+  form:Add("text_line","viz",str)
+  form:Add_help("Network Vizualitation",vizstr)
+  return form
+end
+
+function viz_form()
+  local str = status_str()
+  local vizstr = viz_str()
+  form = formClass.new("Status")
+  form:Add("text_line","viz",str)
+  form:Add_help("Network Vizualitation",vizstr)
+  return form
+end
+
+function status_str()
+  local pepe = io.popen([[wget -q -O - http://127.0.0.1:2006/]])
+  local str = ""
+  for line in pepe:lines() do
+    if string.trim(line) == "Table: Links" then
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"8\">"..line.."</td></tr>"
+    elseif string.trim(line) == "Table: Neighbors" then
+      str = str .."</table>"
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"6\">"..line.."</td></tr>"
+    elseif string.trim(line) == "Table: Topology" then
+      str = str .."</table>"
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"5\">"..line.."</td></tr>"
+    elseif string.trim(line) == "Table: HNA" then
+      str = str .."</table>"
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"3\">"..line.."</td></tr>"
+    elseif string.trim(line) == "Table: MID" then
+      str = str .."</table>"
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"2\">"..line.."</td></tr>"
+    elseif string.trim(line) == "Table: Routes" then
+      str = str .."</table>"
+      str = str .."<table border=\"1\">"
+      str = str .."<tr><td colspan=\"5\">"..line.."</td></tr>"
+    else
+--    local t = string.gmatch(line,"^ ")
+      str = str .."<tr>"
+      for t in string.gmatch(line,"[^\t]+") do
+        str = str .."<td>"..t.."<td>"
+      end
+      str = str .."</tr>"
+    end
+  end
+  str = str .."</table>"
+  return str
+end
+
+function viz_str()
+local vizstr = [[
+<NOSCRIPT>
+<H1>OLSR Viz</H1>
+<TABLE BORDER="0" CLASS="note"><TR><TD>No JavaScript - no Viz.</TD></TR>
+</TABLE>
+<P>&nbsp;</P></NOSCRIPT>
+<SCRIPT SRC="/js/olsr-viz.js" LANGUAGE="JavaScript1.2" TYPE="text/javascript"></SCRIPT>
+
+<DIV ID="main" STYLE="width: 100%; height: 400px; border: 1px solid #ccc; margin-left:auto; margin-right:auto; text-align:center; overflow: scroll">
+  <DIV ID="edges" STYLE="width: 1px; height: 1px; position: relative; z-index:2"></DIV>
+  <DIV ID="nodes" STYLE="width: 1px; height: 1px; position: relative; z-index:4"></DIV>
+</DIV>
+<DIV STYLE="z-index:99">
+<FORM ACTION="">
+<P><B TITLE="Defines the display magification.">Zoom</B>&nbsp;
+<A HREF="javascript:set_scale(scale+0.1)">+</A>&nbsp;
+<A HREF="javascript:set_scale(scale-0.1)">&ndash;</A>&nbsp;
+<INPUT ID="zoom" NAME="zoom" TYPE="text" VALUE="2.0" SIZE="5" ONCHANGE="set_scale()">&nbsp;&nbsp;
+<B TITLE="Limits the view to a maximal hop distance.">Metric</B>&nbsp;
+<A HREF="javascript:set_maxmetric(maxmetric+1)">+</A>&nbsp;
+<A HREF="javascript:if(0<maxmetric)set_maxmetric(maxmetric-1)">&ndash;</A>&nbsp;
+<INPUT ID="maxmetric" NAME="maxmetric" TYPE="text" VALUE="3" SIZE="4" ONCHANGE="set_maxmetric(this.value)">&nbsp;&nbsp;
+<B TITLE="Enables the automatic layout optimization.">Optimization</B>
+<INPUT ID="auto_declump" NAME="auto_declump" TYPE="checkbox" ONCHANGE="set_autodeclump(this.checked)" CHECKED="CHECKED">&nbsp;&nbsp;
+<B TITLE="Show hostnames.">Hostnames</B>
+<INPUT ID="show_hostnames" NAME="show_hostnames" TYPE="checkbox" ONCHANGE="set_showdesc(this.checked)" CHECKED="CHECKED">&nbsp;&nbsp;
+<A HREF="javascript:viz_save()" TITLE="Saves the current settings to a cookie.">Save</A>&nbsp;&nbsp;
+<A HREF="javascript:viz_reset()" TITLE="Restarts the Viz script.">Reset</A>
+</P>
+</FORM>
+</DIV>
+<P><SPAN ID="debug" STYLE="visibility:hidden;"></SPAN>
+<IFRAME ID="RSIFrame" NAME="RSIFrame" STYLE="border:0px; width:0px; height:0px; visibility:hidden;">
+</IFRAME>
+
+<SCRIPT LANGUAGE="JavaScript1.2" TYPE="text/javascript">
+  viz_setup("RSIFrame","main","nodes","edges");
+  viz_update();
+</SCRIPT> </P>
+]]
+return vizstr
+end
