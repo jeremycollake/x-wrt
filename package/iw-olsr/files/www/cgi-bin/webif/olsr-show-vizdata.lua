5295cc9e78fea8c8b58dc56439d1789cdf7cf289
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-05-20 05:31:58 +0000

    fix olsr-show-vizdata.lua

--- package/iw-olsr/files/www/cgi-bin/webif/olsr-show-vizdata.lua
+++ package/iw-olsr/files/www/cgi-bin/webif/olsr-show-vizdata.lua
@@ -1,7 +1,8 @@
 #!/usr/bin/lua
-dofile("/usr/lib/webif/LUA/config.lua")
+require("init")
 require("olsrdata")
 olsrdata = olsrdataClass.new()
 print([[Content-Type: text/html; charset=UTF-8
-Pragma: no-cache]])
+Pragma: no-cache
+]])
 print (olsrdata:html())
