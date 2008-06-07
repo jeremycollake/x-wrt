426cb319409fc0936f7dcd5b929b036ebffd3b84
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-06-07 17:09:32 +0000

    webif: export only lines with descriptions when creating langs and themes lists

--- trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
+++ trunk/package/webif/files/usr/lib/webif/webif-mklanglist.sh
@@ -2,7 +2,7 @@
 languages_lst="/etc/languages.lst"
 tmplanglst=$(mktemp "/tmp/.webif-XXXXXX")
 echo "option|en|English" >  "$tmplanglst"
-ipkg list | awk '/webif-lang/ { gsub("webif-lang-",""); print "option|"$1"|"$5}' | sort | uniq >> "$tmplanglst"
+ipkg list | awk '/webif-lang/ { gsub("webif-lang-",""); if ($5) print "option|"$1"|"$5}' | sort | uniq >> "$tmplanglst"
 if [ "'cat $tmplanglst'" != "option|en|English" ]; then
 	if [ "'cat $tmplanglst'" != "'cat $languages_lst'" ]; then
 		rm -f "$languages_lst"
