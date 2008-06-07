4d5b7f487d819f29806c96cdca5848b60e0a430e
lubek <lubek@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-06-07 17:09:32 +0000

    webif: export only lines with descriptions when creating langs and themes lists

--- package/webif/files/usr/lib/webif/webif-mkthemelist.sh
+++ package/webif/files/usr/lib/webif/webif-mkthemelist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 themes_lst="/etc/themes.lst"
 tmpthemeslst=$(mktemp "/tmp/.webif-XXXXXX")
-ipkg list | awk '/webif-theme/ { gsub("webif-theme-",""); print "option|"$1"|"$5}' | sort | uniq >> "$tmpthemeslst"
+ipkg list | awk '/webif-theme/ { gsub("webif-theme-",""); if ($5) print "option|"$1"|"$5}' | sort | uniq >> "$tmpthemeslst"
 if [ "'cat $tmpthemeslst'" != "" ]; then
 	if [ "'cat $tmpthemeslst'" != "'cat $themes_lst'" ]; then
 		rm -f "$themes_lst"
