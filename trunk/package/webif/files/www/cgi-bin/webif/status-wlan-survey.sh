0899b2ff451bd4daf58e0b8bb4e00ae04adad62a
fofware <fofware@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-06-15 00:58:41 +0000

    fix progress bar caption to show low %

--- trunk/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/status-wlan-survey.sh
@@ -249,7 +249,7 @@ else
 			$QUALITY_STRING
 			string|<tr><td><strong>@TR<<Signal>></strong> $SIGNAL_DBM dBm / <strong>@TR<<Noise>></strong> $NOISE_DBM dBm</td></tr><tr><td>
 # I guess is more graphic
-			progressbar|SNR|<strong>@TR<<SNR>></strong> $SIGNAL_INTEGRITY dBm|200|$SNR_PERCENT|$SNR_PERCENT "%"
+			progressbar|SNR|<strong>@TR<<SNR>></strong> $SIGNAL_INTEGRITY dBm|200|$SNR_PERCENT|"$SNR_PERCENT%"
 #			progressbar|SNR|<strong>@TR<<SNR>></strong> $SIGNAL_INTEGRITY dBm|200|$SNR_PERCENT|$SIGNAL_INTEGRITY dBm
 			string|</td></tr><tr><td>&nbsp;</td></tr>"
 
