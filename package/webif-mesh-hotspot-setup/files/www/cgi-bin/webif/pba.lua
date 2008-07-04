568e2bf5c111e714cd33522efb66562f2e261d03
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-07-04 05:02:41 +0000

    fix wireless settings in chillispot apply fix cron script for olsrd fix plugin detection for olsr

--- package/webif-mesh-hotspot-setup/files/www/cgi-bin/webif/pba.lua
+++ package/webif-mesh-hotspot-setup/files/www/cgi-bin/webif/pba.lua
@@ -1,54 +1,23 @@
 #!/usr/bin/lua
-require("set_path")
-require("init")
-require("uci_iwaddon")
-local coovadir = uci.get_type("coovachilli","coovadir")
-local str_set="#### This conf file was writed by iw-apply for coova-chilli ####\n"
-for x,y in pairs(coovadir) do
-  for k, v in pairs(y) do
-    if k ~= ".type"
-    and k ~= ".name" then
-      str_set = str_set..k.."="..v.."\n"
-    end
-  end
-end
-
-local chillisettings = uci.get_type("coovachilli","settings")
-for x,y in pairs(chillisettings) do
-  for k, v in pairs(y) do
-    if k ~= ".type"
-    and k ~= ".name" then
-      if string.match(v,"%s") then v = "\""..v.."\"" end
---      if k == "HS_LOC_NAME"
---      or k == "HS_LOC_NETWORK" then
---        str_set = str_set..k.."=\""..v.."\"\n"
---      else
-        str_set = str_set..k.."="..v.."\n"
---      end
-    end
-  end
-end
-print(str_set)
-local conf_file ="#### This conf file was writed by iw-apply for chillispot ####\n"
-local chillisetting = uci.get_type("chillispot","settings")
-for n=1, #chillisetting do
-  for k,v in pairs(chillisetting[n]) do
-    if k ~= ".type"
-    and k ~= ".name" then
-      if k == "debug"
-      or k == "macauth"
-      or k == "uamanydns"
-      or k == "coanoipcheck"
-      or k == "acctupdate"
-      or k == "fg"
-      or k == "eapolenable" then
-        if tonumber(v) == 1 then
-          conf_file = conf_file .. k .. "\n"
-        end
+print("Compile files... to save space")
+local IPKG_INSTROOT = os.getenv("IPKG_INSTROOT") or ""
+local IPKG_CONF_DIR = os.getenv("IPKG_CONF_DIR") or ""
+local IPKG_OFFLINE_ROOT = os.getenv("IPKG_OFFLINE_ROOT") or ""
+local path = {}
+path[1] = "/usr/share/internet-wifi/lib/"
+path[2] = "/usr/share/internet-wifi/pkgs/"
+path[3] = "/usr/share/internet-wifi/applys/"
+for i=1, #path do
+  local libdir = io.popen("ls "..IPKG_INSTROOT..path[i])
+  for file in libdir:lines() do
+    if string.match(file,".lua") then
+      io.write("Compilling "..file)
+      local out = string.gsub(file,".lua",".out")
+      if os.execute("luac -s -o "..IPKG_INSTROOT..path[i]..file.." "..IPKG_INSTROOT..path[i]..file) == 0 then
+        print("..OK!!!")
       else
-        conf_file = conf_file .. k .. " " .. v .. "\n"
+        print(" ERROR!!")
       end
     end
   end
 end
-print(conf_file) 
