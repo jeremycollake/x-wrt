1578a55a658388b4f466c5ad1d9f5628630ac668
kemen04 <kemen04@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-07-16 23:06:52 +0000

    update pages for haserl 0.9.24

--- trunk/package/webif/files/www/cgi-bin/webif/data.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/data.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/webif-page
-<? echo -e "Content-Type: text/plain\r" ?>
-<? echo -e "\r"`date` ?><?if [ "$FORM_if" ] ?><? grep "${FORM_if}:" /proc/net/dev ?><?el?><? head -n 1 /proc/stat ?><?fi?>
+<?
+echo -e "\r"`date`
+if [ "$FORM_if" ]; then
+	grep "${FORM_if}:" /proc/net/dev
+else
+	head -n 1 /proc/stat
+fi
+?>
