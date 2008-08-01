58496cbe5265379d059a6e8aa47cca35909590da
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-08-01 18:46:46 +0000

    Problem in first apply with freeradius proxy.conf was solved but I can't find the bug, I guess is inside in one of this files, /usr/share/internet-wifi/lib/apply.lua, /usr/share/internet-wifi/applys/freeradius_proxy or /usr/share/internet-wifi/applys/freeradius_clients. When parsers param in /usr/share/internet-wifi/applys/coovachilli is "freeradius freeradius_check freeradius_proxy freeradius_clients" /usr/share/internet-wifi/lib/apply.lua process two times clients, and not process proxy, if param is "freeradius freeradius_check freeradius_clients freeradius_proxy" everything work ok.

--- package/webif-lua/files/usr/share/internet-wifi/lib/apply.lua
+++ package/webif-lua/files/usr/share/internet-wifi/lib/apply.lua
@@ -4,6 +4,19 @@ package.cpath = "?;./?.so;/usr/share/internet-wifi/lib.so/?.so;/usr/lib/lua/*.so
 package.path = "?;./?.lua;/usr/share/internet-wifi/?.lua;/usr/share/internet-wifi/lib/?.lua;/usr/share/internet-wifi/pkgs/?.lua;" .. package.path 
 require("common")
 wwwprint = print
+
+local reboot_list = {}
+local parsers_list = {}
+local old_parsers = {}
+local depends_list = ""
+local stop_list = {}
+-- local enable_list = {}
+-- local disable_list = {}
+local file_list = {}
+
+local exe_before = {}
+local exe_after = {}
+
 if __WWW then
   wwwprint = function (x)
     return print(x,"<br>")
@@ -23,11 +36,11 @@ function exe_list(list)
   local str = ""
   if __WWW then end_line = "<br>" end
   if list then
-    for command, msg in pairs(list) do
-      str = str .. msg..end_line..command..end_line
+    for command, msg in pairsByKeys(list) do
+      str = str .. msg.." "..command..end_line
       local exe_handler = io.popen(command)
       for line in exe_handler:lines() do
-        str = str..line..end_line
+--        str = str.."Command Info : "..line..end_line
       end
     end
   end
@@ -88,7 +101,8 @@ function init_list(list)
   return str      
 end
 
-function call_parser(file,parsers_list,depends_list,exe_before,exe_after,reboot_list)
+--function call_parser(file,parsers_list,depends_list,exe_before,exe_after,reboot_list)
+function call_parser(file)
     require("/usr/share/internet-wifi/applys/"..file)
     parsers_list[file] = {p = parser}
     -- Read if this package depends or need others packages to done configuration
@@ -96,20 +110,20 @@ function call_parser(file,parsers_list,depends_list,exe_before,exe_after,reboot_
       if depends_list == "" then depends_list = parser.depends_pkgs
       else depends_list = depends_list ..","..parser.depends_pkgs end
     end
-    
+
     exe_before[parser.init_script.." stop"] = "Stopping "
-    
+
     if parser.enable == nil or tonumber(parser.enable) == 0 then
       exe_before[parser.init_script.. " disable"] = "Disabling "
     else
       exe_before[parser.init_script.. " enable"] = "Enabling "
       exe_after [parser.init_script.. " start"] = "Starting "
     end
-    
+
     if parser.reboot == true then
       reboot_list[#reboot_list+1] = file
     end
-    
+
     if parser.exe_before then
       for command,msg in pairs(parser.exe_before) do
         exe_before[command] = msg
@@ -121,20 +135,15 @@ function call_parser(file,parsers_list,depends_list,exe_before,exe_after,reboot_
         exe_after[command] = msg
       end
     end
-
+--[[
+    wwwprint("------"..file.. "--------")
+    for f, d in pairs(parsers_list) do
+      wwwprint(f)
+      wwwprint(d.p.name)
+    end
+    wwwprint("-------------------------")
+]]--
 end
-
-local reboot_list = {}
-local parsers_list = {}
-local old_parsers = {}
-local depends_list = ""
-local stop_list = {}
--- local enable_list = {}
--- local disable_list = {}
-local file_list = {}
-
-local exe_before = {}
-local exe_after = {}
 
 local file_to_process = {}
 for i=1, #arg do
@@ -188,15 +197,22 @@ for i=1, #file_to_process do
     end
 
     if parser.call_parser then
-      for i in string.gmatch(parser.call_parser,"%S+") do
-        call_parser(i,parsers_list,depends_list,exe_before,exe_after,reboot_list)
+      for u in string.gmatch(parser.call_parser,"%S+") do
+        call_parser(u)
       end
     end
   elseif io.exists("/usr/lib/webif/apply-"..file) == true then
     old_parsers[file] = "/usr/lib/webif/apply-"..file
   end
 end
-
+--[[
+    wwwprint("-------------------------")
+    for f, d in pairs(parsers_list) do
+      wwwprint(f)
+      wwwprint(d.p.name)
+    end
+    wwwprint("-------------------------")
+]]--
 install(depends_list)
 
 if __WWW then 
@@ -218,6 +234,7 @@ end
 -- and execute the parsers
 
 for f, d in pairs(parsers_list) do
+  wwwprint("File "..f)
   wwwprint("Processing "..d.p.name)
   d.p.process()
   file_list[f] = true
@@ -241,14 +258,12 @@ print(init_list(exe_after))
 
 --  local form = formClass.new("Apply...",true)
 --  print (form:startFullForm())
-
 	changes_apply=io.popen ("/usr/lib/webif/apply.sh 2>&1")
 	for linea in changes_apply:lines() do
 		wwwprint(trsh(linea))
 	end
 -- 	print (form:endForm())
 	changes_apply:close()
-
 
 if __WWW then print(page:footer()) end
 os.exit(0)
