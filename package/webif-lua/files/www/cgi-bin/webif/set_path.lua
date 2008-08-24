4ad4a9ca445f3093d2e5958ba75fff58c0417cc4
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-08-24 22:27:40 +0000

    change set_path to default path of lua (at this time)

--- package/webif-lua/files/www/cgi-bin/webif/set_path.lua
+++
@@ -1,2 +0,0 @@
---package.cpath = "?;./?.so;/usr/share/internet-wifi/lib.so/?.so;" .. package.cpath 
-package.path = package.path .. "/usr/lib/lua-wrt/?.lua;/usr/lib/lua-wrt/pkgs/?.lua;/usr/lib/lua-wrt/applys/?.lua;" 
