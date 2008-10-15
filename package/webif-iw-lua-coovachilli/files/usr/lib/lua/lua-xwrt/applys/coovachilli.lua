aee64576cd8897ae01f6cb34d173352664c8f5bb
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-10-15 06:02:06 +0000

    fixed package

---
+++ package/webif-iw-lua-coovachilli/files/usr/lib/lua/lua-xwrt/applys/coovachilli.lua
@@ -0,0 +1,210 @@
+require("uci_iwaddon")
+require("common")
+
+parser = {}
+local P = {}
+parser = P
+-- Import Section:
+-- declare everything this package needs from outside
+local wwwprint = wwwprint
+if wwwprint == nil then wwwprint=print end
+local ipcalc = ipcalc
+local uci = uci
+local io = io
+local string = string
+local oldprint = oldprint
+local table = table
+local pairs = pairs
+local tonumber = tonumber
+-- no more external access after this point
+setfenv(1, P)
+
+name = "Coovachilli"
+script = "chilli"
+init_script = "/etc/init.d/chilli"
+
+enable = tonumber(uci.get("coovachilli.webadmin.enable")) or 0
+local userlevel = tonumber(uci.get("coovachilli.webadmin.userlevel")) or 0
+local radiususers = tonumber(uci.get("coovachilli.webadmin.radconf")) or 0
+call_parser = nil
+
+reboot = false                -- reboot device after all apply process
+--exe_before = {} -- execute os process in this table before any process
+exe_after  = {} -- execute os process after all apply process
+if radiususers > 1 then
+	call_parser = "freeradius freeradius_check freeradius_clients freeradius_proxy"
+  exe_after["/etc/init.d/radiusd restart"]="freeradius"
+end
+--depends_pkgs = "libltdl freeradius freeradius-mod-files freeradius-mod-chap freeradius-mod-radutmp freeradius-mod-realm iw-freeradius"
+exe_after["/etc/init.d/network restart"]="network"
+exe_after["wifi"]="wifi"
+
+
+function process()
+  write_init()
+  write_config()
+  uci.commit("coovachilli")
+end
+
+
+function write_init()
+  wwwprint ("Writing init file /etc/init.d/chilli")
+  local init_file = [[#!/bin/sh /etc/rc.common
+START=59
+
+EXTRA_COMMANDS="status checkrunning radconfig condrestart"
+EXTRA_HELP="	status	Show current status
+    checkrunning If services is not running start it"
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/sbin/chilli
+NAME=chilli
+DESC=chilli
+CONFFILE=/etc/chilli.conf
+OPTS="--pidfile /var/run/$NAME.pid"
+
+. /etc/chilli/functions
+check_required
+
+start() {
+	echo -n "Starting $DESC: "
+   /sbin/modprobe tun >/dev/null 2>&1
+   echo 1 > /proc/sys/net/ipv4/ip_forward
+
+   writeconfig
+   radiusconfig
+
+   (crontab -l 2>&- | grep -v $0
+      test ${HS_ADMINTERVAL:-0} -gt 0 && echo "*/$HS_ADMINTERVAL * * * * $0 radconfig"
+      echo "*/10 * * * * $0 checkrunning"
+      #echo "*/2  * * * * $0 arping"
+   ) | crontab - 2>&-
+  $DAEMON -- $OPTS
+  RETVAL=$?
+	echo "$NAME."
+}
+
+status () {
+		pid=$(cat "/var/run/$NAME.pid" 2>/dev/null)
+		[ -n "$pid" -a -d "/proc/$pid" ] && {
+      echo "$DESC running"
+      exit 0
+		}
+    echo "$DESC stopped"
+}
+
+checkrunning () {
+    local pid
+		pid=$(cat "/var/run/$NAME.pid" 2>/dev/null)
+		[ -n "$pid" -a -d "/proc/$pid" ] && {
+			exit 0
+		}
+    $0 start
+}
+
+radconfig () {
+      [ -e $MAIN_CONF ] || writeconfig
+      radiusconfig
+}
+
+stop () {
+    local pid
+		pid=$(cat "/var/run/$NAME.pid" 2>/dev/null)
+		[ -n "$pid" -a -d "/proc/$pid" ] && {
+      echo -n "Stopping $DESC: "
+      crontab -l 2>&- | grep -v $0 | crontab -
+      kill -TERM "$pid"
+      [ "$?" -eq 0 ] && sleep 1
+      [ ! -d "/proc/$pid" ] && echo "OK" || {
+  			echo "Failed!"
+        echo -n "Killing chilli..."
+        kill -KILL "$pid"
+        [ "$?" -eq 0 ] && echo "OK" || echo "Failed!"
+      }
+      exit 0
+		}
+		echo "$DESC was not running"
+}
+
+condrestart() {
+    local pid
+		pid=$(cat "/var/run/$NAME.pid" 2>/dev/null)
+		[ -n "$pid" -a -d "/proc/$pid" ] && {
+      echo -n "Restarting $DESC: "
+      $0 restart
+      RETVAL=$?
+		}
+}
+]]
+  write_file = io.open("/etc/init.d/chilli","w")
+  write_file:write(init_file)
+  write_file:close()
+  wwwprint("/etc/init.d/chilli writed OK!")
+end
+
+function set_alloweds()
+	coovachilli = uci.get_all("coovachilli")
+	allowed = uci.get_type("coovachilli","sitesallowed")
+	tallow = {}
+	if allowed then
+		for i,t in pairs(allowed) do
+			sites = string.gsub(t.site," ","")
+			for site in string.gmatch(sites,"[^,]+") do
+				tallow[site] = "" 
+			end
+		end
+	end
+	if coovachilli.settings.HS_RADIUS then tallow[coovachilli.settings.HS_RADIUS] = "" end
+	if coovachilli.settings.HS_RADIUS2 then tallow[coovachilli.settings.HS_RADIUS2] ="" end
+	if coovachilli.settings.HS_DNS1 then tallow[coovachilli.settings.HS_DNS1] ="" end
+	if coovachilli.settings.HS_DNS2 then tallow[coovachilli.settings.HS_DNS2] ="" end
+	if coovachilli.settings.HS_UAMLISTEN then tallow[coovachilli.settings.HS_UAMLISTEN] ="" end
+	tallow["x-wrt.org"] = ""
+	tallow["openwrt.org"] = ""
+	tallow["www.internet-wifi.com.ar"] = ""
+	tallow["coova.org"] = ""
+	if coovachilli.system.paypal ~= nil then
+		tallow["www.paypal.com"] = ""
+		tallow["66.211.168.0/24"] = ""
+		tallow["64.4.241.0/24"] = ""
+		tallow["216.113.188.0/24"] = ""
+		tallow["www.paypalobjects.com"] = ""
+		tallow["88.221.0.0/16"] = ""
+		tallow["84.53.0.0/16"] = ""
+		tallow["67.133.200.0/22"] = ""
+		tallow["72.246.0.0/15"] = ""
+		tallow["paypal.112.2o7.net"] = ""
+		tallow["216.52.17.0/24"] = ""
+		tallow["70.42.134.0/24"] = ""
+		tallow["128.242.125.0/24"] = ""
+	end 
+	local allow_str =""
+	local sep_str = ""
+	for i, v in pairs(tallow) do
+		allow_str = allow_str..sep_str..i
+		sep_str=","
+	end
+	return allow_str
+end
+
+function write_config()
+  wwwprint ("Writing configuration file /etc/chilli/config")
+	settings = uci.get_section("coovachilli","settings")
+	conf_str = "#### This conf file was writed by webif-iw-lua-coovachilli-apply ####\n"
+	if settings then
+		for i, t in pairs(settings) do
+			if not string.match(i,"[.]") then
+      	if string.match(t,"%s") then t = "\""..t.."\"" end
+				conf_str = conf_str .. i.."="..t.."\n"
+			end
+		end
+	end
+	conf_str = conf_str.."HS_UAMALLOW="..set_alloweds().."\n"
+
+  write_file = io.open("/etc/chilli/config","w")
+  write_file:write(conf_str)
+  write_file:close()
+  wwwprint("/etc/chilli/config writed OK!")
+end
+
+return parser
---
+++ package/webif-iw-lua/files/usr/lib/lua/lua-xwrt/firewall.lua
@@ -0,0 +1,207 @@
+--[[
+	firewall handle functions
+ 	Copyright (C) 2008 Fabian Omar Franzotti <fofware@gmail.com>	
+	
+	functions:
+		set_zone(zone,input,output,forward,masq)
+			input, output, forward, masq can be nil
+			this function create or modify the values of zone
+		
+		delete_zone(zone)
+			this function delete zones and all forwardings to this zone name
+		
+		set_forwarding(src,dest)
+			this functin create forwarding
+		
+		delete_forwarding(src,dest)
+			this function delete forwarding
+]]--
+require("uci_iwaddon")
+require("common")
+
+firewall = {}
+local P = {}
+firewall = P
+-- Import Section:
+-- declare everything this package needs from outside
+local print = print
+local pairs = pairs
+local uci = uci
+local string = string
+ 
+-- no more external access after this point
+setfenv(1, P)
+
+function reset()
+	uci.save("firewall")
+	all = {}
+	zones = {}
+	forwardings = {}
+	defaults = {}
+	all = uci.get_all("firewall")
+	for i,t in pairs(all) do
+		if t[".type"] == "zone" then
+			zones[t.name] = i
+		end
+		if t[".type"] == "forwarding" then
+			if forwardings[t.src] == nil then forwardings[t.src] = {} end
+			forwardings[t.src][t.dest] = i
+		end
+		if t[".type"] == "defaults" then
+			defaults = t
+		end
+	end
+end
+
+function check_valid_value(val)
+	local val = has_value(val)
+	if val == nil then return nil end
+	if val then
+		val = string.trim(val)
+		if val ~= "ACCEPT"
+		or val ~= "REJECT"
+		or val ~= "DROP" then
+			val = nil
+		end
+	end
+	return val
+end
+		 
+function has_value(val)
+	local val = val
+	if val == nil then return nil end
+	val = string.trim(val)
+	if val == "" then val = nil end
+	return val
+end
+		 
+function zone(zone)
+	local zone = has_value(zone)
+	if zone then
+		return zones[zone]
+	else
+		return zones
+	end
+end
+
+function set_zone(zone,input,output,forward,masq)
+	local cfgret = nil
+	local zone = has_value(zone)
+	if zone then
+		networks = uci.get_all("network")
+		if networks[zone] == nil then 
+			print(zone," Not found")
+			return nil
+		end
+		local input = check_valid_value(input)
+		local output = check_valid_value(output)
+		local forward = check_valid_value(forward)
+		if string.trim(masq) == "" then masq = nil end
+		
+		if zones[zone] then
+			cfgret = zones[zone]
+			input = input or uci.get("firewall",cfgret,"input")
+			output = output or uci.get("firewall",cfgret,"output")
+			forward = forward or uci.get("firewall",cfgret,"forward")
+			masq = masq or uci.get("firewall",cfgret,"masq")
+		else
+			input = input or defaults.input
+			output = output or defaults.output
+			forward = forward or defaults.forward
+			masq = masq or defaults.masq
+			cfgret = uci.add("firewall","zone")
+		end
+		if cfgret then
+			uci.set("firewall",cfgret,"name",zone)
+			uci.set("firewall",cfgret,"input",input)
+			uci.set("firewall",cfgret,"output",output)
+			uci.set("firewall",cfgret,"forward",forward)
+			if masq then uci.set("firewall",cfgret,"masq",masq) end
+			reset()
+		end
+	end
+	return cfgret
+end
+
+function delete_zone(zone)
+	local zone = has_value(zone)
+	ret = false
+	if zone then
+		if forwardings then
+			for i,t in pairs(forwardings) do
+				for k,cfg in pairs(t) do
+					if i == zone
+					or k == zone then
+						uci.delete("firewall",cfg)
+					end
+				end
+			end
+		end 
+		if zones[zone] then			
+			ret = uci.delete("firewall",zones[zone])
+			reset()
+		end
+	end
+	return ret
+end
+
+function forwarding(src,dest)
+	local src = has_value(src)
+	local dest = has_value(dest)
+	if src then
+		if dest then
+			return forwardings[src][dest]
+		else
+			return forwardings[src]
+		end
+	else
+		return forwardings
+	end
+end
+
+function set_forwarding(src,dest)
+	cfgret = nil
+	local src = has_value(src)
+	local dest = has_value(dest)
+	print(src, dest)
+	if src and dest then
+		if forwardings[src] and forwardings[src][dest] then
+			print("dice que existe")
+			cfgret = forwardings[src][dest]
+		else
+			print("set_zone ",src)
+			print("set_zone ",dest)
+			set_zone(src)
+			set_zone(dest)
+			cfgret = uci.add("firewall","forwarding")
+			if cfgret then
+				uci.set("firewall",cfgret,"src",src)
+				uci.set("firewall",cfgret,"dest",dest)
+				reset()
+			end
+		end
+	end
+	return cfgret
+end
+
+function delete_forwarding(src,dest)
+	local ret = false
+	local src = has_value(src)
+	local dest = has_value(dest)
+	if src and dest then
+		if forwardings[src][dest] then
+			ret = uci.delete("firewall",forwardings[src][dest])
+			reset()
+		end
+	end
+	return ret
+end
+
+-- defaults --
+all = {}
+zones = {}
+forwardings = {}
+defaults = {}
+reset()
+
+return firewall
