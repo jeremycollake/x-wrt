8e84754a2c08606c652b6f22aabcf44434f9e00e
kemen04 <kemen04@2dee665f-611b-0410-ab39-d748a57cfec1>
2008-11-06 18:23:21 +0000

    backport changes from trunk

--- tags/kamikaze_8.09/package/webif/files/www/cgi-bin/webif/network-wlan.sh
+++ tags/kamikaze_8.09/package/webif/files/www/cgi-bin/webif/network-wlan.sh
@@ -205,8 +205,10 @@ for device in $DEVICES; do
 	        config_get FORM_channel $device channel
 	        config_get FORM_maxassoc $device maxassoc
 	        config_get FORM_distance $device distance
-	        config_get_bool FORM_txantenna $device txantenna 0
-	        config_get_bool FORM_rxantenna $device rxantenna 0
+	        config_get FORM_txantenna $device txantenna
+		[ -n $FORM_txantenna ] && FORM_txantenna=0
+	        config_get FORM_rxantenna $device rxantenna
+		[ -n $FORM_raxntenna ] && FORM_rxantenna=0
 	        config_get_bool FORM_diversity $device diversity 1
 	        config_get_bool FORM_disabled $device disabled 0
 		config_get FORM_antenna $device antenna
