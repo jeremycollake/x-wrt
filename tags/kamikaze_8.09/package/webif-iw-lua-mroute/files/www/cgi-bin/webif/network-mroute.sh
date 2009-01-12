b495290a0be4fa518d31a357908a751017316c97
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2009-01-12 18:52:31 +0000

    some fixes to mroute new package to webif to configure mroute script

---
+++ tags/kamikaze_8.09/package/webif-iw-lua-mroute/files/www/cgi-bin/webif/network-mroute.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/lua
+--------------------------------------------------------------------------------
+-- mroute.sh
+-- This script is writen in LUA, the extension is ".sh" for compatibilities
+-- reasons width menu system of X-Wrt
+--
+-- Description:
+--        Administrative console to configure load balancing route
+--
+-- Author(s) [in order of work date]:
+--       Fabián Omar Franzotti
+--         
+-- Configuration files referenced:
+--    mroute
+--
+--------------------------------------------------------------------------------
+--[[
+##WEBIF:name:Network:510:M-Routes
+]]--
+require("set_path")
+require("init")
+require("mroute")
+require("webpkg")
+-- pageClass is part of the framework 
+local fstat, f = load_file("/var/run/mroute.active")
+if f == false then
+	f=io.popen("/etc/init.d/mroute status")
+	for line in f:lines() do
+		page.title = "Multi-Route is "..line
+	end
+else
+	page.title = "Multi-Route - "..fstat
+end
+
+mroute.set_menu()
+__WIP=2
+
+if __FORM.__menu == nil then
+  __FORM.__menu = "7:8"
+end
+
+__FORM.option = string.trim(__FORM.option) or ""
+forms = {}
+if __FORM.option == "status" then
+--	forms[#forms+1] = mroute.status_form()
+	forms[#forms+1] = mroute.interfaces_form()
+elseif __FORM.option == "iface" then
+	forms[#forms+1] = mroute.tuneup_form()
+elseif __FORM.option == "tuneup" then
+	forms[#forms+1] = mroute.tuneup_form()
+else
+	forms[#forms+1] = mroute.core_form()
+	forms[#forms+1] = mroute.interfaces_form()
+end
+
+print(page:header())
+for i=1, #forms do
+  forms[i]:print()
+end
+for k, v in pairs(__FORM) do
+	print(k,v,"<br>")
+end
+print (page:footer())
