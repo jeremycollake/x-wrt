cc22b84d927a86c7295eb7c085e492953f34d00e
lubekgc <lubekgc@2dee665f-611b-0410-ab39-d748a57cfec1>
2009-03-11 16:22:01 +0000

    fix finally SVG pages

--- package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
+++ package/webif/files/www/cgi-bin/webif/graphs-cpu.sh
@@ -7,14 +7,14 @@
 . /www/cgi-bin/webif/graphs-subcategories.sh
 
 header "Graphs" "graphs_cpu_subcategory#CPU" "@TR<<graphs_cpu_CPU_Usage#CPU Usage>>" "" ""
-# IE (all versions) does not support the object tag with svg!
-#	<object data="" width="500" height="250" type="image/svg+xml">@TR<<graphs_svg_required#This object requires the SVG support.>></object>
+# This construction supports all recent browsers, degrades correctly, 
+# see http://joliclic.free.fr/html/object-tag/en/object-svg.html
 ?>
 <center>
-	<object data="/cgi-bin/webif/graph_cpu_svg.sh"
-		width="500" height="250" type="image/svg+xml" />
-	<object>
-		<br><a href=http://www.adobe.com/svg/viewer/install/main.html>@TR<<If the graph is not fuctioning download the viewer here.>></a>
+	<object type="image/svg+xml" data="/cgi-bin/webif/graph_cpu_svg.sh"
+		width="500" height="250">
+		<param name="src" value="/cgi-bin/webif/graph_cpu_svg.sh" />
+		<a href="http://www.adobe.com/svg/viewer/install/main.html">@TR<<graphs_svg_download#If the graph is not fuctioning download the viewer here.>></a>
 	</object>
 </center>
 <? footer ?>
