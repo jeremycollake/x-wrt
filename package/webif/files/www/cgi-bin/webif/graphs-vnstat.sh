88e0b418a67d718e924729b4abf947749b07e6d5
kemen04 <kemen04@2dee665f-611b-0410-ab39-d748a57cfec1>
2009-03-13 05:32:17 +0000

    Only generate horizontal summary images with multiple vnstat interfaces, hide the link outline flicker thanks stephendwalker closes #134

--- package/webif/files/www/cgi-bin/webif/graphs-vnstat.sh
+++ package/webif/files/www/cgi-bin/webif/graphs-vnstat.sh
@@ -8,7 +8,10 @@ header_inject_head=$(cat <<EOF
     <script type="text/javascript" src="/webif.js"></script>
     <style type="text/css">
     <!--
-    .monthly {
+    a.graphs_vnstat_link:visited {
+        outline: none;
+    }
+    #graphs_vnstat_monthly {
         padding-top: 2px;
     }
     -->
@@ -31,13 +34,6 @@ if [ -z "$interfaces" ]; then
     echo "<pre>Replace 'eth0' with the interface that should be monitored. A list</pre>"
     echo "<pre>of available interfaces can be seen with the 'ifconfig' command.</pre>"
 else
-    interfaces_count=$(ls $LIB_D | wc -l)
-    if [ $interfaces_count -eq 1 ]; then
-        multiple=false;
-    else
-        multiple=true;
-    fi
-
     BIN=${IPKG_INSTROOT}/usr/bin/vnstati
     VAR_D=${IPKG_INSTROOT}/var/vnstat
     WWW_D=${IPKG_INSTROOT}/www/vnstat
@@ -45,20 +41,26 @@ else
     [ -d $VAR_D ] || mkdir -p $VAR_D
     [ -d $WWW_D ] || mkdir -p $WWW_D
 
+    interfaces_count=$(ls $LIB_D | wc -l)
+    [ $interfaces_count -eq 1 ] && multiple=false || multiple=true
+
+    outputs="s h d t m"
+    $multiple && outputs="hs $outputs"
+
     for interface in $interfaces; do
-        for output in hs s h d t m; do
+        for output in $outputs; do
             [ -L $WWW_D/vnstat_${interface}_${output}.png ] || ln -sf $VAR_D/vnstat_${interface}_${output}.png $WWW_D/vnstat_${interface}_${output}.png
             $BIN -${output} -i $interface -c 15 -o $VAR_D/vnstat_${interface}_${output}.png
         done
-cat <<EOF 
+cat <<EOF
     <h2>Traffic of Interface $interface</h2>
 EOF
         if $multiple; then
 cat <<EOF
-    <a href="#" title="Click to see ${interface}'s Details" onclick="set_visible('${interface}_summary', false); set_visible('${interface}_details', true);">
+    <a class="graphs_vnstat_link" href="#" title="Click to see ${interface}'s Details" onclick="set_visible('${interface}_summary', false); set_visible('${interface}_details', true);">
         <img id="${interface}_summary" src="/vnstat/vnstat_${interface}_hs.png" alt="${interface} Summary" />
     </a>
-    <a href="#" title="Click to see ${interface}'s Summary" onclick="set_visible('${interface}_details', false); set_visible('${interface}_summary', true);">
+    <a class="graphs_vnstat_link" href="#" title="Click to see ${interface}'s Summary" onclick="set_visible('${interface}_details', false); set_visible('${interface}_summary', true);">
         <table id="${interface}_details" summary="${interface} Details" style="display: none;">
 EOF
         else
@@ -83,7 +85,7 @@ cat <<EOF
                 <td valign="top">
                     <img src="/vnstat/vnstat_${interface}_t.png" alt="${interface} Top 10" />
                     <br />
-                    <img class="monthly" src="/vnstat/vnstat_${interface}_m.png" alt="${interface} Monthly" />
+                    <img id="graphs_vnstat_monthly" src="/vnstat/vnstat_${interface}_m.png" alt="${interface} Monthly" />
                 </td>
             </tr>
         </tbody>
