9c6d2fe5f85dd200041e9f00a2156ffe6eac1e31
kemen04 <kemen04@2dee665f-611b-0410-ab39-d748a57cfec1>
2009-09-05 21:38:09 +0000

    add patches done by Stefan Stapelberg <stefan@rent-a-guru.de> * Rewrite of status-wlan-survey.sh * Caching of permissions for viewing pages. * Caching the list of categories/subcategories on a per-user base. (Provides a 2 second speedup of network.sh on my WRT54G V2.2)

--- package/webif/files/www/cgi-bin/webif/graphs-subcategories.sh
+++ package/webif/files/www/cgi-bin/webif/graphs-subcategories.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+
+cachedir=/tmp/.webcache
+
+uci_load network
+
 subcategories() {
-	(
-	echo "#""#WEBIF:name:Graphs:1:graphs_cpu_subcategory#CPU:graphs-cpu.sh"
-	echo "#""#WEBIF:name:Graphs:160:graphs_bandwidth_subcategory#Bandwidth:graphs-bandwidth.sh"
-	echo "#""#WEBIF:name:Graphs:3:graphs_vnstat_subcategory#Vnstat:graphs-vnstat.sh"
-	cat /proc/net/dev | \
-	egrep -v "No statistics available" | \
-	sed -n '/:/{s/:.*//;s/^ *\(.*\)/##WEBIF:name:Graphs:2:graphs_if_Traffic#Traffic\>\> \1@TR\<\<:graphs-if.sh?if=\1/;p}'
-	) | \
-	awk -v "selected=$2" \
-	-v "USER=$REMOTE_USER" \
-	-v "rootdir=$rootdir" \
-	-f /usr/lib/webif/common.awk \
-	-f /usr/lib/webif/subcategories.awk -
+	[ -s "$cachedir/graphs" ] || {          # create a cache if it does not exists already
+		[ -d "$cachedir" ] || mkdir "$cachedir" 2>/dev/null 1>&2
+		(echo "Graphs:10:CPU:graphs-cpu.sh"
+		 echo "Graphs:20:Bandwidth:graphs-bandwidth.sh"
+		 echo "Graphs:30:Vnstat:graphs-vnstat.sh"
+		 sed -n -e "/:/"'{s/:.*//;s/^ *\(.*\)/Graphs:50:Traffic\>\> \1@TR\<\<:graphs-if.sh?if=\1/;p}' /proc/net/dev 2>/dev/null
+		) | sort -n >$cachedir/graphs
+	}
+	awk -F: -v "category=Graphs" -v "selected=$2" -f /usr/lib/webif/common.awk -f /usr/lib/webif/subcategories.awk $cachedir/graphs
 }
--- package/webif/files/www/cgi-bin/webif/system-access-control.sh
+++ package/webif/files/www/cgi-bin/webif/system-access-control.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/webif-page
-<%
-. "/usr/lib/webif/webif.sh"
+<?
+. /usr/lib/webif/webif.sh
 
 ###################################################################
-# system configuration page
+#
+# System configuration page
 #
 # Description:
 #	Configures access control for the webif
@@ -17,6 +18,10 @@
 #   /etc/config/webif_access_control
 #
 
+cachedir=/tmp/.webcache
+#admin is also a superuser but maybe removed in the future.
+superuser=root
+
 mkdir /tmp/.webif/
 exists /etc/config/webif_access_control || touch /etc/config/webif_access_control
 
@@ -26,105 +31,105 @@ string|FORM_password_add|@TR<<Password>>|required min=5|$FORM_password_add
 EOF
 	equal "$FORM_password_add" "$FORM_password2_add" || {
 		[ -n "$ERROR" ] && ERROR="${ERROR}<br />"
-		ERROR="${ERROR}@TR<<Passwords do not match>><br />"
+		ERROR="${ERROR}@TR<<password_mismatch#The passwords do not match!>><br />"
 	}
 	if [ "${FORM_user_add}" = "root" -o "${FORM_user_add}" = "admin" ]; then
 		[ -n "$ERROR" ] && ERROR="${ERROR}<br />"
-		ERROR="${ERROR}@TR<<root and admin are already users.>><br />"
+		ERROR="${ERROR}@TR<<root and admin are already users>>.<br />"
 	fi
 	empty "$ERROR" && {
 		password=$(httpd -m $FORM_password_add)
 		[ -e /tmp/.webif/file-httpd.conf ] || cp /etc/httpd.conf /tmp/.webif/file-httpd.conf
-		echo "/cgi-bin/webif/:${FORM_user_add}:${password}" >> /tmp/.webif/file-httpd.conf
+		echo "/cgi-bin/webif/:${FORM_user_add}:${password}" >>/tmp/.webif/file-httpd.conf
 		uci_add "webif_access_control" "accesscontrol" "${FORM_user_add}"
 	}
 fi
 header "System" "Access Control" "@TR<<Access Control>>" '' "$SCRIPT_NAME"
+
 exists /tmp/.webif/file-httpd.conf && HTTPD_CONFIG_FILE=/tmp/.webif/file-httpd.conf || HTTPD_CONFIG_FILE=/etc/httpd.conf
-cat $HTTPD_CONFIG_FILE | awk '
-BEGIN {
-	FS=":"
-	ucount = 0
-	include("/usr/lib/webif/common.awk")
-	start_form("Users")
-	if ((ENVIRON["FORM_submit"] != "") && ((ENVIRON["FORM_change_password_"$2] != "") || (ENVIRON["FORM_remove_user_"$2] == ""))) system("/bin/rm /tmp/.webif/file-httpd.conf; touch/tmp/.webif/file-httpd.conf")
+
+awk -F: -v "cachedir=$cachedir" -v "superuser=$superuser" '
+BEGIN { ucount = 0;
+	include("/usr/lib/webif/common.awk");
+	start_form("@TR<<Users>>");
+
+	if (ENVIRON["FORM_submit"] != "") {
+		if (ENVIRON["FORM_remove_user_"$2] != "") {
+			system("/bin/rm -f " cachedir "/cat" $2);
+			system("/bin/rm -f " cachedir "/subcat" $2);
+			system("/bin/rm /tmp/.webif/file-httpd.conf; touch/tmp/.webif/file-httpd.conf");
+		} else if (ENVIRON["FORM_change_password_"$2] != "")
+			system("/bin/rm /tmp/.webif/file-httpd.conf; touch/tmp/.webif/file-httpd.conf");
+	}
 }
-(($1 == "/cgi-bin/webif/") && (($2 != "root") && ($2 != "admin"))) {
+($1 == "/cgi-bin/webif/" && $2 != superuser && $2 != "admin") {
 	if (ENVIRON["FORM_remove_user_" $2] == "") {
-		field($2)
-		password("user_"$2, ENVIRON["FORM_user_" $2])
-		field("&nbsp;","")
-		submit("change_password_"$2, "Change Password")
-		field("&nbsp;","")
-		submit("remove_user_"$2, "Remove "$2)
-		ucount = ucount + 1
+		field($2);
+		password("user_"$2, ENVIRON["FORM_user_" $2]);
+		submit("change_password_"$2, "@TR<<system_acl_changepw#Change Password>>");
+		field("&nbsp;");
+		print "<span class=\"smalltext\"><a href=\"" ENVIRON["SCRIPT_NAME"] "?remove_user_" $2 "\">@TR<<system_acl_remove_user#Remove user>> " $2 "</a></span>";
+		field("&nbsp;");
+		ucount = ucount + 1;
 	}
 }
 ((ENVIRON["FORM_submit"] != "") && ($1 != "")) {
 	if (($1 == "/cgi-bin/webif/") && (ENVIRON["FORM_remove_user_"$2] == "") && (ENVIRON["FORM_change_password_"$2] == "")) {
-		print $1":"$2":"$3 >> "/tmp/.webif/file-httpd.conf"
+		print $1":"$2":"$3 >>"/tmp/.webif/file-httpd.conf";
 	}
 	if ($1 != "/cgi-bin/webif/") {
-		print $1":"$2 >> "/tmp/.webif/file-httpd.conf"
+		print $1":"$2 >>"/tmp/.webif/file-httpd.conf";
 	}
 	if (ENVIRON["FORM_change_password_"$2] != "") {
-		("httpd -m " ENVIRON["FORM_user_"$2]) | getline password
-		print $1":"$2":"password >> "/tmp/.webif/file-httpd.conf"
+		("httpd -m " ENVIRON["FORM_user_"$2]) | getline password;
+		print $1":"$2":"password >>"/tmp/.webif/file-httpd.conf";
 	}
 }
 END {
-	if (ucount == 0) {
-		print "<tr><td>No users defined."
-	}
-	end_form("","",1)
-	start_form("Add User")
-	field("Username")
-	textinput3("user_add", ENVIRON["FORM_user_add"])
-	field("Password")
-	password("password_add")
-	field("Confirm Password")
-	password("password2_add")
-	field("&nbsp;","")
-	submit("add_user", "Add User")
-	end_form("","",1)
-}'
+	if (ucount == 0)
+		print "<tr><td>No users defined.";
+
+	end_form("","",1);
+	start_form("@TR<<Add User>>");
+	field("@TR<<Username>>");
+	textinput3("user_add", ENVIRON["FORM_user_add"]);
+	field("@TR<<Password>>");
+	password("password_add");
+	field("@TR<<Confirm Password>>");
+	password("password2_add");
+	field("&nbsp;","");
+	submit("add_user", "@TR<<Add User>>");
+	end_form("","",1);
+}' $HTTPD_CONFIG_FILE 2>/dev/null
+
 exists /tmp/.webif/file-httpd.conf && HTTPD_CONFIG_FILE=/tmp/.webif/file-httpd.conf || HTTPD_CONFIG_FILE=/etc/httpd.conf
-users=`cat $HTTPD_CONFIG_FILE | awk '
-BEGIN {
-	FS=":"
-}
-($1 == "/cgi-bin/webif/") {
-	if (($2 != "root") && ($2 != "admin")) print $2
-}`
+users=$(awk -F":" '($1 == "/cgi-bin/webif/") { if (($2 != "root") && ($2 != "admin")) print $2 }' $HTTPD_CONFIG_FILE 2>/dev/null)
 
 for user in $users; do
 	export user
-	grep -H "##[W]EBIF:name:" /www/cgi-bin/webif/*.sh |sed -e 's,^.*/\([a-zA-Z0-9\.\-]*\):\(.*\)$,\2:\1,' |sort -n |awk '
+	#[ -s $cachedir/subcat_${user} ] || init_cache "$user"
+	awk -F: '
 BEGIN {
-	FS=":"
 	include("/usr/lib/webif/common.awk")
 	config_load("webif_access_control")
-	start_form("User:" ENVIRON["user"])
+	start_form("@TR<<ACL User>>: " ENVIRON["user"])
 }
-($2 == "name") {
+{
 	if (ENVIRON["FORM_submit"] == "") {
-		if ($3 != "Graphs") {
-			var = config_get_bool(ENVIRON["user"], $3"_"$4, "0")
-		}
-		else {
+		if ($1 != "Graphs") {
+			var = config_get_bool(ENVIRON["user"], $1"_"$2, "0")
+		} else {
 			var = config_get_bool(ENVIRON["user"], "Graphs", "0")
 		}
-	}
-	else {
-		if ($3 != "Graphs") {
-			varorig = config_get_bool(ENVIRON["user"], $3"_"$4, "0")
-			var2 = "FORM_" ENVIRON["user"] "_" $3 "_" $4
+	} else {
+		if ($1 != "Graphs") {
+			varorig = config_get_bool(ENVIRON["user"], $1"_"$2, "0")
+			var2 = "FORM_" ENVIRON["user"] "_" $1 "_" $2
 			var = ENVIRON[var2]
 			if (varorig != var) {
-				uci_set("webif_access_control", ENVIRON["user"], $3 "_" $4, var)
+				uci_set("webif_access_control", ENVIRON["user"], $1 "_" $2, var)
 			}
-		}
-		else {
+		} else {
 			varorig = config_get_bool(ENVIRON["user"], "Graphs", "0")
 			var2 = "FORM_" ENVIRON["user"] "_Graphs"
 			var = ENVIRON[var2]
@@ -133,19 +138,18 @@ BEGIN {
 			}
 		}
 	}
-	if ((category != $3) && ($3 != GRAPHS)) {
-		field("<h2>@TR<<"$3">></h2>")
+	if (($1 != category) && ($1 != GRAPHS)) {
+		field("<h2>@TR<<"$1">></h2>")
 	}
-	category = $3
-	if ($3 != GRAPHS) {
-		if ($3 == "Graphs") {
-			GRAPHS = $3
+	category = $1
+	if ($1 != GRAPHS) {
+		if ($1 == "Graphs") {
+			GRAPHS = $1
 			field("@TR<<Graphs>>")
 			select(ENVIRON["user"]"_Graphs", var)
-		}
-		else {
-			field("@TR<<"$5">>")
-			select(ENVIRON["user"]"_"$3"_"$4, var)
+		} else {
+			field("@TR<<"$3">>")
+			select(ENVIRON["user"]"_"$1"_"$2, var)
 		}
 		option("1", "@TR<<Enabled>>")
 		option("0", "@TR<<Disabled>>")
@@ -153,13 +157,15 @@ BEGIN {
 		select_open = 0
 	}
 }
-END {
-	end_form("","",1)
-}'
+	END {
+		end_form("","",1)
+	}' $cachedir/subcat_$superuser
 done
 
-footer %>
+footer
+
+?>
 
 <!--
-##WEBIF:name:System:150:Access Control
+##WEBIF:name:System:160:Access Control
 -->
