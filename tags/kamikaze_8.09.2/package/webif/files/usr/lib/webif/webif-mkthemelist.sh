a91bbab306381a114c8e54092add5a27b6fdd0ee
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2010-02-03 09:35:21 +0000

    wrong previous fix :)

--- tags/kamikaze_8.09.2/package/webif/files/usr/lib/webif/webif-mkthemelist.sh
+++ tags/kamikaze_8.09.2/package/webif/files/usr/lib/webif/webif-mkthemelist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 themes_lst="/etc/themes.lst"
 tmpthemeslst=$(mktemp "/tmp/.webif-XXXXXX")
-opkg list | awk '/webif-theme/ { gsub("webif-theme-*",""); if ($5) print "option|"$1"|"$5}' | sort | uniq >> "$tmpthemeslst"
+opkg list | awk '/webif-theme/ { gsub("webif-theme-",""); if ($5) print "option|"$1"|"$5}' | sort | uniq >> "$tmpthemeslst"
 if [ "'cat $tmpthemeslst'" != "" ]; then
 	if [ "'cat $tmpthemeslst'" != "'cat $themes_lst'" ]; then
 		rm -f "$themes_lst"
