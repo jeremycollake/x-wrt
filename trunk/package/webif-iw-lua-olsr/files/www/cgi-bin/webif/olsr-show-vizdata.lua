ad30d07b59e93fe1d6f37daab255bf9cf7904783
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2010-03-24 15:35:45 +0000

    Removing old Stuff of lua Preparing to commit new stuff

--- trunk/package/webif-iw-lua-olsr/files/www/cgi-bin/webif/olsr-show-vizdata.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua
-require("set_path")
-require("init")
-require("olsrdata")
-olsrdata = olsrdataClass.new()
-print([[Content-Type: text/html; charset=UTF-8
-Pragma: no-cache
-]])
-print (olsrdata:html())
