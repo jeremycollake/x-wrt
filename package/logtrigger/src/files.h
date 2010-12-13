2659aa7ed6e7330c805b124c97da8438b3279e95
fofware@gmail.com <fofware@gmail.com@2dee665f-611b-0410-ab39-d748a57cfec1>
2010-12-13 08:10:40 +0000

    get sources from own hosting

--- package/logtrigger/src/files.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LOGTRIGGER_FILES_H
-#define _LOGTRIGGER_FILES_H
-
-//char *readfile(FILE *fp);
-char *readfile(FILE *fp, char *ret);
-char *readNewLines(const char *filename, long *last, char *ret);
-char *readline(FILE *pf, char *tmp);
-long getsize(const char *filename);
-#endif
