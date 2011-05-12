01a6531d95dfdc9fba64e9844ee153ece8d1bd4f
kemen04 <kemen04@2dee665f-611b-0410-ab39-d748a57cfec1>
2011-05-12 00:24:52 +0000

    remove qos and wireless monitor interfaces

--- trunk/package/webif/files/www/cgi-bin/webif/graphs-subcategories.sh
+++ trunk/package/webif/files/www/cgi-bin/webif/graphs-subcategories.sh
@@ -10,7 +10,7 @@ subcategories() {
 		(echo "Graphs:10:CPU:graphs-cpu.sh"
 		 echo "Graphs:20:Bandwidth:graphs-bandwidth.sh"
 		 echo "Graphs:30:Vnstat:graphs-vnstat.sh"
-		 sed -n -e "/:/"'{s/:.*//;s/^ *\(.*\)/Graphs:50:Traffic\>\> \1@TR\<\<:graphs-if.sh?if=\1/;p}' /proc/net/dev 2>/dev/null
+		 grep -v -e "mon." -e "ifb[0-9]" /proc/net/dev |sed -n -e "/:/"'{s/:.*//;s/^ *\(.*\)/Graphs:50:Traffic\>\> \1@TR\<\<:graphs-if.sh?if=\1/;p}' 2>/dev/null
 		) | sort -n >$cachedir/graphs
 	}
 	awk -F: -v "category=Graphs" -v "selected=$2" -f /usr/lib/webif/common.awk -f /usr/lib/webif/subcategories.awk $cachedir/graphs
